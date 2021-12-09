baeb377d04069c5b567a3a14649999be0e4c1d2d
Joel Tulloch <joel@jtulloch.com>
2021-12-09 08:40:05 -0500

    wire up eth connector & provider for infura

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  INFURA_URL: process.env.REACT_APP_INFURA_URL,
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,30 @@ import { Main } from './Main';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+// XXX - this library is entirely untested, and a liability. we should move away from it,
+// but using it initially to get up and running. make sure we maintain
+// isolation.
+import Web3Provider, { Connectors as Web3ReactConnectors } from 'web3-react';
+import { config } from './config';
+import { Web3Connect } from './core-components/web3-connect';
+import { Connectors } from './core-components/web3-connect';
+
+const { NetworkOnlyConnector } = Web3ReactConnectors;
+
+const connectors = {
+  [Connectors.Infura]: new NetworkOnlyConnector({
+    providerURL: config.INFURA_URL,
+  }),
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Main />
+      <Web3Provider connectors={connectors} libraryName='ethers.js'>
+        <Web3Connect>
+          <Main />
+        </Web3Connect>
+      </Web3Provider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')

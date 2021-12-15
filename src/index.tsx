4b5fd4dcde267ab7b765632c0a0421a3c5b6e9d8
Joel Tulloch <joel@jtulloch.com>
2021-12-15 12:23:39 -0500

    add redirect to default route at root

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import { ZnsRouteConnect } from './zns-route-connect';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 
 import { ContextProvider as Web3ReactContextProvider } from './lib/web3/web3-react';
+import {config} from './config';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Web3ReactContextProvider>
+          <Route path='/'>
+            <Redirect to={`/${config.defaultZnsRoute}`} />
+          </Route>
           <Route path='/:znsRoute' component={ZnsRouteConnect} />
         </Web3ReactContextProvider>
       </BrowserRouter>

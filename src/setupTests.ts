d30de5cdd0d66bbb0f6407b20266913d6b303597
Joel Tulloch <joel@jtulloch.com>
2021-12-22 14:54:20 -0500

    hook up extended matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,8 @@
+import * as matchers from 'jest-extended';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// add all jest-extended matchers
+expect.extend(matchers);
 
 configure({ adapter: new Adapter() });

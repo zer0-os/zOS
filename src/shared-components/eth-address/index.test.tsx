0f813dcd3de2ef58eecab0ca8e471c810273ddb4
Joel Tulloch <joel@jtulloch.com>
2022-01-07 13:24:58 -0500

    add eth address display component

---
+++ src/shared-components/eth-address/index.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { EthAddress } from '.';
+
+describe('EthAddress', () => {
+  const subject = (props: any = {}, child: any = <div />) => {
+    const allProps = {
+      address: '',
+      ...props,
+    };
+
+    return shallow(<EthAddress {...allProps} />);
+  };
+
+  it('adds className', () => {
+    const wrapper = subject({ className: 'taco-launcher' });
+
+    expect(wrapper.find('.eth-address').hasClass('taco-launcher')).toBe(true);
+  });
+
+  it('it displays truncated wallet address', () => {
+    const address = '0x0D1C97113D70E4D04345D55807CB19C648E17FBA';
+
+    const wrapper = subject({ address });
+
+    expect(wrapper.find('.eth-address__address').text().trim()).toBe('0x0D1C...7FBA');
+  });
+});

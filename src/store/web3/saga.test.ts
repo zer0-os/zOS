c3f3ad32c34d42e58389497bbfacc8457617b128
Joel Tulloch <joel@jtulloch.com>
2022-01-12 10:36:28 -0500

    Connecting status is propagated to sandbox

--- src/store/web3/saga.test.ts
+++ src/store/web3/saga.test.ts
@@ -1,8 +1,11 @@
 import { expectSaga } from 'redux-saga-test-plan';
+import * as matchers from 'redux-saga-test-plan/matchers';
 
 import { updateConnector } from './saga';
 import { Connectors, ConnectionStatus } from '../../lib/web3';
 import { reducer } from '.';
+import { setConnectionStatus } from '../../app-sandbox/store/web3';
+import { dispatch } from '../../app-sandbox/store';
 
 describe('web3 saga', () => {
   it('should set new connector and status to Connecting', async () => {
@@ -16,5 +19,13 @@ describe('web3 saga', () => {
         connector: Connectors.Metamask,
       },
     });
+  });
+
+  it('should dispatch Connecting status to app sandbox', async () => {
+    await expectSaga(updateConnector, { payload: Connectors.Metamask })
+      .provide([ matchers.call.fn(dispatch) ])
+      .call(dispatch, setConnectionStatus(ConnectionStatus.Connecting))
+      .withReducer(reducer)
+      .run();
   });
 });
--- src/store/web3/saga.ts
+++ src/store/web3/saga.ts
@@ -1,11 +1,17 @@
-import { takeLatest, put } from 'redux-saga/effects';
+import { takeLatest, put, call } from 'redux-saga/effects';
 import { SagaActionTypes, setConnectionStatus, setConnector } from '.';
+import { dispatch } from '../../app-sandbox/store';
+import { setConnectionStatus as appSandboxSetConnectionStatus } from '../../app-sandbox/store/web3';
 
 import { ConnectionStatus } from '../../lib/web3';
 
 export function* updateConnector(action) {
   yield put(setConnector(action.payload));
   yield put(setConnectionStatus(ConnectionStatus.Connecting));
+
+  // do we want to encapsulate this better, or does it make
+  // sense for this to need to dispatch?
+  yield call(dispatch, appSandboxSetConnectionStatus(ConnectionStatus.Connecting));
 }
 
 export function* saga() {

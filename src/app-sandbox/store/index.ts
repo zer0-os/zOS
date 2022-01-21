e69649d6924e7fe8a377493a54706b29ed890fae
Joel Tulloch <joel@jtulloch.com>
2022-01-21 15:43:38 -0500

    remove saga from app-sandbox

--- src/app-sandbox/store/index.ts
+++ src/app-sandbox/store/index.ts
@@ -1,28 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import createSagaMiddleware from 'redux-saga';
-
-import { rootSaga } from './saga';
 
 import { reducer as zns } from './zns';
 import { reducer as web3 } from './web3';
-import { reducer as feed } from '../../apps/feed/store';
-
-const sagaMiddleware = createSagaMiddleware({
-  onError: (e) => {
-    console.error('Encountered uncaught error in root saga: ', e);
-  },
-});
 
 export const store = configureStore({
   reducer: {
     zns,
     web3,
-    feed,
   },
-  middleware: (defaults) => defaults({ thunk: false }).concat(sagaMiddleware),
+  middleware: (defaults) => defaults({ thunk: false }),
 });
-
-sagaMiddleware.run(rootSaga);
 
 export const { dispatch } = store;
 export type RootState = ReturnType<typeof store.getState>;

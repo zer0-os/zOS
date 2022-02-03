1bcff4e6f4d34f3b2b9c5da132a17db14428d4df
Joel Tulloch <joel@jtulloch.com>
2022-02-03 14:33:42 -0500

    add view mode toggle and wire up theme engine

---
+++ src/core-components/theme-engine/index.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { RootState } from '../../store';
+
+import { ThemeEngine, ViewModes } from '../../shared-components/theme-engine';
+import { Container } from '.';
+
+describe('ThemeEngine', () => {
+  const subject = (props: any = {}) => {
+    const allProps = {
+      updateConnector: () => undefined,
+      ...props,
+    };
+
+    return shallow(<Container {...allProps} />);
+  };
+
+  it('passes viewMode to theme engine', () => {
+    const wrapper = subject({ viewMode: ViewModes.Dark });
+
+    expect(wrapper.find(ThemeEngine).prop('viewMode')).toBe(ViewModes.Dark);
+  });
+
+  describe('mapState', () => {
+    const subject = (state: Partial<RootState>) => Container.mapState({
+      ...state,
+      theme: {
+        viewMode: ViewModes.Dark,
+        ...(state.theme || {}),
+      },
+    } as RootState);
+
+    test('viewMode', () => {
+      const state = subject({ theme: { value: { viewMode: ViewModes.Light } } });
+
+      expect(state.viewMode).toEqual(ViewModes.Light);
+    });
+  });
+});

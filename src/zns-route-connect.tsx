042d31a077fdd81f17190ca17a63479094e24ea6
Adam Kahtava <adam@kahtava.com>
2022-04-05 13:53:54 -0400

    Supporting selected app routing, displaying app menu

--- src/zns-route-connect.tsx
+++ src/zns-route-connect.tsx
@@ -3,13 +3,15 @@ import { RootState } from './store';
 import { connectContainer } from './store/redux-container';
 
 import { setRoute } from './store/zns';
+import { setSelectedApp } from './store/apps';
 import { Web3Connect } from './components/web3-connect';
 import { Main } from './Main';
 
 export interface Properties {
   setRoute: (route: string) => void;
+  setSelectedApp: (selectedApp: string) => void;
 
-  match: { params: { znsRoute: string } };
+  match: { params: { znsRoute: string, app: string } };
 }
 
 export class Container extends React.Component<Properties> {
@@ -19,23 +21,33 @@ export class Container extends React.Component<Properties> {
   }
 
   static mapActions(_props: Properties): Partial<Properties> {
-    return { setRoute };
+    return { setRoute, setSelectedApp };
   }
 
   componentDidMount() {
     this.props.setRoute(this.extractRouteFromProps());
+    this.props.setSelectedApp(this.extractAppFromProps());
   }
 
   componentDidUpdate(prevProps: Properties) {
     const currentRoute = this.extractRouteFromProps();
+    const selectedApp = this.extractAppFromProps();
 
     if (currentRoute !== this.extractRouteFromProps(prevProps)) {
       this.props.setRoute(currentRoute);
+    }
+
+    if (selectedApp !== this.extractAppFromProps(prevProps)) {
+      this.props.setSelectedApp(selectedApp);
     }
   }
 
   extractRouteFromProps(props: Properties = this.props) {
     return props.match.params.znsRoute;
+  }
+
+  extractAppFromProps(props: Properties = this.props) {
+    return props.match.params.app;
   }
   
   render() {

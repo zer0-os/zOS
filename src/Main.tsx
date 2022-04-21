8efd5e4d0a6ba3bc53fdccfe9384a79bb602e1c7
Joel Tulloch <joel@jtulloch.com>
2022-04-21 13:46:08 -0600

    pull main content back into main component

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,6 +3,8 @@ import { WalletManager } from './components/wallet-manager';
 import { ViewModeToggle } from './components/view-mode-toggle';
 import { ThemeEngine } from './components/theme-engine';
 import { AddressBarContainer } from './components/address-bar/container';
+import { ReactComponent as WilderWideLogo } from './assets/images/wilder-wide-logo.svg';
+import { AppMenuContainer } from './components/app-menu/container';
 
 import './main.scss';
 
@@ -14,8 +16,18 @@ export class Main extends React.Component {
       <div className='app-main'>
         <div className='container main'>
           <div className='container__left-sidebar'>
-            <div className='container__networks'>
+            <div className='container__navigation-world'>
               <ViewModeToggle className='main__view-mode-toggle' />
+            </div>
+            <div className='app-sandbox__navigation-platform'>
+              <div className='app-sandbox__navigation-content'>
+                <div className='container__network'>
+                  <WilderWideLogo className="logo-wilder-wide"/>
+                </div>
+                <div className='container__navigation'>
+                  <AppMenuContainer />
+                </div>
+              </div>
             </div>
           </div>
           <div className='container__content'>
--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ import { Router, Redirect, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ContextProvider as Web3ReactContextProvider } from './lib/web3/web3-react';
 import { config } from './config';
-import { ReactComponent as WilderWideLogo } from './assets/images/wilder-wide-logo.svg';
 
-import { AppMenuContainer } from './components/app-menu/container';
 import { AppSandboxContainer } from './app-sandbox/container';
 
 import '../node_modules/@zer0-os/zos-component-library/dist/index.css';
@@ -41,17 +39,6 @@ ReactDOM.render((
     <Router history={history}>
       <div className='container'>
         <div className='container__left-sidebar'>
-          <div className='container__networks'></div>
-          <div className='app-sandbox__navigation'>
-            <div className='app-sandbox__navigation-content'>
-              <div className='container__network'>
-                <WilderWideLogo className="logo-wilder-wide"/>
-              </div>
-              <div className='container__navigation'>
-                <AppMenuContainer store={store} />
-              </div>
-            </div>
-          </div>
         </div>
         <div className='container__content'>
           <AppSandboxContainer store={store} />

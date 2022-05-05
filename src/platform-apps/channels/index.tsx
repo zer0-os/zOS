4cce9e492918ccfb7b1104577dc261ba05f1d162
Joel Tulloch <joel@jtulloch.com>
2022-05-05 09:30:13 -0400

    add initial channels app components

---
+++ src/platform-apps/channels/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface Properties {
+}
+
+export class Channels extends React.Component<Properties> {
+  render() {
+    return <div className='channels' />;
+  }
+}

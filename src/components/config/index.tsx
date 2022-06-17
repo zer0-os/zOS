481050db084f61023a9d9490e66136179c21a68c
Joel Tulloch <joel@jtulloch.com>
2022-06-17 11:23:13 -0400

    add config injector

---
+++ src/components/config/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as config from '../../config';
+
+// should probably use context for this, but we need
+// to update our usage of context to match the current
+// api.
+export function inject<T>(ChildComponent: any) {
+  return class ConfigInjector extends React.Component<T> {
+    render() {
+      return <ChildComponent {...this.props} config={config} />;
+    }
+  };
+}

6abb1550af9d06c46f9f477ae12f0b98d2b2ddde
Joel Tulloch <joel@jtulloch.com>
2022-07-08 14:32:41 -0400

    adds normalized message state & channel.messages

--- src/store/channels/index.ts
+++ src/store/channels/index.ts
@@ -1,12 +1,18 @@
 import { createNormalizedSlice } from '../normalized';
 
+import { Message, schema as messageSchema } from '../messages';
+
 export interface Channel {
   id: string;
   name: string;
+  messages: Message[];
 }
 
 const slice = createNormalizedSlice({
   name: 'channels',
+  schemaDefinition: {
+    messages: [messageSchema],
+  },
 });
 
 export const { receiveNormalized } = slice.actions;
---
+++ src/store/messages.ts
@@ -0,0 +1,13 @@
+import { createNormalizedSlice } from './normalized';
+
+export interface Message {
+  id: string;
+  name: string;
+}
+
+const slice = createNormalizedSlice({
+  name: 'messages',
+});
+
+export const { receiveNormalized } = slice.actions;
+export const { normalize, denormalize, schema } = slice;

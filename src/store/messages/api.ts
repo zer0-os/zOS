24cc142835bb052c3a13e412a49d35e781c619f7
Joel Tulloch <joel@jtulloch.com>
2022-07-11 16:46:09 -0400

    add channel context to stub messages

--- src/store/messages/api.ts
+++ src/store/messages/api.ts
@@ -1,10 +1,10 @@
 export const api = {
-  async fetch(_id: string) {
+  async fetch(id: string) {
     return [
-      'this is a message all about stuff',
-      'this is a message all about tacos',
-      'this is a message all about cats',
-      'this is a message all about work',
+      `this is a message in channel #${id} all about stuff`,
+      `this is a message in channel #${id} all about tacos`,
+      `this is a message in channel #${id} all about cats`,
+      `this is a message in channel #${id} all about work`,
     ].map((message, index) => ({
       id: `message-id-${index}`,
       message,

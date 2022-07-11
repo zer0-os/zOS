4b52af467f4103b7c5a7e323d8e6105d13495d88
Joel Tulloch <joel@jtulloch.com>
2022-07-11 14:17:08 -0400

    message text is rendered

--- src/platform-apps/channels/message.tsx
+++ src/platform-apps/channels/message.tsx
@@ -3,6 +3,10 @@ import { Message as MessageModel } from '../../store/messages';
 
 export class Message extends React.Component<MessageModel> {
   render() {
-    return <div className='message'></div>;
+    return (
+      <div className='message'>
+        <div className='message__body'>{this.props.message}</div>
+      </div>
+    );
   }
 }

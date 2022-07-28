4644e813afa4bd8f9b7d2b73320f6aef17a66560
Hamza Khanouchi <khanouchi.hamza@gmail.com>
2022-07-28 14:51:36 +0100

    Style view images

--- src/platform-apps/channels/message.test.tsx
+++ src/platform-apps/channels/message.test.tsx
@@ -8,6 +8,7 @@ describe('message', () => {
   const subject = (props: any = {}) => {
     const allProps = {
       message: '',
+      media: { url: '', name: '', type: '' },
       ...props,
     };
 
@@ -22,6 +23,12 @@ describe('message', () => {
     expect(textes).toStrictEqual('the message');
   });
 
+  it('does not renders message text', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png' } });
+
+    expect(wrapper.find('.message__block-body').exists()).toBe(false);
+  });
+
   it('renders header date', () => {
     const wrapper = subject({ message: 'the message' });
 
@@ -32,5 +39,29 @@ describe('message', () => {
     const wrapper = subject({ message: 'the message' });
 
     expect(wrapper.find('.message__block-icon').exists()).toBe(true);
+  });
+
+  it('renders message image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image').exists()).toBe(true);
+  });
+
+  it('does not renders message text', () => {
+    const wrapper = subject({ message: 'the message' });
+
+    expect(wrapper.find('.message__block-image').exists()).toBe(false);
+  });
+
+  it('passes src prop to image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image img').prop('src')).toStrictEqual('https://image.com/image.png');
+  });
+
+  it('passes alt prop to image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', name: 'work', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image img').prop('alt')).toStrictEqual('work');
   });
 });

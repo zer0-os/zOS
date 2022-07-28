8ca1faeb541e91a7db6bad33040a544007661925
mrval5 <102802882+mrval5@users.noreply.github.com>
2022-07-28 10:19:17 +0100

    fetch channels (#129)
    Co-authored-by: mrval <kbenrafik@gmail.com>

--- src/store/messages/index.ts
+++ src/store/messages/index.ts
@@ -2,9 +2,30 @@ import { createAction } from '@reduxjs/toolkit';
 
 import { createNormalizedSlice } from '../normalized';
 
+interface Sender {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  profileImage: string;
+  profileId: string;
+}
+
+export interface MessagesResponse {
+  hasMore: boolean;
+  messages: Message[];
+}
+
 export interface Message {
   id: string;
   message: string;
+  createdAt: string;
+  updatedAt: string;
+  sender: Sender[];
+  // TODO: type to be defined
+  mentionedUsers: any;
+  hidePreview: boolean;
+  // TODO: type to be defined
+  preview: any;
 }
 
 export enum SagaActionTypes {
--- src/store/messages/saga.ts
+++ src/store/messages/saga.ts
@@ -2,12 +2,12 @@ import { takeLatest, put, call } from 'redux-saga/effects';
 import { SagaActionTypes } from '.';
 import { receive } from '../channels';
 
-import { api } from './api';
+import { fetchMessagesByChannelId } from './api';
 
 export function* fetch(action) {
   const channelId = action.payload;
 
-  const messages = yield call(api.fetch, channelId);
+  const messages = yield call(fetchMessagesByChannelId, channelId);
 
   yield put(receive({ id: channelId, messages }));
 }

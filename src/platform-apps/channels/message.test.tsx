dbc7ff381e20714a3b812b305b82d211112007c4
hamza khanouchi <khanouchi.hamza@gmail.com>
2022-08-02 23:00:22 +0100

    Style view images (#131)

--- src/platform-apps/channels/message.test.tsx
+++ src/platform-apps/channels/message.test.tsx
@@ -8,6 +8,7 @@ describe('message', () => {
   const subject = (props: any = {}) => {
     const allProps = {
       message: '',
+      media: { url: '', name: '', type: '' },
       ...props,
     };
 
@@ -20,6 +21,12 @@ describe('message', () => {
     const textes = wrapper.find('.message__block-body').text().trim();
 
     expect(textes).toStrictEqual('the message');
+  });
+
+  it('does not renders message text', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png' } });
+
+    expect(wrapper.find('.message__block-body').exists()).toBe(false);
   });
 
   it('renders header date', () => {
@@ -44,5 +51,29 @@ describe('message', () => {
     const wrapper = subject({ media: { url: 'https://image.com/video.mp4', type: 'video' } });
 
     expect(wrapper.find('.message__block-video video source').prop('src')).toStrictEqual('https://image.com/video.mp4');
+  });
+
+  it('renders message image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image').exists()).toBe(true);
+  });
+
+  it('does not renders message text', () => {
+    const wrapper = subject({ message: 'the message' });
+
+    expect(wrapper.find('.message__block-image').exists()).toBe(false);
+  });
+
+  it('passes src prop to image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image img').prop('src')).toStrictEqual('https://image.com/image.png');
+  });
+
+  it('passes alt prop to image', () => {
+    const wrapper = subject({ media: { url: 'https://image.com/image.png', name: 'work', type: 'image' } });
+
+    expect(wrapper.find('.message__block-image img').prop('alt')).toStrictEqual('work');
   });
 });

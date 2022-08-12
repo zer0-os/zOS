6c583e18db0a0c8ee654225e07b1fd75f7d72822
mrval5 <102802882+mrval5@users.noreply.github.com>
2022-08-12 15:11:39 +0100

    add emojis (#138)
    Co-authored-by: mrval <kbenrafik@gmail.com>

--- src/platform-apps/channels/message.tsx
+++ src/platform-apps/channels/message.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
 import { Message as MessageModel } from '../../store/messages';
+import { textToEmojis } from './utils';
 
 interface Properties extends MessageModel {
   className: string;
@@ -39,13 +40,26 @@ export class Message extends React.Component<Properties> {
     return <div className='message__time'>{createdTime}</div>;
   }
 
+  renderMessage() {
+    const parts = this.props.message.split(/(@\[.*?\]\([a-z]+:[A-Za-z0-9_-]+\))/gi);
+    return parts.map((part) => {
+      const match = part.match(/@\[(.*?)\]\(([a-z]+):([A-Za-z0-9_-]+)\)/i);
+
+      if (!match) {
+        return textToEmojis(part);
+      }
+
+      return part;
+    });
+  }
+
   render() {
     return (
       <div className={classNames('message', this.props.className)}>
         <div className='message__block'>
           <div className='message__block-icon'></div>
           {this.props.media && this.renderImage()}
-          {this.props.message && <div className='message__block-body'>{this.props.message}</div>}
+          {this.props.message && <div className='message__block-body'>{this.renderMessage()}</div>}
           {this.renderTime()}
         </div>
       </div>

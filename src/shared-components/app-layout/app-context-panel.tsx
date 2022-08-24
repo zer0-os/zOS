e5a2f484273e1bb569bc7ecdbc6baff318b7ea05
Joel Tulloch <joel@jtulloch.com>
2022-08-24 10:58:32 -0400

    remove app layout components

--- src/shared-components/app-layout/app-context-panel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class AppContextPanel extends React.Component {
-  render() {
-    return <div className='app-context-panel'>{this.props.children}</div>;
-  }
-}
--- src/shared-components/app-layout/index.tsx
+++
@@ -1,3 +0,0 @@
-export { AppLayout } from './app-layout';
-export { AppContextPanel } from './app-context-panel';
-export { AppContent } from './app-content';

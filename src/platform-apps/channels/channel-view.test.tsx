09d62ce8ca6c5d97d86003c4695a4acd0de796a1
Hamza Khanouchi <khanouchi.hamza@gmail.com>
2022-08-25 14:15:50 +0100

    Add indicator when new messages arrive in real time

--- src/platform-apps/channels/channel-view.test.tsx
+++ src/platform-apps/channels/channel-view.test.tsx
@@ -18,6 +18,7 @@ describe('ChannelView', () => {
     const allProps = {
       name: '',
       messages: [],
+      hasNewMessage: 0,
       ...props,
     };
 
@@ -115,5 +116,26 @@ describe('ChannelView', () => {
     const wrapper = subject({ messages: [] });
 
     expect(wrapper.find(Waypoint).exists()).toBe(false);
+  });
+
+  it('renders indicator when new messages arrive', () => {
+    const wrapper = subject({ hasNewMessage: 0 });
+
+    expect(wrapper.find('.channel-view__newMessage').exists()).toBe(false);
+
+    wrapper.setProps({ hasNewMessage: 2 });
+
+    expect(wrapper.find('.channel-view__newMessage').exists()).toBe(true);
+  });
+
+  it('it should not renders indicator when click on it', () => {
+    const closeIndicatorSpy = jest.fn();
+    const wrapper = subject({ hasNewMessage: 3, closeIndicator: closeIndicatorSpy });
+
+    expect(wrapper.find('.channel-view__newMessage').exists()).toBe(true);
+
+    wrapper.find('.channel-view__newMessage-bar').simulate('click');
+
+    expect(wrapper.prop('scrollToBottom')).toHaveBeenCalled;
   });
 });
--- src/platform-apps/channels/channel-view.tsx
+++ src/platform-apps/channels/channel-view.tsx
@@ -13,9 +13,17 @@ export interface Properties {
   name: string;
   messages: MessageModel[];
   onFetchMore: () => void;
+  closeIndicator: () => void;
+  hasNewMessage: number;
 }
 
 export class ChannelView extends React.Component<Properties> {
+  bottomRef;
+  constructor(props) {
+    super(props);
+    this.bottomRef = React.createRef();
+  }
+
   getMessagesByDay() {
     return this.props.messages.reduce((prev, current) => {
       const createdAt = moment(current.createdAt);
@@ -40,6 +48,11 @@ export class ChannelView extends React.Component<Properties> {
       sameElse: 'MMM D, YYYY',
     });
   }
+
+  scrollToBottom = () => {
+    this.props.closeIndicator();
+    this.bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   renderDay(day: string, messagesByDay: ChatMessageGroups) {
     const allMessages = messagesByDay[day];
@@ -79,13 +92,47 @@ export class ChannelView extends React.Component<Properties> {
   render() {
     return (
       <div className='channel-view'>
+        {this.props.hasNewMessage > 0 && (
+          <div className='channel-view__newMessage'>
+            <button
+              type='button'
+              className='channel-view__newMessage-bar'
+              aria-label='Jump to last unread message'
+              onClick={this.scrollToBottom}
+            >
+              <span className='channel-view__newMessage-bar-text'>{this.props.hasNewMessage} new messages</span>
+            </button>
+            <button
+              type='button'
+              className='channel-view__newMessage-alt'
+              onClick={this.scrollToBottom}
+            >
+              Mark As Read
+              <svg
+                aria-hidden='true'
+                role='img'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+              >
+                <path
+                  fill='currentColor'
+                  fill-rule='evenodd'
+                  clip-rule='evenodd'
+                  d='M12.291 5.70697L15.998 9.41397L21.705 3.70697L20.291 2.29297L15.998 6.58597L13.705 4.29297L12.291 5.70697ZM1.99805 7H11.088C11.564 9.837 14.025 12 16.998 12V18C16.998 19.103 16.102 20 14.998 20H8.33205L2.99805 24V20H1.99805C0.894047 20 -0.00195312 19.103 -0.00195312 18V9C-0.00195312 7.897 0.894047 7 1.99805 7Z'
+                ></path>
+              </svg>
+            </button>
+          </div>
+        )}
         <InvertedScroll className='channel-view__inverted-scroll'>
           <div className='channel-view__name'>
             <h1>Welcome to #{this.props.name}</h1>
             <span>This is the start of the channel.</span>
           </div>
-          {this.props.messages.length > 0 && <Waypoint onEnter={this.props.onFetchMore} />}
-          {this.props.messages.length > 0 && this.renderMessages()}
+          {this.props.messages.length && <Waypoint onEnter={this.props.onFetchMore} />}
+          {this.props.messages.length && this.renderMessages()}
+          <div ref={this.bottomRef} />
         </InvertedScroll>
       </div>
     );

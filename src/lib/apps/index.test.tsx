9b8850d5937a0d10646517927512b33250fe85ad
mrval <kbenrafik@gmail.com>
2022-08-29 09:08:57 +0200

    remove channels feature flag

--- src/lib/apps/index.test.tsx
+++ src/lib/apps/index.test.tsx
@@ -1,18 +1,7 @@
 import { Apps, apps, allApps } from '.';
-import { featureFlags } from '../feature-flags';
-
-jest.mock('../feature-flags');
 
 describe('apps', () => {
-  it('defaults to feed app', () => {
-    expect(allApps()).toStrictEqual([
-      apps[Apps.Feed],
-    ]);
-  });
-
-  it('includes Channels when channelsApp feature flag is true', () => {
-    featureFlags.channelsApp = true;
-
+  it('defaults to feed, Channels app', () => {
     expect(allApps()).toStrictEqual([
       apps[Apps.Feed],
       apps[Apps.Channels],

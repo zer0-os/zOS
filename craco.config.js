e08ad73390b0868c590ae77f9c972f9447cb1558
Brett <12437916+colbr@users.noreply.github.com>
2022-08-31 14:37:00 +1200

    build(craco): configure for linked ES6 modules

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,21 @@
 const path = require('path');
+const fs = require('fs');
+const appDirectory = fs.realpathSync(process.cwd());
+
+/**
+ * Converts a relative path to an absolute path.
+ * e.g. '../zApp-Staking' -> '/users/zero/zApp-Staking'
+ */
+const resolveApp = (relativePath) => {
+  path.resolve(appDirectory, relativePath);
+};
+
+/**
+ * Maps a list of relative app locations to absolute paths
+ */
+const resolveApps = (appLocations) => {
+  return appLocations.map((app) => resolveApp(app));
+};
 
 module.exports = {
   webpack: {
@@ -9,6 +26,18 @@ module.exports = {
       'react-router-dom': path.resolve('./node_modules/react-router-dom'),
     },
   },
+  plugins: [
+    {
+      /* Runs babel-loader on arbitrary folders, enabling us to use ES6
+       * syntax in linked zApps */
+      plugin: require('craco-babel-loader'),
+      options: {
+        // Relative locations of linked packages e.g. '../zApp-BuyDomains', '../zApp-Staking'
+        // Note: do not commit any changes to this array
+        includes: resolveApps([]),
+      },
+    },
+  ],
   devServer: {
     watchOptions: {
       ignored: '**/node_modules/**',

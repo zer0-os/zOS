5e623fc9e4a81abadcde93d9281b1bf94039028e
mrval <kbenrafik@gmail.com>
2022-09-02 11:43:09 +0200

    send filter as json

--- src/store/messages/api.ts
+++ src/store/messages/api.ts
@@ -5,7 +5,9 @@ import { config } from '../../config';
 export async function fetchMessagesByChannelId(channelId: string, lastCreatedAt?: number): Promise<MessagesResponse> {
   const filter = lastCreatedAt ? { lastCreatedAt } : {};
 
-  const response = await Request.get(`${config.ZERO_API_URL}/chatChannels/${channelId}/messages`).query({ filter });
+  const response = await Request.get(`${config.ZERO_API_URL}/chatChannels/${channelId}/messages`).query({
+    filter: JSON.stringify(filter),
+  });
 
   return response.body;
 }

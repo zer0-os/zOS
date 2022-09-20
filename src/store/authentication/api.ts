8f66296c04de450cd7ae8683069da176dafbd14a
mrval <kbenrafik@gmail.com>
2022-09-20 15:59:38 +0200

    fix cookies

--- src/store/authentication/api.ts
+++ src/store/authentication/api.ts
@@ -5,10 +5,9 @@ import { config } from '../../config';
 interface AuthorizationResponse {}
 
 export async function authorize(signedWeb3Token: string): Promise<AuthorizationResponse> {
-  const response = await Request.post(`${config.ZERO_API_URL}/authentication/authorize`).set(
-    'Authorization',
-    `Web3 ${signedWeb3Token}`
-  );
+  const response = await Request.post(`${config.ZERO_API_URL}/authentication/authorize`)
+    .withCredentials()
+    .set('Authorization', `Web3 ${signedWeb3Token}`);
 
   return response.body;
 }

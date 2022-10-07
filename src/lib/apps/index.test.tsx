7817f124f90ca4b2231ac3353d4ceaa56dc706e6
eric-zero-dev <eric.low.dev@gmail.com>
2022-10-07 12:34:10 +0100

    fix: test

--- src/lib/apps/index.test.tsx
+++ src/lib/apps/index.test.tsx
@@ -1,12 +1,13 @@
 import { Apps, apps, allApps } from '.';
 
 describe('apps', () => {
-  it('defaults to apps: Feed, Staking, Channels, DAOS', () => {
+  it('defaults to apps: Feed, Staking, Channels, DAOS, BuyDomains', () => {
     expect(allApps()).toStrictEqual([
       apps[Apps.Feed],
       apps[Apps.Staking],
       apps[Apps.Channels],
       apps[Apps.DAOS],
+      apps[Apps.BuyDomains],
     ]);
   });
 });

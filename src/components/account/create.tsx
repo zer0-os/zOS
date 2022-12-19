f02350a05038512427b75bf7600f028a60febf17
mrval5 <102802882+mrval5@users.noreply.github.com>
2022-12-19 15:26:32 +0100

    fix console warning (#269)
    Co-authored-by: mrval <kbenrafik@gmail.com>

--- src/components/account/create.tsx
+++ src/components/account/create.tsx
@@ -183,8 +183,8 @@ export class Container extends React.Component<Properties, State> {
             <path
               d='M8.00004 5.33333V8M8.00004 10.6667H8.00671M14.6667 8C14.6667 11.6819 11.6819 14.6667 8.00004 14.6667C4.31814 14.6667 1.33337 11.6819 1.33337 8C1.33337 4.3181 4.31814 1.33333 8.00004 1.33333C11.6819 1.33333 14.6667 4.3181 14.6667 8Z'
               stroke='#FF6369'
-              stroke-linecap='round'
-              stroke-linejoin='round'
+              strokeLinecap='round'
+              strokeLinejoin='round'
             />
           </g>
           <defs>

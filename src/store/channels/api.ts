10502fe9fc75ec53534c788cfae935d96dc01f69
Ratik Jindal <ratikjindal21@gmail.com>
2022-12-30 04:19:34 +0530

    feat: add markAllMessagesAsReadInChannel API

--- src/store/channels/api.ts
+++ src/store/channels/api.ts
@@ -1,4 +1,4 @@
-import { get, post } from '../../lib/api/rest';
+import { get, post, put } from '../../lib/api/rest';
 
 export async function fetchUsersByChannelId(channelId: string): Promise<any> {
   return await get<any>(`/chatChannels/${channelId}/members`)
@@ -12,6 +12,12 @@ export async function fetchUsersByChannelId(channelId: string): Promise<any> {
 
 export async function joinChannel(channelId: string): Promise<number> {
   const response = await post<any>(`/chatChannels/${channelId}/join`);
+
+  return response.status;
+}
+
+export async function markAllMessagesAsReadInChannel(channelId: string, userId: string) {
+  const response = await put<any>(`/chatChannels/${channelId}/messages/mark-as-read`).send({ id: userId });
 
   return response.status;
 }

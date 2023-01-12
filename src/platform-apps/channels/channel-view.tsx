9b55b4b33353fbbba1b405a3bfdaed3ebdcec26e
Dale Fukami <dale@zero.tech>
2023-01-12 13:43:50 -0700

    Add conventional right padding to Channel app

--- src/platform-apps/channels/channel-view.tsx
+++ src/platform-apps/channels/channel-view.tsx
@@ -186,27 +186,29 @@ export class ChannelView extends React.Component<Properties, State> {
           />
         )}
         <InvertedScroll className='channel-view__inverted-scroll'>
-          <div className='channel-view__name'>
-            <h1>Welcome to #{this.props.name}</h1>
-            <span>This is the start of the channel.</span>
+          <div className='channel-view__main'>
+            <div className='channel-view__name'>
+              <h1>Welcome to #{this.props.name}</h1>
+              <span>This is the start of the channel.</span>
+            </div>
+            {this.props.messages.length > 0 && <Waypoint onEnter={this.props.onFetchMore} />}
+            {this.props.messages.length > 0 && this.renderMessages()}
+            <IfAuthenticated showChildren>
+              {isMemberOfChannel && (
+                <MessageInput
+                  onMessageInputRendered={this.props.onMessageInputRendered}
+                  placeholder='Speak your truth...'
+                  onSubmit={this.props.sendMessage}
+                  users={this.props.users}
+                />
+              )}
+              {!isMemberOfChannel && this.renderJoinButton()}
+            </IfAuthenticated>
+            <IfAuthenticated hideChildren>
+              <ConnectButton className='authentication__connect-wrapper--with-space' />
+            </IfAuthenticated>
+            <div ref={this.bottomRef} />
           </div>
-          {this.props.messages.length > 0 && <Waypoint onEnter={this.props.onFetchMore} />}
-          {this.props.messages.length > 0 && this.renderMessages()}
-          <IfAuthenticated showChildren>
-            {isMemberOfChannel && (
-              <MessageInput
-                onMessageInputRendered={this.props.onMessageInputRendered}
-                placeholder='Speak your truth...'
-                onSubmit={this.props.sendMessage}
-                users={this.props.users}
-              />
-            )}
-            {!isMemberOfChannel && this.renderJoinButton()}
-          </IfAuthenticated>
-          <IfAuthenticated hideChildren>
-            <ConnectButton className='authentication__connect-wrapper--with-space' />
-          </IfAuthenticated>
-          <div ref={this.bottomRef} />
         </InvertedScroll>
       </div>
     );

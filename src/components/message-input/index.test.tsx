490f56e6b22a01734e4ae49c8ccdba6b2188e4f4
Dale Fukami <dale@zero.tech>
2023-01-17 10:04:43 -0700

    Fix some lint errors in tests

--- src/components/message-input/index.test.tsx
+++ src/components/message-input/index.test.tsx
@@ -1,10 +1,6 @@
-/**
- * @jest-environment jsdom
- */
-
 import React from 'react';
 import { MentionsInput } from 'react-mentions';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { MessageInput, Properties } from '.';
 import { Key } from '../../lib/keyboard-search';
@@ -29,19 +25,6 @@ describe('MessageInput', () => {
     };
 
     return shallow(<MessageInput {...allProps}>{child}</MessageInput>);
-  };
-  const subjectMount = (props: Partial<Properties>, child: any = <div />) => {
-    const allProps: Properties = {
-      className: '',
-      placeholder: '',
-      users: [],
-      onSubmit: () => undefined,
-      getUsersForMentions: () => undefined,
-      onMessageInputRendered: () => undefined,
-      ...props,
-    };
-
-    return mount(<MessageInput {...allProps}>{child}</MessageInput>);
   };
 
   it('adds className', () => {

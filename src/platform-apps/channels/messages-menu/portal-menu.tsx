518eea728ecf48d539f450ab8888e0203ee41de9
Dale Fukami <dale@zero.tech>
2023-02-08 14:39:38 -0700

    PortalMenu now uses a portal

--- src/platform-apps/channels/messages-menu/portal-menu.tsx
+++ src/platform-apps/channels/messages-menu/portal-menu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 import classNames from 'classnames';
 
 export interface Properties {
@@ -7,20 +8,21 @@ export interface Properties {
   onClose: () => void;
 }
 
-interface State {
-  isMenuOpen: boolean;
-}
-
-export default class PortalMenu extends React.Component<Properties, State> {
-  state = { isMenuOpen: false };
-
+export default class PortalMenu extends React.Component<Properties> {
   close = () => this.props.onClose();
+  ref: HTMLDivElement = null;
 
-  render() {
+  renderMenu() {
+    if (!this.ref) {
+      return null;
+    }
+
+    const { x, y } = this.ref.getBoundingClientRect();
     return (
       <div
-        className={classNames(this.props.className, { active: this.props.isOpen })}
+        className={classNames(this.props.className)}
         onClick={this.close}
+        style={{ top: y, left: x }}
       >
         <div className='portal-menu__content'>
           <ul>{this.props.children}</ul>
@@ -30,6 +32,19 @@ export default class PortalMenu extends React.Component<Properties, State> {
           onClick={this.close}
         />
       </div>
+    );
+  }
+
+  setPositionRef = (el) => {
+    this.ref = el;
+  };
+
+  render() {
+    return (
+      <>
+        <div ref={this.setPositionRef}></div>
+        {this.props.isOpen && createPortal(this.renderMenu(), document.body)}
+      </>
     );
   }
 }

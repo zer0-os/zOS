db0f75785994b0717b8715d15fca2466d8b6098a
Dale Fukami <dale@zero.tech>
2023-03-15 12:01:40 -0600

    Fix notifications request

--- src/store/notifications/api.ts
+++ src/store/notifications/api.ts
@@ -1,10 +1,6 @@
 import { get } from '../../lib/api/rest';
 
 export async function fetchNotifications(userId: string) {
-  console.log('heh', userId);
-  const response = await get<any>('/api/notifications/filter', {
-    limit: 15,
-    userId,
-  });
+  const response = await get<any>('/api/notifications/filter', { limit: 15 }, { userId });
   return response.body;
 }

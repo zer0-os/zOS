8f03eea6004d8ddba1ae047278a7284d4744b724
Adam Kahtava <adam@kahtava.com>
2023-03-16 17:22:12 -0400

    Remove fetchChatAccessToken and getChatAccessToken as this is done during initial authentication

--- src/store/chat/saga.ts
+++ src/store/chat/saga.ts
@@ -1,23 +1,10 @@
-import { takeLatest, put, call } from 'redux-saga/effects';
-import { SagaActionTypes, setChatAccessToken, setReconnecting } from '.';
-import { fetchChatAccessToken } from './api';
+import { takeLatest, put } from 'redux-saga/effects';
+import { SagaActionTypes, setReconnecting } from '.';
 
-export function* getChatAccessToken() {
-  yield put(setChatAccessToken({ value: '', isLoading: true }));
-  const response = yield call(fetchChatAccessToken);
-
-  yield put(
-    setChatAccessToken({
-      value: response.chatAccessToken,
-      isLoading: false,
-    })
-  );
-}
 export function* receiveIsReconnecting(action) {
   yield put(setReconnecting(action.payload));
 }
 
 export function* saga() {
-  yield takeLatest(SagaActionTypes.FetchChatAccessToken, getChatAccessToken);
   yield takeLatest(SagaActionTypes.ReceiveIsReconnecting, receiveIsReconnecting);
 }

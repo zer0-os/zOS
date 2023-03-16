ff45a8e803e9686320ad4ba673938cee1992114c
Dale Fukami <dalefukami@users.noreply.github.com>
2023-03-16 06:34:45 -0600

    Empty Notifications design (#403)

--- src/components/notification/list/index.tsx
+++ src/components/notification/list/index.tsx
@@ -1,3 +1,4 @@
+import { IconBellRinging1 } from '@zero-tech/zui/icons';
 import React from 'react';
 
 import { NotificationItem } from '../item';
@@ -20,17 +21,26 @@ export class NotificationList extends React.Component<Properties> {
   render() {
     return (
       <div className='notification-list__wrapper'>
-        <div className='notification-list__items'>
-          {this.props.list.map((n) => (
-            <NotificationItem
-              key={n.id}
-              body={n.body}
-              createdAt={n.createdAt}
-              originatingName={n.originatingName}
-              originatingImageUrl={n.originatingImageUrl}
-            />
-          ))}
-        </div>
+        {this.props.list.length > 0 && (
+          <div className='notification-list__items'>
+            {this.props.list.map((n) => (
+              <NotificationItem
+                key={n.id}
+                body={n.body}
+                createdAt={n.createdAt}
+                originatingName={n.originatingName}
+                originatingImageUrl={n.originatingImageUrl}
+              />
+            ))}
+          </div>
+        )}
+        {this.props.list.length <= 0 && (
+          <div className='notification-empty-list'>
+            <IconBellRinging1 size='56' />
+            <div className='notification-empty-list__title'>Clear.</div>
+            <div>You'll be notified about important things here.</div>
+          </div>
+        )}
       </div>
     );
   }

e2e85509f0fd9270cdbb15c3d1fee084e40b844e
Dale Fukami <dalefukami@users.noreply.github.com>
2023-03-20 06:10:39 -0600

    Toggle the conversation list with the UserAction (#418)

--- src/components/sidekick/index.tsx
+++ src/components/sidekick/index.tsx
@@ -2,19 +2,9 @@ import React from 'react';
 import { RootState } from '../../store';
 import { connectContainer } from '../../store/redux-container';
 import { IfAuthenticated } from '../authentication/if-authenticated';
-import { IconButton, Icons } from '@zer0-os/zos-component-library';
 import classNames from 'classnames';
-import { AuthenticationState } from '../../store/authentication/types';
-import {
-  UpdateSidekickPayload,
-  updateSidekick,
-  setActiveSidekickTab,
-  SetActiveSidekickTabPayload,
-  syncSidekickState,
-} from '../../store/layout';
+import { syncSidekickState } from '../../store/layout';
 import { MessengerList } from '../messenger/list';
-import { denormalize } from '../../store/channels';
-import { SidekickTabs as Tabs } from './types';
 
 import './styles.scss';
 
@@ -23,48 +13,23 @@ interface PublicProperties {
 }
 
 export interface Properties extends PublicProperties {
-  user: AuthenticationState['user'];
-  updateSidekick: (action: UpdateSidekickPayload) => void;
-  setActiveSidekickTab: (action: SetActiveSidekickTabPayload) => void;
   syncSidekickState: () => void;
-  countAllUnreadMessages: number;
   isOpen: boolean;
-  activeTab: Tabs;
 }
 
-export interface State {
-  canStartAnimation: boolean;
-}
-
-export class Container extends React.Component<Properties, State> {
-  state = { canStartAnimation: false };
-  defaultProps: {
-    activeTab: Tabs.MESSAGES;
-  };
-
+export class Container extends React.Component<Properties> {
   static mapState(state: RootState): Partial<Properties> {
-    const directMessages = denormalize(state.channelsList.value, state).filter((channel) => Boolean(channel.isChannel));
-
-    const countAllUnreadMessages = directMessages.reduce(
-      (count, directMessage) => count + directMessage.unreadCount,
-      0
-    );
-
     const {
-      authentication: { user },
       layout: { value },
     } = state;
 
     return {
-      user,
-      countAllUnreadMessages,
       isOpen: value.isSidekickOpen,
-      activeTab: value.activeSidekickTab,
     };
   }
 
   static mapActions(_props: Properties): Partial<Properties> {
-    return { updateSidekick, setActiveSidekickTab, syncSidekickState };
+    return { syncSidekickState };
   }
 
   componentDidMount() {
@@ -75,102 +40,18 @@ export class Container extends React.Component<Properties, State> {
     return this.props.isOpen;
   }
 
-  clickTab(tab: Tabs): void {
-    this.props.setActiveSidekickTab({
-      activeTab: tab,
-    });
-  }
-
-  toggleSidekickPanel = (): void => {
-    this.setState({ canStartAnimation: true });
-    this.props.updateSidekick({ isOpen: !this.isOpen });
-  };
-
-  renderSidekickPanel(): JSX.Element {
-    return (
-      <div
-        className='app-sidekick-panel__target'
-        onClick={this.toggleSidekickPanel}
-      >
-        <svg
-          className='sidekick-panel-tab__tab'
-          viewBox='0 0 16 104'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <path d='M4.03079 89.9538L11.9692 78.0462C14.5975 74.1038 16 69.4716 16 64.7334V51.5V39.2666C16 34.5284 14.5975 29.8962 11.9692 25.9538L4.03079 14.0462C1.40251 10.1038 0 5.47158 0 0.733383V51.5V103.267C0 98.5284 1.40251 93.8962 4.03079 89.9538Z' />
-        </svg>
-        <div className='sidekick-panel-tab__icon'>
-          <span className='sidekick-panel-tab__icon-item' />
-          <span className='sidekick-panel-tab__icon-item' />
-          <span className='sidekick-panel-tab__icon-item' />
-        </div>
-      </div>
-    );
-  }
-
-  renderMessageTab() {
-    if (this.props.countAllUnreadMessages > 0) {
-      return (
-        <div
-          className='sidekick__tab-notifications sidekick__tab-notifications--unread-messages'
-          onClick={this.clickTab.bind(this, Tabs.MESSAGES)}
-        >
-          {this.props.countAllUnreadMessages}
-        </div>
-      );
-    } else {
-      return (
-        <IconButton
-          className='sidekick__tabs-messages'
-          icon={Icons.Messages}
-          onClick={this.clickTab.bind(this, Tabs.MESSAGES)}
-        />
-      );
-    }
-  }
-
-  renderTabs(): JSX.Element {
-    return (
-      <div className='sidekick__tabs'>
-        <IconButton
-          className='sidekick__tabs-network'
-          icon={Icons.Network}
-          onClick={this.clickTab.bind(this, Tabs.NETWORK)}
-        />
-        {this.renderMessageTab()}
-      </div>
-    );
-  }
-
   renderTabContent(): JSX.Element {
-    switch (this.props.activeTab) {
-      case Tabs.NETWORK:
-        return <div className='sidekick__tab-content--network'>NETWORK</div>;
-      case Tabs.MESSAGES:
-        return (
-          <div className='sidekick__tab-content--messages'>
-            <MessengerList />
-          </div>
-        );
-      default:
-        return null;
-    }
+    return (
+      <div className='sidekick__tab-content--messages'>
+        <MessengerList />
+      </div>
+    );
   }
 
   render() {
     return (
       <IfAuthenticated showChildren>
-        <div
-          className={classNames(
-            'sidekick',
-            this.props.className,
-            { 'sidekick--slide-out': this.state.canStartAnimation && !this.isOpen },
-            { 'sidekick--slide-in': this.isOpen }
-          )}
-        >
-          {this.renderSidekickPanel()}
-          <div className='sidekick-panel'>{this.renderTabs()}</div>
+        <div className={classNames('sidekick', this.props.className, { 'sidekick--open': this.isOpen })}>
           <div className='sidekick__tab-content'>{this.renderTabContent()}</div>
         </div>
       </IfAuthenticated>
--- src/components/user-actions/index.test.tsx
+++ src/components/user-actions/index.test.tsx
@@ -8,6 +8,8 @@ describe('UserActions', () => {
     const allProps = {
       userImageUrl: '',
       userIsOnline: false,
+      isConversationListOpen: false,
+      updateConversationState: (_) => undefined,
       ...props,
     };
 
@@ -48,14 +50,52 @@ describe('UserActions', () => {
   it('opens and closes the notification list', () => {
     const wrapper = subject();
 
-    wrapper.find('button').simulate('click');
+    notificationButton(wrapper).simulate('click');
 
     expect(wrapper.find('IconBell1').prop('isFilled')).toBeTrue();
     expect(wrapper.find('NotificationPopup').exists()).toBeTrue();
 
-    wrapper.find('button').simulate('click');
+    notificationButton(wrapper).simulate('click');
 
     expect(wrapper.find('IconBell1').prop('isFilled')).toBeFalse();
     expect(wrapper.find('NotificationPopup').exists()).toBeFalse();
   });
+
+  it('opens the conversation list', () => {
+    const updateConversationState = jest.fn();
+    const wrapper = subject({ updateConversationState, isConversationListOpen: false });
+
+    conversationButton(wrapper).simulate('click');
+
+    expect(updateConversationState).toBeCalledWith(true);
+  });
+
+  it('closes the conversation list', () => {
+    const updateConversationState = jest.fn();
+    const wrapper = subject({ updateConversationState, isConversationListOpen: true });
+
+    conversationButton(wrapper).simulate('click');
+
+    expect(updateConversationState).toBeCalledWith(false);
+  });
+
+  it('renders conversation list closed', () => {
+    const wrapper = subject({ isConversationListOpen: false });
+
+    expect(wrapper.find('IconMessageSquare2').prop('isFilled')).toBeFalse();
+  });
+
+  it('renders conversation list open', () => {
+    const wrapper = subject({ isConversationListOpen: true });
+
+    expect(wrapper.find('IconMessageSquare2').prop('isFilled')).toBeTrue();
+  });
 });
+
+function notificationButton(wrapper) {
+  return wrapper.find('button').at(1);
+}
+
+function conversationButton(wrapper) {
+  return wrapper.find('button').at(0);
+}

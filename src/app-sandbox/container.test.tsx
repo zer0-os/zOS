7c2eacb7b42fd65ed556f26418f2b84571ee12d3
Adam K <adam@kahtava.com>
2023-03-27 16:16:40 -0400

    Resolve: 'Warning: Setting defaultProps...' warning (#438)
    * Resolve: 'Warning: Setting defaultProps as an instance property on Container is not supported and will be ignored. Instead, define defaultProps as a static property on Container.'
    
    * Remove document reference and untested background color reset code

--- src/app-sandbox/container.test.tsx
+++ src/app-sandbox/container.test.tsx
@@ -25,7 +25,6 @@ describe('AppSandboxContainer', () => {
       layout: {} as AppLayout,
       updateLayout: () => undefined,
       isAuthenticated: false,
-      document: null,
       ...props,
     };
 

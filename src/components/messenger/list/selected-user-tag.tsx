b87c88779077be7917c3e41d7a8d82b7efb67f0a
Dale Fukami <dale@zero.tech>
2023-04-03 11:33:49 -0600

    Extract SelectedUserTag

---
+++ src/components/messenger/list/selected-user-tag.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+import { Option } from '../autocomplete-members';
+
+import { Avatar } from '@zero-tech/zui/components';
+import { IconXClose } from '@zero-tech/zui/icons';
+
+export interface Properties {
+  userOption: Option;
+
+  onRemove: (id: string) => void;
+}
+
+export class SelectedUserTag extends React.Component<Properties> {
+  publishRemove = () => {
+    this.props.onRemove(this.props.userOption.value);
+  };
+
+  render() {
+    const { userOption: option } = this.props;
+
+    return (
+      <div className='start-group-panel__selected-option' key={option.value}>
+        <div className='start-group-panel__selected-tag'>
+          <Avatar size={'extra small'} type={'circle'} imageURL={option.image} />
+          <span className='start-group-panel__user-label'>{option.label}</span>
+          <button onClick={this.publishRemove} data-value={option.value} className='start-group-panel__user-remove'>
+            <IconXClose size={16} />
+          </button>
+        </div>
+      </div>
+    );
+  }
+}

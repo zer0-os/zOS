b4e3475049e6b8692ed86883d0a9c7dd4abb8d58
Dale Fukami <dale@zero.tech>
2023-04-05 11:55:05 -0600

    Fix test

--- src/components/messenger/list/selected-user-tag.test.tsx
+++ src/components/messenger/list/selected-user-tag.test.tsx
@@ -22,7 +22,7 @@ describe('SelectedUserTag', () => {
 
     const wrapper = subject({ userOption });
 
-    expect(wrapper.find('.start-group-panel__user-label').text()).toEqual('User 1');
+    expect(wrapper.find('.selected-user-tag__user-label').text()).toEqual('User 1');
     expect(wrapper.find('Avatar').prop('imageURL')).toEqual('url-1');
   });
 

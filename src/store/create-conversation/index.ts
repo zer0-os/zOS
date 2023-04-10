55643a243ad883dd747c0429e441812a098be914
Dale Fukami <dale@zero.tech>
2023-04-10 10:47:21 -0600

    Push selecting members for group chat into saga

--- src/store/create-conversation/index.ts
+++ src/store/create-conversation/index.ts
@@ -1,11 +1,13 @@
 import { PayloadAction, createAction, createSlice } from '@reduxjs/toolkit';
 import { CreateMessengerConversation } from '../channels-list/types';
+import { MembersSelectedPayload } from './types';
 
 export enum SagaActionTypes {
   Start = 'create-conversation/start',
   Forward = 'create-conversation/forward',
   Back = 'create-conversation/back',
   Reset = 'create-conversation/reset',
+  MembersSelected = 'create-conversation/members-selected',
   CreateConversation = 'create-conversation/create',
 }
 
@@ -14,6 +16,7 @@ export const startCreateConversation = createAction(SagaActionTypes.Start);
 export const forward = createAction(SagaActionTypes.Forward);
 export const back = createAction(SagaActionTypes.Back);
 export const reset = createAction(SagaActionTypes.Reset);
+export const membersSelected = createAction<MembersSelectedPayload>(SagaActionTypes.MembersSelected);
 export const createConversation = createAction<CreateMessengerConversation>(SagaActionTypes.CreateConversation);
 
 export enum Stage {
@@ -26,6 +29,10 @@ export enum Stage {
 type CreateConversationState = {
   stage: Stage;
   isActive: boolean;
+  groupUsers: [];
+  startGroupChat: {
+    isLoading: boolean;
+  };
   groupDetails: {
     isCreating: boolean;
   };
@@ -34,6 +41,10 @@ type CreateConversationState = {
 const initialState: CreateConversationState = {
   stage: Stage.None,
   isActive: false,
+  groupUsers: [],
+  startGroupChat: {
+    isLoading: false,
+  },
   groupDetails: {
     isCreating: false,
   },
@@ -49,11 +60,17 @@ const slice = createSlice({
     setActive: (state, action: PayloadAction<boolean>) => {
       state.isActive = action.payload;
     },
+    setFetchingConversations: (state, action: PayloadAction<boolean>) => {
+      state.startGroupChat.isLoading = action.payload;
+    },
     setGroupCreating: (state, action: PayloadAction<boolean>) => {
       state.groupDetails.isCreating = action.payload;
+    },
+    setGroupUsers: (state, action: PayloadAction<[]>) => {
+      state.groupUsers = action.payload;
     },
   },
 });
 
-export const { setActive, setGroupCreating, setStage } = slice.actions;
+export const { setActive, setGroupCreating, setStage, setGroupUsers, setFetchingConversations } = slice.actions;
 export const { reducer } = slice;
--- src/store/create-conversation/saga.ts
+++ src/store/create-conversation/saga.ts
@@ -1,15 +1,29 @@
 import { takeLatest, put, call, select } from 'redux-saga/effects';
-import { SagaActionTypes, Stage, setActive, setGroupCreating, setStage } from '.';
-import { createConversation as performCreateConversation } from '../channels-list/saga';
+import {
+  SagaActionTypes,
+  Stage,
+  setActive,
+  setFetchingConversations,
+  setGroupCreating,
+  setGroupUsers,
+  setStage,
+} from '.';
+import { channelsReceived, createConversation as performCreateConversation } from '../channels-list/saga';
+import { fetchConversationsWithUsers } from '../channels-list/api';
+import { setActiveMessengerId } from '../chat';
+import { currentUserSelector } from '../authentication/saga';
 
 export function* startConversation(_action) {
   yield put(setActive(false));
   yield put(setGroupCreating(false));
+  yield put(setGroupUsers([]));
   yield put(setStage(Stage.CreateOneOnOne));
 }
 
 export function* reset(_action) {
   yield put(setActive(false));
+  yield put(setGroupUsers([]));
+  yield put(setFetchingConversations(false));
   yield put(setGroupCreating(false));
   yield put(setStage(Stage.None));
 }
@@ -43,6 +57,33 @@ export function* forward(_action) {
   }
 }
 
+export function* groupMembersSelected(action) {
+  yield put(setFetchingConversations(true));
+  yield call(performGroupMembersSelected, action);
+  yield put(setFetchingConversations(false));
+}
+
+export function* performGroupMembersSelected(action) {
+  const { users } = action.payload;
+
+  const currentUser = yield select(currentUserSelector);
+  const userIds = [
+    currentUser.id,
+    ...users.map((o) => o.value),
+  ];
+  const existingConversations = yield call(fetchConversationsWithUsers, userIds);
+
+  if (existingConversations.length === 0) {
+    yield put(setGroupUsers(users));
+    yield put(setStage(Stage.GroupDetails));
+  } else {
+    const selectedConversation = existingConversations[0];
+    yield call(channelsReceived, { payload: { channels: [selectedConversation] } });
+    yield put(setActiveMessengerId(selectedConversation.id));
+    yield call(reset, {});
+  }
+}
+
 export function* createConversation(action) {
   yield put(setActive(true));
   yield put(setGroupCreating(true));
@@ -57,5 +98,6 @@ export function* saga() {
   yield takeLatest(SagaActionTypes.Back, back);
   yield takeLatest(SagaActionTypes.Forward, forward);
   yield takeLatest(SagaActionTypes.Reset, reset);
+  yield takeLatest(SagaActionTypes.MembersSelected, groupMembersSelected);
   yield takeLatest(SagaActionTypes.CreateConversation, createConversation);
 }

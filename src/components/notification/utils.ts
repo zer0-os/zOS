655094b38437dd62ca3dc22455b32264b10512ea
Adam Kahtava <adam@kahtava.com>
2023-04-19 13:49:44 -0400

    Move mapNotification to utils

---
+++ src/components/notification/utils.ts
@@ -0,0 +1,60 @@
+export function mapNotification(notification, channelName) {
+  const channelText = channelName ? `#${channelName}` : 'a channel';
+
+  if (notification.notificationType === 'chat_channel_mention') {
+    let senderName = displayName(notification.originUser?.profileSummary);
+
+    return {
+      id: notification.id,
+      createdAt: notification.createdAt,
+      body: `${senderName} mentioned you in ${channelText}`,
+      originatingName: senderName,
+      originatingImageUrl: notification.originUser?.profileSummary?.profileImage,
+      isUnread: notification.isUnread,
+    };
+  } else if (notification.notificationType === 'chat_channel_message_replied') {
+    let senderName = displayName(notification.originUser?.profileSummary);
+
+    return {
+      id: notification.id,
+      createdAt: notification.createdAt,
+      body: `${senderName} replied to you in ${channelText}`,
+      originatingName: senderName,
+      originatingImageUrl: notification.originUser?.profileSummary?.profileImage,
+      isUnread: notification.isUnread,
+    };
+  } else if (notification.notificationType === 'chat_dm_mention') {
+    let senderName = displayName(notification.originUser?.profileSummary);
+    return {
+      id: notification.id,
+      createdAt: notification.createdAt,
+      body: `${senderName} mentioned you in a conversation`,
+      originatingName: senderName,
+      originatingImageUrl: notification.originUser?.profileSummary?.profileImage,
+      isUnread: notification.isUnread,
+    };
+  } else if (notification.notificationType === 'chat_dm_message_replied') {
+    let senderName = displayName(notification.originUser?.profileSummary);
+    return {
+      id: notification.id,
+      createdAt: notification.createdAt,
+      body: `${senderName} replied to you in a conversation`,
+      originatingName: senderName,
+      originatingImageUrl: notification.originUser?.profileSummary?.profileImage,
+      isUnread: notification.isUnread,
+    };
+  }
+
+  return null;
+}
+
+function displayName(profileSummary) {
+  let displayName = [
+    profileSummary?.firstName,
+    profileSummary?.lastName,
+  ]
+    .filter((e) => e)
+    .join(' ');
+  displayName = displayName || 'Someone';
+  return displayName;
+}

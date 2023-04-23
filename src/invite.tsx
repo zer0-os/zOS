5eacc67d1515dac9c395f1630a02babac9a096b7
Ratik Jindal <ratikjindal21@gmail.com>
2023-04-23 19:05:15 +0530

    make UI

--- src/invite.tsx
+++ src/invite.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { RootState } from './store/reducer';
 import { connectContainer } from './store/redux-container';
 import { ReactComponent as ZeroLogo } from './zero-logo.svg';
-import { Wizard, Input, Button } from '@zero-tech/zui/components';
+import { Input, Button } from '@zero-tech/zui/components';
+import { ThemeEngine, Themes } from '@zero-tech/zui/components/ThemeEngine';
 
 import './invite.scss';
 
+const INVITE_CODE_LENGTH = 6;
 export interface Properties {
   isAuthenticated: boolean;
 }
 
+export interface State {
+  inviteCode: string;
+  isValidating: boolean;
+  isDisabled: boolean;
+}
+
 export class Container extends React.Component {
+  state: State = {
+    inviteCode: '',
+    isValidating: false,
+    isDisabled: true,
+  };
+
   static mapState(_state: RootState): Partial<Properties> {
     return {
       isAuthenticated: true,
@@ -23,29 +37,40 @@ export class Container extends React.Component {
 
   componentDidMount() {}
 
+  onInviteCodeChanged = async (code) => {
+    this.setState({ inviteCode: code });
+    this.setState({ isDisabled: String(code).length !== INVITE_CODE_LENGTH });
+  };
+
   render() {
     return (
       <>
+        <ThemeEngine theme={Themes.Dark} />
         <div className='main'>
           <ZeroLogo className='main__logo' />
-
-          <Wizard.Container className='main__title'>
-            <Wizard.Header
-              header='Add your invite code'
-              //headerInfo="This is the 6 digit code you received in your invite"
-              subHeader='This is the 6 digit code you received in your invite'
-            />
-          </Wizard.Container>
+          <h3 className='main__title'>Add your invite code</h3>
+          <p className='main__text'>This is the 6 digit code you received in your invite</p>
           <Input
-            label={<div className='main__input__label'> E.g. 123456 </div>}
-            onChange={function noRefCheck() {}}
-            //placeholder="eg. 123456"
+            onChange={this.onInviteCodeChanged}
+            placeholder='E.g. 123456'
             value=''
             className='main__input'
+            type='number'
           />
-          <Button variant='primary' className='main__button' isDisabled={true}>
+          <Button variant='primary' className='main__button' isDisabled={this.state.isDisabled}>
             Get access
           </Button>
+
+          {/* TODO: hyperlink the second texts */}
+          <div className='main__subtext1'>
+            <p className='main__subtext1__text1'> Not been invited yet? </p>
+            <p className='main__subtext1__text2'>Subscribe for public launch updates </p>
+          </div>
+
+          <div className='main__subtext2'>
+            <p className='main__subtext2__text1'> Already on ZERO? </p>
+            <p className='main__subtext2__text2'>Log in </p>
+          </div>
         </div>
       </>
     );

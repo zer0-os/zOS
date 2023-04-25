7310ecc4b3a657d5afbc7388792847b0ff99b31c
Dale Fukami <dale@zero.tech>
2023-04-25 16:52:16 -0600

    Add password strength component

---
+++ src/components/password-strength/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import './styles.scss';
+import { bem } from '../../lib/bem';
+const c = bem('password-strength');
+
+export enum Strength {
+  None = 0,
+  Weak = 1,
+  Acceptable = 2,
+  Good = 3,
+  Strong = 4,
+}
+
+export interface Properties {
+  strength: Strength;
+}
+
+const statusNames = [
+  'none',
+  'weak',
+  'acceptable',
+  'good',
+  'strong',
+];
+
+export class PasswordStrength extends React.Component<Properties> {
+  barStatus(index) {
+    return index > this.props.strength ? '' : 'filled';
+  }
+
+  get strengthText() {
+    return this.props.strength === Strength.None ? '' : statusNames[this.props.strength];
+  }
+
+  render() {
+    return (
+      <div className={c('')} data-strength={statusNames[this.props.strength]}>
+        <div className={c('status-bars')}>
+          <div className={c('status-bar')} data-status={this.barStatus(1)} />
+          <div className={c('status-bar')} data-status={this.barStatus(2)} />
+          <div className={c('status-bar')} data-status={this.barStatus(3)} />
+          <div className={c('status-bar')} data-status={this.barStatus(4)} />
+        </div>
+        <div>
+          <span>Password Strength: </span>
+          <span className={c('strength-text')}>{this.strengthText}</span>
+        </div>
+      </div>
+    );
+  }
+}

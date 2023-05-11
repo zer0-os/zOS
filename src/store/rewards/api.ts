159309c17ee07474c5e6e20e019259a3f402c105
Dale Fukami <dale.fukami@gmail.com>
2023-05-11 09:38:07 -0600

    Remove test sleep

--- src/store/rewards/api.ts
+++ src/store/rewards/api.ts
@@ -1,7 +1,6 @@
 import { get } from '../../lib/api/rest';
 
 export async function fetchRewards(_obj: any) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await get('/rewards/mine');
   return {
     success: true,

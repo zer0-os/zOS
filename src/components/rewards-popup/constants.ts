1e4f449673a6f75f169dd39946de513da364bd8f
Ratik Jindal <ratikjindal21@gmail.com>
2023-05-18 05:54:53 +0530

    add constant

---
+++ src/components/rewards-popup/constants.ts
@@ -0,0 +1,27 @@
+export const rewardsFaq = [
+  {
+    content:
+      'A product does not exist without the people who use it. By using Zero, you are not doing nothing, you are adding value and we believe you should be rewarded for that.As a web3 platform, Zero rewards are not just cash, they have additional uses that will be added over time, such as; voting, creating your own networks and launching DAOs & NFTs. See Wilder World for an example of what Zero tech can do Your participation in most large centralised social networks results in large profit to the company through advertisement - the monetisation of attention. If you produce something amazing and millions of people see it, millions of people see ads too and that money goes to the company, NOT YOU. We think this needs to change. You are creating the value, so you should share in the revenue',
+    title: 'This seems too good to be true, how can I earn for doing nothing?',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum bibendum bibendum metus, sit amet auctor erat rutrum ac. Curabitur nec lectus in tellus egestas aliquet non ac lorem. Curabitur id mauris nisi. Ut velit felis, posuere eu gravida quis, varius ullamcorper leo. Sed interdum posuere diam vitae facilisis. Sed commodo consectetur est consequat aliquam. Vestibulum vel tempor nibh, a pellentesque erat. Ut mauris orci, vehicula ut sagittis quis, eleifend vel ante. Maecenas sed lacinia dolor. Vivamus condimentum hendrerit turpis vitae porta. Nunc malesuada massa sit amet sem aliquet, non posuere odio placerat. Curabitur leo ipsum, ultricies sed finibus quis, sodales.',
+    title: 'How does Zero make money if it’s free AND I’m being paid?',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius aliquet diam, sed luctus mauris lobortis eu. Vivamus nec mollis ipsum. Etiam nisl risus, facilisis eu pulvinar sed, tempus non massa. Maecenas quis nisi quam. Aenean mattis sit amet felis sed pellentesque. Sed sollicitudin risus eget neque tincidunt auctor. Vivamus luctus ipsum sit amet quam dictum, nec fringilla purus consectetur. Vestibulum consequat sollicitudin libero sit amet luctus. Donec consequat dui et nibh lacinia malesuada. Maecenas a augue a quam aliquam semper. Duis pellentesque venenatis lorem, eu dignissim tortor. In vel nibh ante. In molestie risus lorem, ac pharetra orci cursus nec.',
+    title: 'When can I redeem my tokens into cash?',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius aliquet diam, sed luctus mauris lobortis eu. Vivamus nec mollis ipsum. Etiam nisl risus, facilisis eu pulvinar sed, tempus non massa. Maecenas quis nisi quam. Aenean mattis sit amet felis sed pellentesque. Sed sollicitudin risus eget neque tincidunt auctor. Vivamus luctus ipsum sit amet quam dictum, nec fringilla purus consectetur. Vestibulum consequat sollicitudin libero sit amet luctus. Donec consequat dui et nibh lacinia malesuada. Maecenas a augue a quam aliquam semper. Duis pellentesque venenatis lorem, eu dignissim tortor. In vel nibh ante. In molestie risus lorem, ac pharetra orci cursus nec.',
+    title: 'What do i need to do to earn tokens?',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius aliquet diam, sed luctus mauris lobortis eu. Vivamus nec mollis ipsum. Etiam nisl risus, facilisis eu pulvinar sed, tempus non massa. Maecenas quis nisi quam. Aenean mattis sit amet felis sed pellentesque. Sed sollicitudin risus eget neque tincidunt auctor. Vivamus luctus ipsum sit amet quam dictum, nec fringilla purus consectetur. Vestibulum consequat sollicitudin libero sit amet luctus. Donec consequat dui et nibh lacinia malesuada. Maecenas a augue a quam aliquam semper. Duis pellentesque venenatis lorem, eu dignissim tortor. In vel nibh ante. In molestie risus lorem, ac pharetra orci cursus nec.',
+    title: 'Is Zero a DAO?',
+  },
+];

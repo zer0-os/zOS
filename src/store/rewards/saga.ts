ac4e4dac414edc390c181efc1f31ed661f4e2691
Ratik Jindal <ratikjindal21@gmail.com>
2023-05-24 13:26:05 +0530

    update saga

--- src/store/rewards/saga.ts
+++ src/store/rewards/saga.ts
@@ -1,14 +1,15 @@
 import { call, put, take } from 'redux-saga/effects';
 
-import { SagaActionTypes, setLoading, setZero } from '.';
-import { fetchRewards } from './api';
+import { SagaActionTypes, setLoading, setZero, setZeroPreviousDay } from '.';
+import { RewardsResp, fetchRewards } from './api';
 
 export function* fetch(_action) {
   yield put(setLoading(true));
   try {
-    const result = yield call(fetchRewards, {});
+    const result: RewardsResp = yield call(fetchRewards, {});
     if (result.success) {
       yield put(setZero(result.response.zero.toString()));
+      yield put(setZeroPreviousDay(result.response.zeroPreviousDay.toString()));
     } else {
     }
   } catch (e) {

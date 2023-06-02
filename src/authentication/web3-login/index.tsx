cab65825323f76303802464cf711fab2055eecf4
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:49:17 +1000

    feat(web3-login): show error Alert

--- src/authentication/web3-login/index.tsx
+++ src/authentication/web3-login/index.tsx
@@ -5,6 +5,7 @@ import { WalletSelect } from '../../components/wallet-select';
 import { bem } from '../../lib/bem';
 const c = bem('web3-login');
 import './styles.scss';
+import { Alert } from '@zero-tech/zui/components';
 
 export interface Web3LoginProperties {
   error: string;
@@ -18,9 +19,12 @@ export class Web3Login extends React.Component<Web3LoginProperties, Web3LoginSta
   render() {
     const { error, isConnecting, onSelect } = this.props;
 
+    const shouldShowError = error && !isConnecting;
+
     return (
       <div>
         <WalletSelect isConnecting={isConnecting} onSelect={onSelect} />
+        {shouldShowError && <Alert variant='error'>{error}</Alert>}
       </div>
     );
   }

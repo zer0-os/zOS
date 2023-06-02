b4c26814ad5b2247871a820b2863c138da7fdfe3
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:49:50 +1000

    test(web3-login): container tests

---
+++ src/authentication/web3-login/container.test.tsx
@@ -0,0 +1,44 @@
+import { Container, Web3LoginContainerProperties } from './container';
+import { shallow } from 'enzyme';
+import { Connectors } from '../../lib/web3';
+
+describe('Web3LoginContainer', () => {
+  const subject = (props: Partial<Web3LoginContainerProperties>) => {
+    const allProps: Web3LoginContainerProperties = {
+      error: '',
+      isConnecting: false,
+      loginByWeb3: (connector: Connectors) => null,
+      ...props,
+    };
+
+    return shallow(<Container {...allProps} />);
+  };
+
+  describe('mapState', () => {
+    it('should map state to props', () => {
+      const state = {
+        web3: { value: { error: 'error' } },
+        login: { loading: true },
+      };
+
+      const props = Container.mapState(state as any);
+
+      expect(props).toEqual({ error: 'error', isConnecting: true });
+    });
+  });
+
+  describe('mapActions', () => {
+    it('should map actions to props', () => {
+      const props = Container.mapActions({} as any);
+
+      expect(props).toHaveProperty('loginByWeb3');
+    });
+  });
+
+  it('should pass props to Web3Login', () => {
+    const wrapper = subject({ error: 'error', isConnecting: true });
+
+    const web3Login = wrapper.find('Web3Login');
+    expect(web3Login.props()).toEqual(expect.objectContaining({ error: 'error', isConnecting: true }));
+  });
+});

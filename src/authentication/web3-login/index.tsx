5ffde1bdf49804c65749e98be581ea762a0494de
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:49:46 +1000

    feat(web3-login): first styling run

--- src/authentication/web3-login/index.tsx
+++ src/authentication/web3-login/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Alert, Button } from '@zero-tech/zui/components';
+import { IconLoading2 } from '@zero-tech/zui/icons';
 import { WalletSelect } from '../../components/wallet-select';
 
 import { bem } from '../../lib/bem';
 const c = bem('web3-login');
 import './styles.scss';
-import { Link } from 'react-router-dom';
 
 export interface Web3LoginProperties {
   error: string;
@@ -20,20 +21,29 @@ export class Web3Login extends React.Component<Web3LoginProperties, Web3LoginSta
   render() {
     const { error, isConnecting, onSelect } = this.props;
 
-    const shouldShowError = error && !isConnecting;
-    const subheaderText = isConnecting ? 'Entering ZERO...' : 'Select your wallet';
-
     return (
-      <div>
-        <h3>Log in</h3>
-        <span>{subheaderText}</span>
+      <div className={c('')}>
+        <div className={c('header')}>
+          <h3 className={c('heading')}>Log in</h3>
+          <span className={c('sub-heading')}>{isConnecting ? 'Entering ZERO...' : 'Select your wallet'}</span>
+        </div>
         {isConnecting ? (
-          <Button isDisabled={true}>Waiting for wallet confirmation</Button>
+          <div className={c('connecting')}>
+            <Button isDisabled={true} endEnhancer={<IconLoading2 isFilled={true} className={c('spinner')} size={20} />}>
+              Waiting for wallet confirmation
+            </Button>
+          </div>
         ) : (
-          <WalletSelect isConnecting={false} onSelect={onSelect} />
+          <div className={c('login')}>
+            <div className={c('select-wallet')}>
+              <WalletSelect isConnecting={false} onSelect={onSelect} />
+            </div>
+            {error && <Alert variant='error'>{error}</Alert>}
+            <div className={c('options')}>
+              <Link to='/login'>Back to log in</Link>
+            </div>
+          </div>
         )}
-        {shouldShowError && <Alert variant='error'>{error}</Alert>}
-        <Link to='/login'>Back to log in</Link>
       </div>
     );
   }

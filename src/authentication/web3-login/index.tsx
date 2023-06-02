bf4a90f64da5702f9f10e37f4b974bad4c50675d
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:49:42 +1000

    feat(web3-login): add other content

--- src/authentication/web3-login/index.tsx
+++ src/authentication/web3-login/index.tsx
@@ -6,6 +6,7 @@ import { WalletSelect } from '../../components/wallet-select';
 import { bem } from '../../lib/bem';
 const c = bem('web3-login');
 import './styles.scss';
+import { Link } from 'react-router-dom';
 
 export interface Web3LoginProperties {
   error: string;
@@ -20,15 +21,19 @@ export class Web3Login extends React.Component<Web3LoginProperties, Web3LoginSta
     const { error, isConnecting, onSelect } = this.props;
 
     const shouldShowError = error && !isConnecting;
+    const subheaderText = isConnecting ? 'Entering ZERO...' : 'Select your wallet';
 
     return (
       <div>
+        <h3>Log in</h3>
+        <span>{subheaderText}</span>
         {isConnecting ? (
           <Button isDisabled={true}>Waiting for wallet confirmation</Button>
         ) : (
           <WalletSelect isConnecting={false} onSelect={onSelect} />
         )}
         {shouldShowError && <Alert variant='error'>{error}</Alert>}
+        <Link to='/login'>Back to log in</Link>
       </div>
     );
   }

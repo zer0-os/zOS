a217ed7eee3a5fbbaf585dd2db1199b845d2afe8
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:48:52 +1000

    feat(web3-login): add props to ui component

--- src/authentication/web3-login/index.tsx
+++ src/authentication/web3-login/index.tsx
@@ -4,12 +4,18 @@ import { bem } from '../../lib/bem';
 const c = bem('web3-login');
 import './styles.scss';
 
-export interface Web3LoginProperties {}
+export interface Web3LoginProperties {
+  error: string;
+  isConnecting: boolean;
+  onSelect: (connector: any) => void;
+}
 
 interface Web3LoginState {}
 
 export class Web3Login extends React.Component<Web3LoginProperties, Web3LoginState> {
   render() {
+    const { error, isConnecting, onSelect } = this.props;
+
     return <></>;
   }
 }

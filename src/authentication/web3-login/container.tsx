ac256b14ea6b2b9a73f81eb0d31570993e28d477
Brett <12437916+colbr@users.noreply.github.com>
2023-06-03 02:49:54 +1000

    feat(web3-login): container

--- src/authentication/web3-login/container.tsx
+++ src/authentication/web3-login/container.tsx
@@ -1,27 +1,43 @@
 import * as React from 'react';
 
+import { Connectors } from '../../lib/web3';
 import { RootState } from '../../store/reducer';
+import { loginByWeb3 } from '../../store/login';
+import { connectContainer } from '../../store/redux-container';
 
 import { Web3Login } from '.';
+import { Web3Connect } from '../../components/web3-connect';
 
-export interface Web3LoginContainerPublicProperties {}
+export interface Web3LoginContainerProperties {
+  error: string;
+  isConnecting: boolean;
+  loginByWeb3: (connector: Connectors) => void;
+}
 
-export interface Web3LoginContainerProperties extends Web3LoginContainerPublicProperties {}
+export class Container extends React.Component<Web3LoginContainerProperties> {
+  static mapState(state: RootState): Partial<Web3LoginContainerProperties> {
+    const {
+      web3: { value },
+      login,
+    } = state;
 
-export class Web3LoginContainer extends React.Component<Web3LoginContainerProperties> {
-  static mapState(state: RootState) {
-    //
+    return {
+      error: value.error,
+      isConnecting: login.loading,
+    };
   }
 
-  static mapErrors(errors: string[]) {
-    //
-  }
-
-  static mapActions() {
-    //
+  static mapActions(_props: Web3LoginContainerProperties): Partial<Web3LoginContainerProperties> {
+    return { loginByWeb3 };
   }
 
   render() {
-    return <Web3Login />;
+    return (
+      <Web3Connect>
+        <Web3Login error={this.props.error} isConnecting={this.props.isConnecting} onSelect={this.props.loginByWeb3} />
+      </Web3Connect>
+    );
   }
 }
+
+export const Web3LoginContainer = connectContainer<{}>(Container);

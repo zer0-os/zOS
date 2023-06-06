1f36a5d5a2826a7e2acfb7463b411c57664df2e2
dominic <domw30@gmail.com>
2023-06-06 15:23:06 +0200

    feat: add user search results component

--- src/components/messenger/list/user-search-results.tsx
+++ src/components/messenger/list/user-search-results.tsx
@@ -1,29 +1,32 @@
 import * as React from 'react';
 
-import { UserSearchResultItem } from './user-search-result-item';
+import { Option } from '../lib/types';
+
+import { Avatar } from '@zero-tech/zui/components';
 
 import { bem } from '../../../lib/bem';
 
 const c = bem('user-search-results');
 
 export interface Properties {
-  results: any;
-  onClick: (userId: string) => void;
+  results: Option[];
+  onCreate: (userId: string) => void;
 }
 
 export class UserSearchResults extends React.Component<Properties> {
+  handleUserClick = (id: string) => {
+    this.props.onCreate(id);
+  };
+
   render() {
     return (
       <div className={c('')}>
         <div className={c('title')}>Start a new conversation:</div>
-        {this.props.results.map((result) => (
-          <UserSearchResultItem
-            key={result.value}
-            id={result.value}
-            label={result.label}
-            image={result.image}
-            onClick={this.props.onClick}
-          />
+        {this.props.results.map((userResult) => (
+          <div key={userResult.value} className={c('item')} onClick={() => this.handleUserClick(userResult.value)}>
+            <Avatar size='regular' type='circle' imageURL={userResult.image} />
+            <div className={c('label')}>{userResult.label}</div>
+          </div>
         ))}
       </div>
     );

bfdd278c870adf55f6af921819447d34e6dcb30e
Brett <12437916+colbr@users.noreply.github.com>
2023-06-06 16:27:57 +1000

    feat(login): add switching between options

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,9 +3,10 @@ import { Link, Redirect } from 'react-router-dom';
 
 import { RootState } from '../../store/reducer';
 import { connectContainer } from '../../store/redux-container';
-import { LoginStage } from '../../store/login';
+import { LoginStage, switchLoginStage } from '../../store/login';
 
 import { ThemeEngine, Themes } from '@zero-tech/zui/components/ThemeEngine';
+import { Web3LoginContainer } from '../../authentication/web3-login/container';
 import { EmailLoginContainer } from '../../authentication/email-login/container';
 import { ReactComponent as ZeroLogo } from '../../zero-logo.svg';
 
@@ -13,6 +14,7 @@ import './login.scss';
 
 export interface LoginContainerProperties {
   stage: LoginStage;
+  switchLoginStage: (stage: LoginStage) => void;
 }
 
 export class LoginContainer extends React.Component<LoginContainerProperties> {
@@ -24,8 +26,18 @@ export class LoginContainer extends React.Component<LoginContainerProperties> {
   }
 
   static mapActions(_props: LoginContainerProperties): Partial<LoginContainerProperties> {
-    return {};
+    return {
+      switchLoginStage,
+    };
   }
+
+  switchToEmailLogin = () => {
+    this.props.switchLoginStage(LoginStage.EmailLogin);
+  };
+
+  switchToWeb3Login = () => {
+    this.props.switchLoginStage(LoginStage.Web3Login);
+  };
 
   render() {
     return (
@@ -35,8 +47,14 @@ export class LoginContainer extends React.Component<LoginContainerProperties> {
           <ZeroLogo />
           <h3>Log in</h3>
           {this.props.stage === LoginStage.EmailLogin && <EmailLoginContainer />}
+          {this.props.stage === LoginStage.Web3Login && <Web3LoginContainer />}
           {this.props.stage === LoginStage.Done && <Redirect to='/' />}
           <div>
+            <button
+              onClick={this.props.stage === LoginStage.Web3Login ? this.switchToEmailLogin : this.switchToWeb3Login}
+            >
+              Log in with {this.props.stage === LoginStage.Web3Login ? 'Email' : 'Web3'}
+            </button>
             <span>
               New to ZERO? <Link to='/get-access'>Create an account</Link>
             </span>

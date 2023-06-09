628fe77377e5f3358496f552c4fb80c7b39c07d3
DomW <domw30@gmail.com>
2023-06-09 14:10:51 +0100

    feat: add empty current conversation search results state and adjust positioning (#646)
    * feat: add empty state and adjust position
    
    * feat: prevent empty state displaying when search input is empty

--- src/components/messenger/list/conversation-list-panel.tsx
+++ src/components/messenger/list/conversation-list-panel.tsx
@@ -143,6 +143,10 @@ export class ConversationListPanel extends React.Component<Properties, State> {
               />
             ))}
 
+            {this.filteredConversations?.length === 0 && this.state.filter !== '' && (
+              <div className='messages-list__empty'>{`You do not have any active conversations with '${this.state.filter}' `}</div>
+            )}
+
             {this.state.userSearchResults?.length > 0 && this.state.filter !== '' && (
               <UserSearchResults
                 results={this.state.userSearchResults}

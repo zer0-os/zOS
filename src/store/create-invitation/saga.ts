fdc919d9a9dd170e35157a6d91b60ccd99ffa737
Ratik Jindal <ratikjindal21@gmail.com>
2023-06-16 21:25:13 +0530

    update

--- src/store/create-invitation/saga.ts
+++ src/store/create-invitation/saga.ts
@@ -1,56 +1,31 @@
-import { take, put, call, race, spawn, takeEvery } from 'redux-saga/effects';
+import { put, call, takeLatest } from 'redux-saga/effects';
 import { SagaActionTypes, reset, setInvite } from '.';
 import { getInvite } from './api';
 import { config } from '../../config';
-import { Events, getAuthChannel } from '../authentication/channels';
 
 export function* fetchInvite() {
-  while (true) {
-    const { cancel } = yield race({
-      cancel: take(SagaActionTypes.Cancel),
-      get: take(SagaActionTypes.GetCode),
-    });
+  // reset before fetching
+  yield put(reset());
 
-    if (cancel) {
-      return yield put(reset());
-    }
+  try {
+    const invitation = yield call(getInvite);
+    console.log('invitation', invitation);
 
-    try {
-      const invitation = yield call(getInvite);
-      // For now, we don't include the code in the url
-      yield put(
-        setInvite({
-          code: invitation.slug,
-          url: config.inviteUrl,
-          invitesUsed: invitation.invitesUsed,
-          maxUses: invitation.maxInvitesPerUser,
-        })
-      );
-      return;
-    } catch (e) {
-      // Listen again
-    }
-  }
-}
-
-function* listenForUserLogin() {
-  const authChannel = yield call(getAuthChannel);
-  while (true) {
-    yield take(authChannel, Events.UserLogin);
-    yield spawn(fetchInvite);
-  }
-}
-
-function* listenForUserLogout() {
-  const authChannel = yield call(getAuthChannel);
-  while (true) {
-    yield take(authChannel, Events.UserLogout);
-    yield put({ type: SagaActionTypes.Cancel });
+    // For now, we don't include the code in the url
+    yield put(
+      setInvite({
+        code: invitation.slug,
+        url: config.inviteUrl,
+        invitesUsed: invitation.invitesUsed,
+        maxUses: invitation.maxInvitesPerUser,
+      })
+    );
+    return;
+  } catch (e) {
+    // Listen again
   }
 }
 
 export function* saga() {
-  yield spawn(listenForUserLogin);
-  yield spawn(listenForUserLogout);
-  yield takeEvery(SagaActionTypes.GetCode, fetchInvite);
+  yield takeLatest(SagaActionTypes.GetCode, fetchInvite);
 }

8f02e7374240696530306c6626c039c1b438662f
dominic <domw30@gmail.com>
2023-06-21 08:32:41 +0200

    feat: add settings menu tests and bit of refactoring

--- src/components/settings-menu/index.tsx
+++ src/components/settings-menu/index.tsx
@@ -17,6 +17,7 @@ export interface Properties {
 
 export class SettingsMenu extends React.Component<Properties> {
   containsAtSymbol = this.props.userHandle.includes('@');
+  userHandle = this.containsAtSymbol ? this.props.userHandle : <Address address={this.props.userHandle} />;
 
   handleLogout = () => {
     this.props.onLogout();
@@ -30,9 +31,7 @@ export class SettingsMenu extends React.Component<Properties> {
         <Avatar size={'regular'} type={'circle'} imageURL={this.props.userAvatarUrl} />
         <div className={c('user-details')}>
           <div className={c('name')}>{this.props.userName}</div>
-          <div className={c('address')}>
-            {this.containsAtSymbol ? this.props.userHandle : <Address address={this.props.userHandle} />}
-          </div>
+          <div className={c('address')}>{this.userHandle}</div>
         </div>
       </div>
     );

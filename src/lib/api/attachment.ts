86632d02d8ffeaa7e58d0e966eb3877c40f60506
Ratik Jindal <ratikjindal21@gmail.com>
2023-06-22 06:17:04 +0530

    update download API to use key directly

--- src/lib/api/attachment.ts
+++ src/lib/api/attachment.ts
@@ -7,9 +7,11 @@ interface AttachmentResponse {
 
 export async function getAttachmentUrl(attachment: { key: string }): Promise<string> {
   const filter: any = { key: attachment.key };
-  const attachmentResponse = await get<AttachmentResponse>('/api/feedItems/getAttachmentDownloadInfo', filter).catch(
-    (err) => console.error(err)
-  );
+  const attachmentResponse = await get<AttachmentResponse>(
+    '/api/feedItems/getAttachmentDownloadInfo',
+    undefined,
+    filter
+  ).catch((err) => console.error(err));
 
   if (attachmentResponse && attachmentResponse.ok) {
     return attachmentResponse.body.signedUrl;
@@ -20,8 +22,13 @@ export async function getAttachmentUrl(attachment: { key: string }): Promise<str
 
 export function download(key: string): void {
   (async () => {
-    const url = await getAttachmentUrl({ key });
+    // This works, it's using the `https://res.cloudinary.url/..` to display the file
+    // (eg. this is how it looks like: https://res.cloudinary.com/fact0ry-dev/image/upload/v1687385590/ax9wzrns7msfezo0ziqu.pdf)
+    // but zero-web is uploading files to S3 first, and this api (getAttachmentUrl) was
+    // created to get the file from S3, should we do that here as well?
+    const url = key;
 
+    //const url = await getAttachmentUrl({ key });
     if (url) {
       const win = window.open('about:blank', '_blank');
       win.location.assign(url);

6439852e1fa19744c0f32519fc7c7088fd656bcc
dominic <domw30@gmail.com>
2023-06-23 12:55:45 +0200

    refactor: rename userHandle -> getUserHandle

--- src/components/settings-menu/index.tsx
+++ src/components/settings-menu/index.tsx
@@ -20,7 +20,7 @@ export class SettingsMenu extends React.Component<Properties> {
     return this.props.userHandle.includes('@');
   }
 
-  userHandle() {
+  getUserHandle() {
     return this.containsAtSymbol() ? this.props.userHandle : <Address address={this.props.userHandle} />;
   }
 
@@ -36,7 +36,7 @@ export class SettingsMenu extends React.Component<Properties> {
         <Avatar size={'regular'} type={'circle'} imageURL={this.props.userAvatarUrl} />
         <div className={c('user-details')}>
           <div className={c('name')}>{this.props.userName}</div>
-          <div className={c('address')}>{this.userHandle()}</div>
+          <div className={c('address')}>{this.getUserHandle()}</div>
         </div>
       </div>
     );

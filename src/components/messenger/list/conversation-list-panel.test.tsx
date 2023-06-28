f5a792ed940e9323af70435afb7c3b40954c3bc5
Brett <12437916+colbr@users.noreply.github.com>
2023-06-28 16:05:14 +1200

    lint: remove unused import

--- src/components/messenger/list/conversation-list-panel.test.tsx
+++ src/components/messenger/list/conversation-list-panel.test.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 import { shallow } from 'enzyme';
 
 import { ConversationListPanel, Properties } from './conversation-list-panel';

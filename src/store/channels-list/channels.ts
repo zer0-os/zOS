a7b43e5653389a6b5d55fca55e7e67055c93607a
Ratik Jindal <ratikjindal21@gmail.com>
2023-07-05 20:13:14 +0530

    comment

--- src/store/channels-list/channels.ts
+++ src/store/channels-list/channels.ts
@@ -1,6 +1,7 @@
 import { multicastChannel } from 'redux-saga';
 import { call } from 'redux-saga/effects';
 
+// create 2 events: message loaded for channel, messages loaded for conversation
 export enum ChannelEvents {
   MessagesLoadedForChannel = 'channel/messages/loaded',
 }

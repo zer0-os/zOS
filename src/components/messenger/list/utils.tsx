7572618a862e8bedf84a9854707884ecd67de6d1
Ratik Jindal <ratikjindal21@gmail.com>
2023-07-14 04:27:31 +0530

    fix test

--- src/components/messenger/list/utils.tsx
+++ src/components/messenger/list/utils.tsx
@@ -13,5 +13,9 @@ export function lastSeenText(user): string {
 }
 
 export function escapeRegExp(string) {
+  if (typeof string !== 'string') {
+    return;
+  }
+
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }

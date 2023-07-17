10d778339765cb3541fd51e50e37b9601a7857e2
Dale Fukami <dale@zero.tech>
2023-07-17 12:10:17 -0600

    Eliminate redundant tests

--- src/store/messages/saga.uploadFileMessage.test.ts
+++ src/store/messages/saga.uploadFileMessage.test.ts
@@ -10,15 +10,7 @@ import { stubResponse } from '../../test/saga';
 import { denormalize as denormalizeChannel, normalize as normalizeChannel } from '../channels';
 
 describe(uploadFileMessages, () => {
-  it('does nothing if there are no files', async () => {
-    await expectSaga(uploadFileMessages, 'id', [])
-      .not.call.fn(uploadFileMessageApi)
-      .not.call.fn(uploadAttachment)
-      .not.call.fn(sendFileMessage)
-      .run();
-  });
-
-  it('uploads media file', async () => {
+  it('uploads an uploadable file', async () => {
     const channelId = 'channel-id';
     const imageFile = { nativeFile: { type: 'image/png' } } as any;
     const imageCreationResponse = { id: 'image-message-id', image: {} };
@@ -52,55 +44,6 @@ describe(uploadFileMessages, () => {
       .call(uploadFileMessageApi, channelId, imageFile1.nativeFile, rootMessageId)
       .call(uploadFileMessageApi, channelId, imageFile2.nativeFile, '')
       .run();
-  });
-
-  it('uploads non-media (attachment)', async () => {
-    const channelId = 'channel-id';
-    const pdfFile = { nativeFile: { type: 'application/pdf' } } as any;
-    const fileUploadResult = {
-      name: 'filename',
-      key: 'file-key',
-      url: 'file-url',
-      type: 'file',
-    } as FileUploadResult;
-    const messageSendResponse = { id: 'new-id' };
-
-    const initialState = existingChannelState({ id: channelId, messages: [{ id: 'existing-message' }] });
-
-    const { storeState } = await expectSaga(uploadFileMessages, channelId, [pdfFile])
-      .provide([
-        stubResponse(call(uploadAttachment, pdfFile.nativeFile), fileUploadResult),
-        stubResponse(call(sendFileMessage, channelId, fileUploadResult), messageSendResponse),
-      ])
-      .withReducer(rootReducer, initialState as any)
-      .run();
-
-    const channel = denormalizeChannel(channelId, storeState);
-    expect(channel.messages[0].id).toEqual('existing-message');
-    expect(channel.messages[1]).toEqual({ id: 'new-id' });
-  });
-
-  it('send Giphy message', async () => {
-    const channelId = 'channel-id';
-    const giphy = {
-      name: 'giphy-file',
-      giphy: { images: { original: { url: 'url_giphy' } }, type: 'gif' },
-    };
-    const expectedFileToSend = { url: 'url_giphy', name: 'giphy-file', type: 'gif' };
-    const messageSendResponse = { id: 'new-id' };
-
-    const initialState = existingChannelState({ id: channelId, messages: [{ id: 'existing-message' }] });
-
-    const { storeState } = await expectSaga(uploadFileMessages, channelId, [giphy])
-      .provide([
-        stubResponse(call(sendFileMessage, channelId, expectedFileToSend as any), messageSendResponse),
-      ])
-      .withReducer(rootReducer, initialState as any)
-      .run();
-
-    const channel = denormalizeChannel(channelId, storeState);
-    expect(channel.messages[0].id).toEqual('existing-message');
-    expect(channel.messages[1]).toEqual({ id: 'new-id' });
   });
 });
 

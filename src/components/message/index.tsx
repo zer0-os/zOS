a642306a1145d69ad3129eae2329e4a807d717b0
dominic <domw30@gmail.com>
2023-07-21 16:44:27 +0100

    fix: resolve failing tests

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -123,7 +123,7 @@ export class Message extends React.Component<Properties, State> {
     return (
       <div {...cn('footer')}>
         {!!this.props.updatedAt && !this.state.isEditing && !isSendStatusFailed && (
-          <span {...cn('edited')}>(Edited)</span>
+          <span {...cn('edited-flag')}>(Edited)</span>
         )}
         {!isSendStatusFailed && this.renderTime(this.props.createdAt)}
         {isSendStatusFailed && (

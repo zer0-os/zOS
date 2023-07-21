9b4a85e4b23b2906380601aae5b4501c0bd262a7
dominic <domw30@gmail.com>
2023-07-21 16:05:40 +0100

    refactor: move edit actions to own folder

---
+++ src/components/message/edit-message-actions/edit-message-actions.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { bemClassName } from '../../../lib/bem';
+
+import { IconCheck, IconXClose } from '@zero-tech/zui/icons';
+import { IconButton } from '@zero-tech/zui/components';
+
+import './styles.scss';
+
+const cn = bemClassName('edit-message-actions');
+
+export interface Properties {
+  className?: string;
+  onEdit: () => void;
+  onCancel?: () => void;
+}
+
+export default class EditMessageActions extends React.Component<Properties> {
+  render() {
+    return (
+      <div {...cn()}>
+        <IconButton {...cn('icon')} onClick={this.props.onEdit} Icon={IconCheck} size={24} />
+        <IconButton {...cn('icon')} onClick={this.props.onCancel} Icon={IconXClose} size={24} />
+      </div>
+    );
+  }
+}

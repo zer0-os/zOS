89fb0057203998724d72262dea409aeda6e77589
DomW <domw30@gmail.com>
2023-07-27 13:33:27 +0100

    feat: update invite screen and layout (#858)
    * feat: update invite screen and layout
    
    * fix: disable button when rendering alert (invalid invite)
    
    * refactor: refactor auth root styles

--- src/authentication/validate-invite/index.tsx
+++ src/authentication/validate-invite/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 
 import { Button, Input, Alert } from '@zero-tech/zui/components';
 import { InviteCodeStatus } from '../../store/registration';
@@ -69,48 +68,49 @@ export class Invite extends React.Component<Properties, State> {
         errorMessage = 'Invite code error.';
     }
 
-    return <Alert variant='error'>{errorMessage}</Alert>;
+    return (
+      <Alert className={c('alert')} variant='error'>
+        {errorMessage}
+      </Alert>
+    );
   };
 
   render() {
     return (
       <div className={c('')}>
-        <h3 className={c('heading')}>Add your invite code</h3>
-        <div className={c('sub-heading')}>This is the 6 digit code you received in your invite</div>
-
+        <div className={c('heading-container')}>
+          <h3 className={c('heading')}>Add invite code</h3>
+          <div className={c('sub-heading')}>6 digit code you received in your invite</div>
+        </div>
         <form className={c('form')} onSubmit={this.submitForm}>
-          <Input
-            onChange={this.onInviteCodeChanged}
-            placeholder='E.g. 123456'
-            value={this.state.inviteCode}
-            type='text'
-          />
+          <div className={c('input-container')}>
+            <Input
+              onChange={this.onInviteCodeChanged}
+              placeholder='e.g 123456'
+              value={this.state.inviteCode}
+              type='text'
+              error={this.state.renderAlert}
+            />
 
-          {this.state.renderAlert &&
-            this.props.inviteCodeStatus !== InviteCodeStatus.VALID &&
-            this.renderAlert(this.props.inviteCodeStatus)}
+            {this.state.renderAlert &&
+              this.props.inviteCodeStatus !== InviteCodeStatus.VALID &&
+              this.renderAlert(this.props.inviteCodeStatus)}
+          </div>
 
           <Button
+            className={c('submit-button')}
             variant='primary'
-            isDisabled={!this.state.inviteCode.length || this.state.inviteCode.length > MAX_INVITE_CODE_LENGTH}
+            isDisabled={
+              !this.state.inviteCode.length ||
+              this.state.inviteCode.length > MAX_INVITE_CODE_LENGTH ||
+              this.state.renderAlert
+            }
             isLoading={this.props.isLoading}
             isSubmit
           >
             Get access
           </Button>
         </form>
-
-        <div className={c('other-options')}>
-          <div>
-            <span>Not been invited yet? </span>
-            <a href='https://www.zine.live/#/portal/signup'>Subscribe for public launch updates</a>
-          </div>
-
-          <div>
-            <span>Already on ZERO? </span>
-            <Link to='/login'>Log in</Link>
-          </div>
-        </div>
       </div>
     );
   }
--- src/invite.tsx
+++ src/invite.tsx
@@ -4,7 +4,6 @@ import { connectContainer } from './store/redux-container';
 import { ReactComponent as ZeroLogo } from './zero-logo.svg';
 import { Redirect } from 'react-router-dom';
 
-import './invite.scss';
 import { ThemeEngine, Themes } from '@zero-tech/zui/components/ThemeEngine';
 import { RegistrationStage } from './store/registration';
 import { InviteContainer } from './authentication/validate-invite/container';
@@ -12,6 +11,12 @@ import { SelectMethodContainer } from './authentication/select-method/container'
 import { CreateEmailAccountContainer } from './authentication/create-email-account/container';
 import { CreateAccountDetailsContainer } from './authentication/create-account-details/container';
 import { CreateWalletAccountContainer } from './authentication/create-wallet-account/container';
+import { Footer } from './authentication/footer/footer';
+
+import { bemClassName } from './lib/bem';
+import './invite.scss';
+
+const cn = bemClassName('invite-main');
 
 export interface Properties {
   stage: RegistrationStage;
@@ -39,14 +44,17 @@ export class Container extends React.Component<Properties> {
     return (
       <>
         <ThemeEngine theme={Themes.Dark} />
-        <div className='invite-main'>
+        <div {...cn('')}>
           <ZeroLogo />
+
           {this.props.stage === RegistrationStage.ValidateInvite && <InviteContainer />}
           {this.props.stage === RegistrationStage.SelectMethod && <SelectMethodContainer />}
           {this.props.stage === RegistrationStage.EmailAccountCreation && <CreateEmailAccountContainer />}
           {this.props.stage === RegistrationStage.WalletAccountCreation && <CreateWalletAccountContainer />}
           {this.props.stage === RegistrationStage.ProfileDetails && <CreateAccountDetailsContainer />}
           {this.props.stage === RegistrationStage.Done && <Redirect to='/' />}
+
+          <Footer />
         </div>
       </>
     );

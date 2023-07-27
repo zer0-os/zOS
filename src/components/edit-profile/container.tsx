c765d69262e1b38f24daf5bd15aaf8376ace711c
Ratik Jindal <ratikjindal21@gmail.com>
2023-07-27 15:30:02 +0530

    add container

---
+++ src/components/edit-profile/container.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { RootState } from '../../store/reducer';
+import { connectContainer } from '../../store/redux-container';
+import { EditProfile } from '.';
+import { editProfile, setChangesSaved } from '../../store/edit-profile';
+import { Container as RegistrationContainer } from '../../authentication/create-account-details/container';
+export interface PublicProperties {
+  onClose?: () => void;
+}
+
+export interface Properties extends PublicProperties {
+  isLoading: boolean;
+  errors: {
+    image?: string;
+    name?: string;
+    general?: string;
+  };
+  changesSaved: boolean;
+  currentDisplayName: string;
+  currentProfileImage: string;
+  editProfile: (data: { name: string; image: File }) => void;
+  setChangesSaved: (value: boolean) => void;
+}
+
+export class Container extends React.Component<Properties> {
+  static mapState(state: RootState) {
+    const {
+      editProfile,
+      authentication: { user },
+    } = state;
+    return {
+      isLoading: editProfile.loading,
+      errors: RegistrationContainer.mapErrors(editProfile.errors),
+      changesSaved: editProfile.changesSaved,
+      currentDisplayName: user?.data?.profileSummary.firstName,
+      currentProfileImage: user?.data?.profileSummary.profileImage,
+    };
+  }
+
+  static mapActions(_props: Properties): Partial<Properties> {
+    return { editProfile, setChangesSaved };
+  }
+
+  componentWillUnmount(): void {
+    this.props.setChangesSaved(false);
+  }
+
+  render() {
+    return (
+      <EditProfile
+        isLoading={this.props.isLoading}
+        onEdit={this.props.editProfile}
+        errors={this.props.errors}
+        onClose={this.props.onClose}
+        changesSaved={this.props.changesSaved}
+        currentDisplayName={this.props.currentDisplayName}
+        currentProfileImage={this.props.currentProfileImage}
+      />
+    );
+  }
+}
+
+export const EditProfileContainer = connectContainer<PublicProperties>(Container);

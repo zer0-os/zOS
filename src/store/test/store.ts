cf9836dfb2298b0b64d92e56b77bcafa82093803
Dale Fukami <dale@zero.tech>
2023-08-01 10:53:52 -0600

    Update StoreBuilder to include more configurability

--- src/store/test/store.ts
+++ src/store/test/store.ts
@@ -2,10 +2,32 @@ import { RootState } from '../reducer';
 
 import { Channel, User, normalize as normalizeChannel } from '../channels';
 import { normalize as normalizeUser } from '../users';
+import { User as AuthenticatedUser } from '../authentication/types';
 
 export class StoreBuilder {
+  channelList: Partial<Channel>[] = [];
   conversationList: Partial<Channel>[] = [];
   users: Partial<{ userId: string }>[] = [];
+
+  activeChannel: Partial<Channel> = {};
+  activeConversation: Partial<Channel> = {};
+  isFullScreenMessenger: boolean = true;
+  currentUser: Partial<AuthenticatedUser>;
+
+  withActiveChannel(channel: Partial<Channel>) {
+    this.activeChannel = channel;
+    return this;
+  }
+
+  withActiveConversation(conversation: Partial<Channel>) {
+    this.activeConversation = conversation;
+    return this;
+  }
+
+  withChannelList(...args: Partial<Channel>[]) {
+    this.channelList.push(...args.map((c) => ({ isChannel: true, ...c })));
+    return this;
+  }
 
   withConversationList(...args: Partial<Channel>[]) {
     this.conversationList.push(...args.map((c) => ({ isChannel: false, ...c })));
@@ -17,11 +39,35 @@ export class StoreBuilder {
     return this;
   }
 
+  withCurrentUser(user: Partial<AuthenticatedUser>) {
+    this.currentUser = user;
+    return this;
+  }
+
+  withCurrentUserId(id: string) {
+    return this.withCurrentUser({ id });
+  }
+
+  inFullScreenMessenger() {
+    this.isFullScreenMessenger = true;
+    return this;
+  }
+
+  inWindowedMode() {
+    this.isFullScreenMessenger = false;
+    return this;
+  }
+
   build() {
     const {
       result: channelsList,
       entities: { channels: normalizedChannels = {} },
-    } = normalizeChannel(this.conversationList);
+    } = normalizeChannel([
+      this.activeChannel,
+      this.activeConversation,
+      ...this.channelList,
+      ...this.conversationList,
+    ]);
     const {
       entities: { users: normalizedUsers = {} },
     } = normalizeUser(this.users);
@@ -32,6 +78,18 @@ export class StoreBuilder {
         channels: normalizedChannels,
         users: normalizedUsers,
       } as any,
+      chat: {
+        activeChannelId: this.activeChannel.id || null,
+        activeConversationId: this.activeConversation.id || null,
+      },
+      layout: {
+        value: {
+          isMessengerFullScreen: this.isFullScreenMessenger,
+        },
+      },
+      authentication: {
+        user: { data: this.currentUser },
+      },
     } as RootState;
   }
 }

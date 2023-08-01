ae798f28ab2e7882da5df2922712b372e22845e6
Dale Fukami <dale@zero.tech>
2023-08-01 08:31:24 -0600

    Save user search results to the store

--- src/store/users/saga.test.ts
+++ src/store/users/saga.test.ts
@@ -1,9 +1,10 @@
 import { expectSaga } from 'redux-saga-test-plan';
-import { clearUsers } from './saga';
+import { clearUsers, receiveSearchResults } from './saga';
 
-import { rootReducer } from '../reducer';
+import { RootState, rootReducer } from '../reducer';
+import { denormalize, normalize } from '.';
 
-describe('users saga', () => {
+describe(clearUsers, () => {
   it('removes the users', async () => {
     const users = { 'id-one': { id: 'id-one', name: 'this should be removed' } };
     const notifications = { 'id-two': { id: 'id-two', name: 'do not remove this one' } };
@@ -15,7 +16,7 @@ describe('users saga', () => {
       .withState({
         normalized: { users, notifications },
       })
-      .run(0);
+      .run();
 
     expect(normalized).toEqual({
       users: {},
@@ -23,3 +24,52 @@ describe('users saga', () => {
     });
   });
 });
+
+describe(receiveSearchResults, () => {
+  it('translates the search results into user records', async () => {
+    const user1 = { id: 'user-1', name: 'Test User 1', profileImage: 'image-url-1' };
+    const user2 = { id: 'user-2', name: 'Test User 2', profileImage: 'image-url-2' };
+
+    const { storeState } = await expectSaga(receiveSearchResults, [
+      user1,
+      user2,
+    ])
+      .withReducer(rootReducer)
+      .run();
+
+    expect(denormalize(user1.id, storeState)).toEqual(
+      expect.objectContaining({
+        userId: user1.id,
+        firstName: user1.name,
+        profileImage: user1.profileImage,
+      })
+    );
+    expect(denormalize(user2.id, storeState)).toEqual(
+      expect.objectContaining({
+        userId: user2.id,
+        firstName: user2.name,
+        profileImage: user2.profileImage,
+      })
+    );
+  });
+
+  it('does not replace an existing record', async () => {
+    const user1 = { id: 'user-1', name: 'Test User 1', profileImage: 'image-url-1' };
+
+    const existingUser = { userId: 'user-1', firstName: 'Test', lastName: 'User 1', profileImage: 'image-url-2' };
+    const initialState = existingUserState(existingUser);
+
+    const { storeState } = await expectSaga(receiveSearchResults, [user1]).withReducer(rootReducer, initialState).run();
+
+    expect(denormalize(user1.id, storeState)).toEqual(expect.objectContaining(existingUser));
+  });
+});
+
+function existingUserState(user) {
+  const normalized = normalize(user);
+  return {
+    normalized: {
+      ...normalized.entities,
+    },
+  } as RootState;
+}

f5584835caa587edaf1bb58b3663d9f7ce08c2d4
dominic <domw30@gmail.com>
2023-08-04 14:19:22 +0100

    feat: add animation to invite code module

--- src/store/create-invitation/saga.test.ts
+++ src/store/create-invitation/saga.test.ts
@@ -20,7 +20,7 @@ describe('fetchInvite', () => {
           ],
         ])
         .withReducer(rootReducer, {
-          createInvitation: { code: '', url: '', invitesUsed: 0, maxUses: 0 },
+          createInvitation: { code: '', url: '', invitesUsed: 0, maxUses: 0, isLoading: false },
         } as any)
         .dispatch({ type: SagaActionTypes.GetCode })
         .run();
@@ -38,12 +38,12 @@ describe('fetchInvite', () => {
         storeState: { createInvitation },
       } = await expectSaga(fetchInvite)
         .withReducer(rootReducer, {
-          createInvitation: { code: 'something', url: 'url', invitesUsed: 2, maxUses: 3 },
+          createInvitation: { code: 'something', url: 'url', invitesUsed: 2, maxUses: 3, isLoading: false },
         } as any)
         .dispatch({ type: SagaActionTypes.Cancel })
         .run();
 
-      expect(createInvitation).toEqual({ code: '', url: '', invitesUsed: 0, maxUses: 0 });
+      expect(createInvitation).toEqual({ code: '', url: '', invitesUsed: 0, maxUses: 0, isLoading: false });
     });
   });
 });

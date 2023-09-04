e81e44a42ce5a715abefbdf161ced5682557a95f
Ratik Jindal <ratikjindal21@gmail.com>
2023-09-04 23:21:46 +0530

    update package.json & craco

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,13 @@ module.exports = {
       'react-router-dom': path.resolve('./node_modules/react-router-dom'),
       '@zer0-os/zos-component-library': path.resolve('./node_modules/@zer0-os/zos-component-library'),
     },
+    configure: (webpackConfig, { _env, _paths }) => {
+      const EXCLUDED_PLUGINS = ['ForkTsCheckerWebpackPlugin'];
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (plugin) => !EXCLUDED_PLUGINS.includes(plugin.constructor.name)
+      );
+      return webpackConfig;
+    },
   },
   devServer: {
     watchOptions: {
@@ -24,5 +31,11 @@ module.exports = {
         ],
       };
     },
+  },
+  typescript: {
+    enableTypeChecking: false /* (default value) */,
+  },
+  eslint: {
+    enable: false /* (default value) */,
   },
 };

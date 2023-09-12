b55e1912c89074cf3d7d888977f21b66c1e600ee
Ratik Jindal <ratikjindal21@gmail.com>
2023-09-12 21:52:38 +0530

    add limit:50 to /searchInMyNetworksByName

--- src/store/users/api.ts
+++ src/store/users/api.ts
@@ -2,7 +2,7 @@ import { get } from '../../lib/api/rest';
 import { MemberNetworks } from './types';
 
 export async function searchMyNetworksByName(filter: string): Promise<MemberNetworks[]> {
-  return await get('/api/v2/users/searchInNetworksByName', { filter })
+  return await get('/api/v2/users/searchInNetworksByName', { filter, limit: 50 })
     .catch((_error) => null)
     .then((response) => response?.body || []);
 }

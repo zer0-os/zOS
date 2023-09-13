c912d33611e9692845e04a2a98439686d4abcb2c
DomW <domw30@gmail.com>
2023-09-13 15:15:09 +0100

    refactor: extracts rewards button from rewards bar container (#1005)
    * refactor: remove includesRewardsAvatar (duplicate prop)
    
    * refactor: minor changes to settings menu container class names
    
    * refactor: extract settings menu and rewards bar to a user account container
    
    * refactor: extract render rewards button to rewards button component file and make adjustments
    
    * fix: center condition to make sense for rewards container
    
    * refactor: rename rewards bar to rewards container
    
    * refactor: revert rename rewards bar to rewards container (to be actioned in another branch)
    
    * refactor: revert to plain empty functions in tests

--- src/components/rewards-bar/index.tsx
+++ src/components/rewards-bar/index.tsx
@@ -1,19 +1,8 @@
 import * as React from 'react';
 
+import { RewardsButton } from '../rewards-button';
 import { RewardsFAQModal } from '../rewards-faq-modal';
-import { TooltipPopup } from '../tooltip-popup/tooltip-popup';
 import { RewardsPopupContainer } from '../rewards-popup/container';
-
-import { Status } from '@zero-tech/zui/components';
-import { IconCurrencyDollar } from '@zero-tech/zui/icons';
-
-import { bem } from '../../lib/bem';
-import { formatWeiAmount } from '../../lib/number';
-
-import classnames from 'classnames';
-import './styles.scss';
-
-const c = bem('rewards-bar');
 
 export interface Properties {
   isFirstTimeLogin: boolean;
@@ -60,45 +49,21 @@ export class RewardsBar extends React.Component<Properties, State> {
   closeRewardsFAQModal = () => this.setState({ isRewardsFAQModalOpen: false });
   closeRewardsTooltip = () => this.props.onRewardsTooltipClose();
 
-  renderRewardsButton() {
-    return (
-      <TooltipPopup
-        open={!this.props.isRewardsLoading && this.props.showRewardsInTooltip && this.props.isMessengerFullScreen}
-        align='center'
-        side='right'
-        content={`You’ve earned ${formatWeiAmount(this.props.zeroPreviousDay)} ZERO today`}
-        onClose={this.closeRewardsTooltip}
-      >
-        <div className={c('rewards-button-container')}>
-          <button
-            onClick={this.openRewards}
-            className={classnames(c('rewards-button'), {
-              [c('rewards-button', 'open')]:
-                this.state.isRewardsPopupOpen && (this.props.hasLoadedConversation || !this.props.isFirstTimeLogin),
-            })}
-          >
-            <div>Rewards</div>
-            <div
-              className={classnames(c('rewards-icon'), {
-                [c('rewards-icon', 'open')]:
-                  this.state.isRewardsPopupOpen && (this.props.hasLoadedConversation || !this.props.isFirstTimeLogin),
-              })}
-            >
-              <IconCurrencyDollar size={16} />
-              {!this.state.isRewardsPopupOpen && this.props.showRewardsInPopup && this.props.isMessengerFullScreen && (
-                <Status type='idle' className={c('rewards-icon__status')} />
-              )}
-            </div>
-          </button>
-        </div>
-      </TooltipPopup>
-    );
-  }
-
   render() {
     return (
-      <div className={c('')}>
-        {this.renderRewardsButton()}
+      <div>
+        <RewardsButton
+          openRewards={this.openRewards}
+          closeRewardsTooltip={this.closeRewardsTooltip}
+          zeroPreviousDay={this.props.zeroPreviousDay}
+          isRewardsLoading={this.props.isRewardsLoading}
+          isFirstTimeLogin={this.props.isFirstTimeLogin}
+          isRewardsPopupOpen={this.state.isRewardsPopupOpen}
+          isMessengerFullScreen={this.props.isMessengerFullScreen}
+          showRewardsInTooltip={this.props.showRewardsInTooltip}
+          showRewardsInPopup={this.props.showRewardsInPopup}
+          hasLoadedConversation={this.props.hasLoadedConversation}
+        />
 
         {this.state.isRewardsPopupOpen && (this.props.hasLoadedConversation || !this.props.isFirstTimeLogin) && (
           <RewardsPopupContainer
---
+++ src/components/rewards-button/index.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+import { TooltipPopup } from '../tooltip-popup/tooltip-popup';
+
+import { Status } from '@zero-tech/zui/components';
+import { IconCurrencyDollar } from '@zero-tech/zui/icons';
+
+import { bem } from '../../lib/bem';
+import { formatWeiAmount } from '../../lib/number';
+
+import classnames from 'classnames';
+import './styles.scss';
+
+const c = bem('rewards-button-container');
+
+export interface Properties {
+  isRewardsPopupOpen: boolean;
+  isFirstTimeLogin: boolean;
+  isMessengerFullScreen: boolean;
+  hasLoadedConversation: boolean;
+
+  zeroPreviousDay: string;
+  isRewardsLoading: boolean;
+  showRewardsInTooltip: boolean;
+  showRewardsInPopup: boolean;
+
+  openRewards: () => void;
+  closeRewardsTooltip: () => void;
+}
+
+export class RewardsButton extends React.Component<Properties> {
+  render() {
+    return (
+      <TooltipPopup
+        open={!this.props.isRewardsLoading && this.props.showRewardsInTooltip && this.props.isMessengerFullScreen}
+        align='center'
+        side='right'
+        content={`You’ve earned ${formatWeiAmount(this.props.zeroPreviousDay)} ZERO today`}
+        onClose={this.props.closeRewardsTooltip}
+      >
+        <div className={c('')}>
+          <button
+            onClick={this.props.openRewards}
+            className={classnames(c('rewards-button'), {
+              [c('rewards-button', 'open')]:
+                this.props.isRewardsPopupOpen && (this.props.hasLoadedConversation || !this.props.isFirstTimeLogin),
+            })}
+          >
+            <div>Rewards</div>
+            <div
+              className={classnames(c('rewards-icon'), {
+                [c('rewards-icon', 'open')]:
+                  this.props.isRewardsPopupOpen && (this.props.hasLoadedConversation || !this.props.isFirstTimeLogin),
+              })}
+            >
+              <IconCurrencyDollar size={16} />
+              {!this.props.isRewardsPopupOpen && this.props.showRewardsInPopup && this.props.isMessengerFullScreen && (
+                <Status type='idle' className={c('rewards-icon__status')} />
+              )}
+            </div>
+          </button>
+        </div>
+      </TooltipPopup>
+    );
+  }
+}

c6f12408a825331502fb00f1dcac7fbc15911517
DomW <domw30@gmail.com>
2023-09-15 10:18:34 +0100

    refactor: (tooltip popup) replace local icon button component with zUI icon button component (#1019)

--- src/components/tooltip-popup/tooltip-popup.tsx
+++ src/components/tooltip-popup/tooltip-popup.tsx
@@ -3,8 +3,8 @@ import './styles.scss';
 import { bem } from '../../lib/bem';
 import type { ReactNode } from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
-import { IconButton } from '../icon-button';
 import { IconXClose } from '@zero-tech/zui/icons';
+import { IconButton } from '@zero-tech/zui/components';
 
 const c = bem('tooltip-popup');
 
@@ -31,7 +31,7 @@ export class TooltipPopup extends React.Component<Properties> {
             <TooltipPrimitive.Content side={this.props.side} align={this.props.align} className={c('content')}>
               {this.props.content}
               <TooltipPrimitive.Arrow className={c('arrow')} width={32} height={16} />
-              <IconButton className={c('close')} Icon={IconXClose} size={16} onClick={this.props.onClose} />
+              <IconButton className={c('close')} Icon={IconXClose} size='x-small' onClick={this.props.onClose} />
             </TooltipPrimitive.Content>
           </TooltipPrimitive.Root>
         </TooltipPrimitive.Provider>

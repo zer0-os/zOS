30c71e6b3c34567dff5c32671aabf752625642dc
Ratik Jindal <ratikjindal21@gmail.com>
2023-09-20 12:26:54 +0530

    remove api file

--- src/store/users/api.ts
+++
@@ -1,8 +0,0 @@
-import { get } from '../../lib/api/rest';
-import { MemberNetworks } from './types';
-
-export async function searchMyNetworksByName(filter: string): Promise<MemberNetworks[]> {
-  return await get('/api/v2/users/searchInNetworksByName', { filter, limit: 50 })
-    .catch((_error) => null)
-    .then((response) => response?.body || []);
-}

c99c23b0af17f98bb18bdd966e5ffa4c6d8e40f6
Ratik Jindal <ratikjindal21@gmail.com>
2023-09-22 03:29:20 +0530

    update matrix message mapping

--- src/lib/chat/chat-message.ts
+++ src/lib/chat/chat-message.ts
@@ -4,6 +4,7 @@ import { ChatMember } from './types';
 import { denormalize as denormalizeUser } from '../../store/users';
 import { currentUserSelector } from '../../store/authentication/saga';
 import moment from 'moment';
+import { MatrixClient as SDKMatrixClient } from 'matrix-js-sdk';
 
 const DEFAULT_MEDIA_TYPE = 'image';
 
@@ -119,19 +120,38 @@ function extractMessageData(jsonData, isMediaMessage) {
   };
 }
 
-export function mapMatrixMessage(matrixMessage) {
-  const parent = matrixMessage.content['m.relates_to'];
+async function extractParentMessageData(matrixMessage, sdkMatrixClient: SDKMatrixClient) {
+  const parentMessageData = {
+    parentMessageId: null,
+    parentMessageText: '',
+  };
 
+  const parent = matrixMessage.content['m.relates_to'];
+  if (parent && parent['m.in_reply_to']) {
+    parentMessageData.parentMessageId = parent['m.in_reply_to'].event_id;
+
+    const parentMessage = await sdkMatrixClient.fetchRoomEvent(
+      matrixMessage.room_id,
+      parentMessageData.parentMessageId
+    );
+    if (parentMessage) {
+      parentMessageData.parentMessageText = parentMessage.content.body;
+    }
+  }
+
+  return parentMessageData;
+}
+
+export async function mapMatrixMessage(matrixMessage, sdkMatrixClient: SDKMatrixClient) {
   return {
     id: matrixMessage.event_id,
     message: matrixMessage.content.body,
-    parentMessageText: '',
-    parentMessageId: parent ? parent['m.in_reply_to'].event_id : null,
     createdAt: matrixMessage.origin_server_ts,
     updatedAt: null,
     sender: {},
     isAdmin: false,
     ...{ mentionedUsers: [], hidePreview: false, media: null, image: null, admin: {} },
+    ...(await extractParentMessageData(matrixMessage, sdkMatrixClient)),
   };
 }
 

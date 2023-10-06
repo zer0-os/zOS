9303e62d05e426fe1e4280a321e1e3d3af713cd3
dominic <domw30@gmail.com>
2023-10-06 10:52:27 +0100

    fix: remove edited flag on unedited messages

--- src/lib/chat/matrix-client.ts
+++ src/lib/chat/matrix-client.ts
@@ -466,7 +466,7 @@ export class MatrixClient implements IChatClient {
       message,
       isAdmin: false,
       createdAt: timestamp,
-      updatedAt: timestamp,
+      updatedAt: null,
       sender: {
         userId: senderId,
         firstName: matrixEvent.sender?.name,

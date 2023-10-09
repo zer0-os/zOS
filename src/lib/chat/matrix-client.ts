c68f6e25ecaddbb118609a793d6731255d2ae8cb
dominic <domw30@gmail.com>
2023-10-09 12:29:52 +0100

    fix: set supports optimistic send to false to ensure create conversation remains in working order

--- src/lib/chat/matrix-client.ts
+++ src/lib/chat/matrix-client.ts
@@ -57,7 +57,7 @@ export class MatrixClient implements IChatClient {
   }
 
   supportsOptimisticSend() {
-    return true;
+    return false;
   }
 
   async connect(userId: string, accessToken: string) {

fc40dde8388628ab48471ac90e3ff374980b4547
jtulloch <joel@jtulloch.com>
2023-10-13 09:20:20 -0400

    E2e encryption (#1098)
    * device session is persisted
    
    * encryption enabled for new rooms
    
    * React to Decrypted message events
    
    * fix failing tests
    
    * matrix session is cleared on disconnect
    
    ---------
    
    Co-authored-by: Dale Fukami <dale@zero.tech>

---
+++ src/lib/chat/session-storage.ts
@@ -0,0 +1,35 @@
+export interface ChatSession {
+  deviceId: string;
+  accessToken: string;
+  userId: string;
+}
+
+export class SessionStorage {
+  constructor(private storage = localStorage) {}
+
+  clear() {
+    const deviceId = this.storage.getItem('mxz_device_id');
+
+    this.storage.removeItem('mxz_device_id');
+    this.storage.removeItem(`mxz_access_token_${deviceId}`);
+    this.storage.removeItem('mxz_user_id');
+  }
+
+  set(session: ChatSession) {
+    this.storage.setItem('mxz_device_id', session.deviceId);
+    this.storage.setItem(`mxz_access_token_${session.deviceId}`, session.accessToken);
+    this.storage.setItem('mxz_user_id', session.userId);
+  }
+
+  get(): ChatSession {
+    const deviceId = this.storage.getItem('mxz_device_id');
+
+    if (!deviceId) return null;
+
+    return {
+      deviceId,
+      accessToken: this.storage.getItem(`mxz_access_token_${deviceId}`),
+      userId: this.storage.getItem('mxz_user_id'),
+    };
+  }
+}

662021ff9c9e8fbe9eaaeaf307b623f45c4996d7
Ratik Jindal <ratikjindal21@gmail.com>
2023-10-23 23:29:04 +0530

    update

--- src/store/chat/bus.ts
+++ src/store/chat/bus.ts
@@ -49,8 +49,6 @@ export function createChatConnection(userId, chatAccessToken) {
       emit({ type: Events.ChannelInvitationReceived, payload: { channelId } });
     const onUserLeft = (channelId, userId) => emit({ type: Events.UserLeftChannel, payload: { channelId, userId } });
     const onUserJoinedChannel = (channel) => emit({ type: Events.UserJoinedChannel, payload: { channel } });
-    const onConversationListChanged = (conversationIds) =>
-      emit({ type: Events.ConversationListChanged, payload: { conversationIds } });
     const onUserPresenceChanged = (matrixId, isOnline, lastSeenAt) =>
       emit({ type: Events.UserPresenceChanged, payload: { matrixId, isOnline, lastSeenAt } });
     const onRoomNameChanged = (roomId, name) => emit({ type: Events.RoomNameChanged, payload: { id: roomId, name } });
@@ -71,7 +69,6 @@ export function createChatConnection(userId, chatAccessToken) {
       onUserReceivedInvitation,
       onUserLeft,
       onUserJoinedChannel,
-      onConversationListChanged,
       onUserPresenceChanged,
       onRoomNameChanged,
       onRoomAvatarChanged,

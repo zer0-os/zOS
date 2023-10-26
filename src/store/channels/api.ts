b12d9f0ad213098e1bb29c4c69e0bfc8fe1577cc
dominic <domw30@gmail.com>
2023-10-26 12:07:19 +0100

    refactor: remove unused api function

--- src/store/channels/api.ts
+++ src/store/channels/api.ts
@@ -1,13 +1,7 @@
-import { post, put } from '../../lib/api/rest';
+import { post } from '../../lib/api/rest';
 
 export async function joinChannel(channelId: string): Promise<number> {
   const response = await post<any>(`/chatChannels/${channelId}/join`);
-
-  return response.status;
-}
-
-export async function markAllMessagesAsReadInChannel(channelId: string, userId: string): Promise<number> {
-  const response = await put<any>(`/chatChannels/${channelId}/messages/mark-as-read`).send({ id: userId });
 
   return response.status;
 }

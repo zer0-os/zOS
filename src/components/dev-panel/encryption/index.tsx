c5bb45f29b51d671e62a434609b305eaeb02df94
Dale Fukami <dale@zero.tech>
2023-11-17 10:13:50 -0700

    Make fields copyable and take up only a single line

--- src/components/dev-panel/encryption/index.tsx
+++ src/components/dev-panel/encryption/index.tsx
@@ -4,6 +4,7 @@ import { bemClassName } from '../../../lib/bem';
 import './styles.scss';
 import { Button } from '@zero-tech/zui/components';
 import { User } from '../../../store/channels';
+import { clipboard } from '../../../lib/clipboard';
 
 const cn = bemClassName('dev-panel-encryption');
 
@@ -19,28 +20,45 @@ export interface Properties {
 
 interface State {}
 
-export class Encryption extends React.Component<Properties, State> {
+export class Encryption extends React.PureComponent<Properties, State> {
   render() {
     return (
-      <>
+      <div {...cn()}>
         <h2>Me</h2>
-        <div {...cn('my-id')}>Id: {this.props.userId}</div>
+        <Field label={'id'} value={this.props.userId} />
         <br />
-        <div {...cn('my-device')}>Device: {this.props.deviceId}</div>
+        <Field label={'device'} value={this.props.deviceId} />
 
         <h2>Room</h2>
-        <div {...cn('room-id')}>{this.props.roomId}</div>
+        <Field label={'id: '} value={this.props.roomId} />
         <h3>Other Members</h3>
         {this.props.otherMembers.map((member) => (
-          <div key={member.userId} {...cn('room-member')}>
-            {member.firstName} - {member.matrixId}
-          </div>
+          <Field key={member.userId} label={member.firstName} value={member.matrixId} />
         ))}
 
         <h2>Print to Console</h2>
         <Button onPress={this.props.onDeviceListRequested}>Device List</Button>
         <Button onPress={this.props.onRoomKeysRequested}>Room Keys</Button>
-      </>
+      </div>
+    );
+  }
+}
+
+export class Field extends React.PureComponent<{ label: string; value: string }, State> {
+  get clipboard() {
+    return clipboard;
+  }
+
+  copyValue = () => {
+    this.clipboard.write(this.props.value);
+  };
+
+  render() {
+    return (
+      <div {...cn('field')} onClick={this.copyValue}>
+        <span {...cn('label')}> {this.props.label}: </span>
+        <span {...cn('value')}> {this.props.value}</span>
+      </div>
     );
   }
 }

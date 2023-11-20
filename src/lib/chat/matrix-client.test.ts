21a88b5fc468ad000d03018cd43a8799ab5a1a7a
dominic <domw30@gmail.com>
2023-11-20 11:00:50 +0000

    refactor: remove notification mock

--- src/lib/chat/matrix-client.test.ts
+++ src/lib/chat/matrix-client.test.ts
@@ -22,12 +22,6 @@ jest.mock('../../store/messages/api', () => {
   return { uploadAttachment: (...args) => mockUploadAttachment(...args) };
 });
 
-global.Notification = {
-  permission: 'granted',
-  requestPermission: jest.fn().mockResolvedValue('granted'),
-  prototype: {},
-} as unknown as typeof Notification;
-
 const stubRoom = (attrs = {}) => ({
   roomId: 'some-id',
   getAvatarUrl: () => '',
@@ -70,7 +64,6 @@ const getSdkClient = (sdkClient = {}) => ({
   fetchRoomEvent: jest.fn(),
   paginateEventTimeline: () => true,
   isRoomEncrypted: () => true,
-  handleNotificationPermissions: jest.fn(),
   ...sdkClient,
 });
 

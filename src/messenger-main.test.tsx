e2df645d8ff52e2ea555cf9a6d1b8d924419c804
DomW <domw30@gmail.com>
2023-11-30 13:59:33 +0000

    feat: remove url paths (replace zns route connect) (#1205)
    * feat: WIP - remove url paths
    
    * refactor: remove logs
    
    * feat: replace zns route connect with messenger main component
    
    * fix: ensure login redirect on logout
    
    * refactor: remove zns route connect files

---
+++ src/messenger-main.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Container } from './messenger-main';
+import { Create as CreateAccount } from './components/account/create';
+import { Provider as AuthenticationContextProvider } from './components/authentication/context';
+import { ZUIProvider } from '@zero-tech/zui/ZUIProvider';
+import { Main } from './Main';
+
+describe('MessengerMain', () => {
+  const mockIsAuthenticated = true;
+
+  const subject = (isAuthenticated = mockIsAuthenticated) => {
+    return shallow(<Container isAuthenticated={isAuthenticated} />);
+  };
+
+  it('renders the CreateAccount component', () => {
+    const wrapper = subject();
+    expect(wrapper.find(CreateAccount)).toHaveLength(1);
+  });
+
+  it('contains AuthenticationContextProvider with the correct context', () => {
+    const wrapper = subject();
+    const provider = wrapper.find(AuthenticationContextProvider);
+    expect(provider).toHaveLength(1);
+    expect(provider.prop('value')).toEqual({ isAuthenticated: mockIsAuthenticated });
+  });
+
+  it('contains the ZUIProvider', () => {
+    const wrapper = subject();
+    expect(wrapper.find(ZUIProvider)).toHaveLength(1);
+  });
+
+  it('renders the Main component', () => {
+    const wrapper = subject();
+    expect(wrapper.find(Main)).toHaveLength(1);
+  });
+});

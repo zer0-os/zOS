40dd0d8c478c55501719fa8319e12efdda737f03
Dale Fukami <dale@zero.tech>
2023-12-05 10:44:35 -0700

    Add EditConversationPanel stub

---
+++ src/components/messenger/list/edit-conversation-panel/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface Properties {}
+
+export class EditConversationPanel extends React.Component<Properties> {
+  render() {
+    return <div>Edit Conversation</div>;
+  }
+}

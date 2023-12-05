ca3eaf77aab25378aa3c3731877c10d67e611272
Ratik Jindal <ratikjindal21@gmail.com>
2023-12-06 03:34:45 +0530

    edit EditConversationPanel to add ability to edit group name + icon

--- src/components/messenger/list/edit-conversation-panel/index.test.tsx
+++ src/components/messenger/list/edit-conversation-panel/index.test.tsx
@@ -1,20 +1,71 @@
 import { shallow } from 'enzyme';
 
 import { EditConversationPanel, Properties } from '.';
+import { ImageUpload } from '../../../image-upload';
+import { buttonLabelled } from '../../../../test/utils';
+import { Alert } from '@zero-tech/zui/components';
 
 describe(EditConversationPanel, () => {
   const subject = (props: Partial<Properties>) => {
     const allProps: Properties = {
       onBack: () => null,
+      errors: {},
+      conversation: {} as any,
       ...props,
     };
 
     return shallow(<EditConversationPanel {...allProps} />);
   };
 
-  it('TODO renders', function () {
-    const wrapper = subject({});
+  describe('Edit Group Icon & Name', () => {
+    it('renders body with ImageUpload and Input', () => {
+      const wrapper = subject({ conversation: { name: 'Display Name', icon: 'icon-url' } as any });
+      expect(wrapper.find('.edit-conversation-panel__body').length).toEqual(1);
+      expect(wrapper.find(ImageUpload).props().imageSrc).toEqual('icon-url');
+      expect(wrapper.find('Input[name="name"]').props().label).toEqual('Display Name');
+    });
 
-    expect(wrapper.exists()).toBeTrue();
+    it('disables Save Changes button when name is empty', () => {
+      const wrapper = subject({ conversation: { name: '', icon: '' } as any });
+      expect(saveButton(wrapper).prop('isDisabled')).toEqual(true);
+    });
+
+    it('disables Save Changes button when no changes are made', () => {
+      const wrapper = subject({ conversation: { name: 'Display Name', icon: 'icon-url' } as any });
+
+      expect(saveButton(wrapper).prop('isDisabled')).toEqual(true);
+    });
+
+    it('enables Save Changes button when changes are made', () => {
+      const wrapper = subject({ conversation: { name: 'Display Name', icon: 'icon-url' } as any });
+
+      wrapper.find('Input[name="name"]').simulate('change', 'Jane Smith');
+      expect(saveButton(wrapper).prop('isDisabled')).toEqual(false);
+    });
+
+    it('renders name error when name is empty', () => {
+      const wrapper = subject({ conversation: { name: '', icon: 'some-url' } as any });
+
+      expect(wrapper.find('Input[name="name"]').prop('alert')).toEqual({
+        variant: 'error',
+        text: 'name cannot be empty',
+      });
+    });
+
+    it('does not render name error when name is not empty', () => {
+      const wrapper = subject({ conversation: { name: 'John Doe' } as any });
+
+      expect(wrapper.find('Input[name="name"]').prop('alert')).toBeNull();
+    });
+
+    it('renders general errors', () => {
+      const wrapper = subject({ errors: { general: 'invalid' } });
+
+      expect(wrapper.find(Alert).prop('children')).toEqual('invalid');
+    });
   });
 });
+
+function saveButton(wrapper) {
+  return buttonLabelled(wrapper, 'Save');
+}
--- src/components/messenger/list/edit-conversation-panel/index.tsx
+++ src/components/messenger/list/edit-conversation-panel/index.tsx
@@ -1,15 +1,109 @@
 import * as React from 'react';
 import { PanelHeader } from '../panel-header';
+import { Channel } from '../../../../store/channels';
+import { bemClassName } from '../../../../lib/bem';
+import { Input, Button, Alert } from '@zero-tech/zui/components';
+import { ImageUpload } from '../../../image-upload';
+import { IconUpload2 } from '@zero-tech/zui/icons';
+
+import './styles.scss';
+import { EditConversationErrors } from '../../../../store/group-management/types';
+const cn = bemClassName('edit-conversation-panel');
 
 export interface Properties {
+  conversation: Channel;
+  errors: EditConversationErrors;
   onBack: () => void;
 }
 
-export class EditConversationPanel extends React.Component<Properties> {
+interface State {
+  name: string;
+  image: File | null;
+}
+
+export class EditConversationPanel extends React.Component<Properties, State> {
+  state = {
+    name: this.props.conversation.name || '',
+    image: null,
+  };
+
+  get isValid() {
+    return this.state.name.trim().length > 0;
+  }
+
+  get nameError() {
+    if (!this.isValid) {
+      return { variant: 'error', text: 'name cannot be empty' } as any;
+    }
+    return null;
+  }
+
+  get generalError() {
+    return this.props.errors?.general;
+  }
+
+  trackName = (value) => this.setState({ name: value });
+  trackImage = (image) => this.setState({ image });
+  renderImageUploadIcon = (): JSX.Element => <IconUpload2 isFilled={true} />;
+
+  handleEdit = () => {
+    // do edit here
+  };
+
+  get isDisabled() {
+    return !!this.nameError || (this.state.name === this.props.conversation.name && this.state.image === null);
+  }
+
+  get changesSaved() {
+    return false;
+  }
+
+  renderEditImageAndIcon = () => {
+    <div {...cn('body')}>
+      <div {...cn('image-upload')}>
+        <ImageUpload
+          onChange={this.trackImage}
+          icon={this.renderImageUploadIcon()}
+          uploadText='Select or drag and drop'
+          imageSrc={this.props.conversation.icon} // to show the existing image
+          isError={Boolean(this.props.errors?.image)}
+          errorMessage={this.props.errors?.image}
+        />
+      </div>
+
+      <Input
+        label='Display Name'
+        name='name'
+        value={this.state.name}
+        onChange={this.trackName}
+        error={!!this.nameError}
+        alert={this.nameError}
+        {...cn('body-input')}
+      />
+
+      {this.generalError && (
+        <Alert {...cn('alert')} variant='error'>
+          {this.generalError}
+        </Alert>
+      )}
+      {this.changesSaved && (
+        <Alert {...cn('alert')} variant='success'>
+          {' '}
+          Changes saved{' '}
+        </Alert>
+      )}
+
+      <Button {...cn('zui-button-large')} isSubmit isDisabled={this.isDisabled} onPress={this.handleEdit}>
+        Save
+      </Button>
+    </div>;
+  };
+
   render() {
     return (
       <>
         <PanelHeader title={'Edit Group'} onBack={this.props.onBack} />
+        {this.renderEditImageAndIcon()}
       </>
     );
   }

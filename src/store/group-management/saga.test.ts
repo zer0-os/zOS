b3a99bf26ab16cb08c2316e2d85b70ab67bdc35a
dominic <domw30@gmail.com>
2023-12-05 20:41:45 +0000

    fix: saga tests

--- src/store/group-management/saga.test.ts
+++ src/store/group-management/saga.test.ts
@@ -1,55 +1,18 @@
 import * as matchers from 'redux-saga-test-plan/matchers';
-import { testSaga } from 'redux-saga-test-plan';
-import { reset, startAddGroupMember, roomMembersSelected } from './saga';
+import { reset, roomMembersSelected } from './saga';
 import { StoreBuilder } from '../test/store';
-import { Stage, setStage, setIsAddingMembers, setAddMemberError } from '.';
+import { Stage, setIsAddingMembers, setAddMemberError } from '.';
 import { expectSaga } from '../../test/saga';
 import { rootReducer } from '../reducer';
 import { chat } from '../../lib/chat';
 import { denormalize as denormalizeUsers } from '../users';
 
 describe('Group Management Saga', () => {
-  describe(startAddGroupMember, () => {
-    it('loops on handler stages', async () => {
-      testSaga(startAddGroupMember).next().call(reset).next().put(setStage(Stage.StartAddMemberToRoom));
-    });
-
-    it('finishes when the next stage is None', async () => {
-      testSaga(startAddGroupMember)
-        .next()
-        .next()
-        .next()
-        .next({ handlerResult: Stage.StartAddMemberToRoom })
-        .put(setStage(Stage.StartAddMemberToRoom))
-        .next()
-        .next({ handlerResult: Stage.None })
-        .put(setStage(Stage.None))
-        .next()
-        .call(reset);
-    });
-
-    it('moves back to the appropriate stage', async () => {
-      testSaga(startAddGroupMember).next().next().next().next({ back: true }).put(setStage(Stage.None));
-    });
-
-    it('clears state if an error is thrown', async () => {
-      testSaga(startAddGroupMember)
-        .next()
-        .next({ handlerResult: Stage.StartAddMemberToRoom })
-        .throw(new Error('Stub error'))
-        .call(reset);
-    });
-
-    it('clears state if a cancellation is received', async () => {
-      testSaga(startAddGroupMember).next().next().next().next({ cancel: true }).next().call(reset);
-    });
-  });
-
   describe(roomMembersSelected, () => {
     const action = {
       payload: {
         roomId: 'roomId123',
-        users: [
+        userIds: [
           { value: 'user1', label: 'user1-label' },
           { value: 'user2', label: 'user2-label' },
         ],
@@ -74,7 +37,7 @@ describe('Group Management Saga', () => {
           [
             matchers.select(
               denormalizeUsers,
-              action.payload.users.map((u) => u.value)
+              action.payload.userIds.map((u) => u.value)
             ),
             mockUsers,
           ],

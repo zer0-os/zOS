1b7a893e3e745876ca97855d79d72a9afc9e21f0
Dale Fukami <dale@zero.tech>
2023-12-12 13:00:28 -0700

    Select the most current conversation after login

---
+++ src/store/channels-list/selectors.ts
@@ -0,0 +1,25 @@
+import getDeepProperty from 'lodash.get';
+import { denormalize } from '../channels';
+import { compareDatesDesc } from '../../lib/date';
+
+export function mostRecentConversation(state) {
+  const roomIds = getDeepProperty(state, 'channelsList.value', []);
+  if (!roomIds.length) {
+    return null;
+  }
+
+  const rooms = denormalize(roomIds, state);
+  const roomsWithMetaData = rooms.map(addLastMessage);
+  return roomsWithMetaData.sort(byLastMessageOrCreation)[0];
+}
+
+function addLastMessage(conversation) {
+  const sortedMessages = conversation.messages?.sort((a, b) => compareDatesDesc(a.createdAt, b.createdAt)) || [];
+  return { ...conversation, lastMessage: sortedMessages[0] };
+}
+
+function byLastMessageOrCreation(a, b) {
+  const aDate = a.lastMessage?.createdAt || a.createdAt;
+  const bDate = b.lastMessage?.createdAt || b.createdAt;
+  return compareDatesDesc(aDate, bDate);
+}

4fa2091b9b7f64dae509a95576acdc9be4f8a8e0
Dale Fukami <dalefukami@users.noreply.github.com>
2023-12-15 11:52:52 -0700

    Remove AppMenu from project (#1306)

--- src/lib/apps/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Apps, apps, allApps } from '.';
-
-describe('apps', () => {
-  it('defaults to apps', () => {
-    expect(allApps()).toStrictEqual([
-      apps[Apps.Channels],
-    ]);
-  });
-});

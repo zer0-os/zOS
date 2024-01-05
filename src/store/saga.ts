1847a0e85496cb52ae161bdd3c75862887e688db
Dale Fukami <dale@zero.tech>
2024-01-05 12:42:23 -0700

    Remove ZNS state management

--- src/store/saga.ts
+++ src/store/saga.ts
@@ -1,7 +1,6 @@
 import { all, call, spawn } from 'redux-saga/effects';
 
 import { saga as web3 } from './web3/saga';
-import { saga as zns } from './zns/saga';
 import { saga as channelsList } from './channels-list/saga';
 import { saga as messages } from './messages/saga';
 import { saga as channels } from './channels/saga';
@@ -27,7 +26,6 @@ export function* rootSaga() {
   const allSagas = {
     pageLoad,
     web3,
-    zns,
     channelsList,
     channels,
     messages,

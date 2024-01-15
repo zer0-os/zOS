e766afbb5236156971d8e4b468122881129cba0a
dominic <domw30@gmail.com>
2024-01-15 20:09:33 +0000

    refactor: simplify group management sort members function

--- src/components/messenger/list/group-management/index.tsx
+++ src/components/messenger/list/group-management/index.tsx
@@ -50,6 +50,7 @@ export class GroupManagement extends React.PureComponent<Properties> {
             icon={this.props.icon}
             currentUser={this.props.currentUser}
             otherMembers={this.props.otherMembers}
+            conversationAdminIds={this.props.conversationAdminIds}
             errors={this.props.errors.editConversationErrors}
             onBack={this.props.onBack}
             onRemoveMember={this.props.onRemoveMember}

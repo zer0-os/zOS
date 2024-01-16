121d726a60747be4a1db500d956f10b5b844fdc4
ratik21 <ratikjindal21@gmail.com>
2024-01-16 16:07:42 +0530

    call validateAndSetActiveConversationId on mount

--- src/messenger-main.tsx
+++ src/messenger-main.tsx
@@ -5,13 +5,14 @@ import { connectContainer } from './store/redux-container';
 import { Main } from './Main';
 import { ZUIProvider } from '@zero-tech/zui/ZUIProvider';
 import { Provider as AuthenticationContextProvider } from './components/authentication/context';
-import { setActiveConversationId } from './store/chat';
+import { setActiveConversationId, validateAndSetActiveConversationId } from './store/chat';
 
 export interface Properties {
   isAuthenticated: boolean;
 
   match: { params: { conversationId: string } };
   setActiveConversationId: (id: string) => void;
+  validateAndSetActiveConversationId: ({ id }: { id: string }) => void;
 }
 
 export class Container extends React.Component<Properties> {
@@ -23,12 +24,13 @@ export class Container extends React.Component<Properties> {
 
   static mapActions() {
     return {
+      validateAndSetActiveConversationId,
       setActiveConversationId,
     };
   }
 
   componentDidMount(): void {
-    this.props.setActiveConversationId(this.conversationId);
+    this.props.validateAndSetActiveConversationId({ id: this.conversationId });
   }
 
   componentDidUpdate(prevProps: Properties): void {

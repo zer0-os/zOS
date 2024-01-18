c3b17efd6b35ad6fd8529706427215c2992f8ea4
dominic <domw30@gmail.com>
2024-01-18 08:34:28 +0000

    fix: resolve failing tests due to merge conflict issue

--- src/store/chat/saga.test.ts
+++ src/store/chat/saga.test.ts
@@ -8,8 +8,6 @@ import { StoreBuilder } from '../test/store';
 import { User } from '../channels';
 import { testSaga } from 'redux-saga-test-plan';
 import { waitForChannelListLoad } from '../channels-list/saga';
-import { chat } from '../../lib/chat';
-import { setActiveConversationId } from '.';
 import { apiJoinRoom, getRoomIdForAlias } from '../../lib/chat';
 import { rawSetActiveConversationId } from '.';
 
@@ -17,12 +15,6 @@ const featureFlags = { allowJoinRoom: false };
 jest.mock('../../lib/feature-flags', () => ({
   featureFlags: featureFlags,
 }));
-
-const chatClient = {
-  getRoomIdForAlias: jest.fn(),
-  apiJoinRoom: jest.fn(),
-};
-
 
 describe(performValidateActiveConversation, () => {
   function subject(...args: Parameters<typeof expectSaga>) {
@@ -133,63 +125,6 @@ describe(performValidateActiveConversation, () => {
       .run();
 
     expect(storeState.chat.isConversationErrorDialogOpen).toBe(true);
-  });
-
-  it('gets the matrix roomId if the active conversation id is an alias', async () => {
-    const alias = '#wildebeest:matrix.org';
-    const conversationId = '!wildebeest:matrix.org';
-    const initialState = new StoreBuilder().withCurrentUser({ id: 'current-user' }).withConversationList({
-      id: '!wildebeest:matrix.org',
-      name: 'Conversation 1',
-      otherMembers: [{ userId: 'user-2' } as User],
-    });
-
-    const { storeState } = await subject(performValidateActiveConversation, alias)
-      .withReducer(rootReducer, initialState.build())
-      .provide([
-        [matchers.call.fn(chatClient.getRoomIdForAlias), conversationId],
-      ])
-      .call(chat.get)
-      .call([chatClient, chatClient.getRoomIdForAlias], alias)
-      .not.call([chatClient, chatClient.apiJoinRoom], conversationId)
-      .put(setActiveConversationId(conversationId))
-      .run();
-
-    expect(storeState.chat.activeConversationId).toBe(conversationId);
-  });
-
-  it('joins the conversation if the active conversation does not exist', async () => {
-    featureFlags.allowJoinRoom = true;
-
-    const initialState = new StoreBuilder().withCurrentUser({ id: 'current-user' });
-
-    await subject(performValidateActiveConversation, '#convo-not-exists')
-      .withReducer(rootReducer, initialState.build())
-      .provide([
-        [matchers.call.fn(chatClient.getRoomIdForAlias), undefined],
-      ])
-      .call(chat.get)
-      .call([chatClient, chatClient.apiJoinRoom], '#convo-not-exists')
-      .run();
-  });
-
-  it('joins the conversation if the active conversation id is an alias and the user is not a member', async () => {
-    featureFlags.allowJoinRoom = true;
-
-    const alias = '#some-other-convo:matrix.org';
-    const initialState = new StoreBuilder()
-      .withCurrentUser({ id: 'current-user' })
-      .withConversationList({ id: 'convo-1', name: 'Conversation 1', otherMembers: [{ userId: 'user-2' } as User] });
-
-    await subject(performValidateActiveConversation, alias)
-      .withReducer(rootReducer, initialState.build())
-      .provide([
-        [matchers.call.fn(chatClient.getRoomIdForAlias), '!some-other-convo:matrix.org'],
-      ])
-      .call(chat.get)
-      .call([chatClient, chatClient.getRoomIdForAlias], alias)
-      .call([chatClient, chatClient.apiJoinRoom], '!some-other-convo:matrix.org')
-      .run();
   });
 });
 

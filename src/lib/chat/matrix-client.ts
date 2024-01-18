de14829c6df560b297039a98f546f7dd3b9fc5ae
ratik21 <ratikjindal21@gmail.com>
2024-01-18 14:49:36 +0530

    move joinRoom to chat/saga/api

--- src/lib/chat/matrix-client.ts
+++ src/lib/chat/matrix-client.ts
@@ -625,28 +625,6 @@ export class MatrixClient implements IChatClient {
       .then((response) => response && response.room_id);
   }
 
-  async apiJoinRoom(aliasOrId: string): Promise<{ success: boolean; response: any; message: string }> {
-    try {
-      const response = await post('/matrix/room/join').send({
-        roomAliasORId: aliasOrId,
-      });
-      return {
-        success: true,
-        response: response.body,
-        message: 'OK',
-      };
-    } catch (error: any) {
-      if (error?.response?.status === 400) {
-        return {
-          success: false,
-          response: error.response.body.code,
-          message: error.response.body.message,
-        };
-      }
-      throw error;
-    }
-  }
-
   async fetchConversationsWithUsers(users: User[]) {
     const userMatrixIds = users.map((u) => u.matrixId);
     const rooms = await this.getRoomsUserIsIn();

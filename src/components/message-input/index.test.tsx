6b448380f2e880be6f87d34d74f52625ac401282
ratik21 <ratikjindal21@gmail.com>
2024-01-25 01:34:56 +0530

    update mentions style

--- src/components/message-input/index.test.tsx
+++ src/components/message-input/index.test.tsx
@@ -247,18 +247,18 @@ describe('MessageInput', () => {
   it('sorts by search string index', async function () {
     const getUsersForMentions = async (_searchString) =>
       Promise.resolve([
-        { id: 'd-2', display: '2-dale', profileImage: 'http://example.com/2' },
-        { id: 'd-3', display: '3--dale', profileImage: 'http://example.com/3' },
-        { id: 'd-1', display: 'dale', profileImage: 'http://example.com/' },
+        { id: 'd-2', display: '2-dale', profileImage: 'http://example.com/2', primaryZID: '0://d-2:dale' },
+        { id: 'd-3', display: '3--dale', profileImage: 'http://example.com/3', primaryZID: '0://d-3:dale' },
+        { id: 'd-1', display: 'dale', profileImage: 'http://example.com/', primaryZID: '0://d-1:dale' },
       ]);
     const wrapper = subject({ getUsersForMentions });
 
     const searchResults = await userSearch(wrapper, 'da');
 
     expect(searchResults).toEqual([
-      { display: 'dale', id: 'd-1', profileImage: 'http://example.com/' },
-      { display: '2-dale', id: 'd-2', profileImage: 'http://example.com/2' },
-      { display: '3--dale', id: 'd-3', profileImage: 'http://example.com/3' },
+      { display: 'dale', id: 'd-1', profileImage: 'http://example.com/', primaryZID: '0://d-1:dale' },
+      { display: '2-dale', id: 'd-2', profileImage: 'http://example.com/2', primaryZID: '0://d-2:dale' },
+      { display: '3--dale', id: 'd-3', profileImage: 'http://example.com/3', primaryZID: '0://d-3:dale' },
     ]);
   });
 

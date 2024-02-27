5c34b136c6b0f4ed9556f7d099a4ce055321be42
dominic <domw30@gmail.com>
2024-02-27 01:19:07 +0000

    feat(generate-prompt-component): add test coverage

---
+++ src/components/secure-backup/generate-prompt/index.test.tsx
@@ -0,0 +1,74 @@
+import { shallow } from 'enzyme';
+
+import { buttonLabelled, pressButton } from '../../../test/utils';
+import { GeneratePrompt, Properties } from '.';
+
+import { bem } from '../../../lib/bem';
+const c = bem('.secure-backup');
+
+describe(GeneratePrompt, () => {
+  const subject = (props: Partial<Properties> = {}) => {
+    const allProps: Properties = {
+      isSystemPrompt: false,
+      errorMessage: '',
+      onGenerate: () => null,
+      onClose: () => null,
+      ...props,
+    };
+
+    return shallow(<GeneratePrompt {...allProps} />);
+  };
+
+  it('renders the primary text for system prompt', function () {
+    const wrapper = subject({ isSystemPrompt: true });
+
+    const expectedText =
+      'Now that you’re messaging, we strongly advise that you backup your account to prevent losing access to your messages';
+
+    expect(wrapper.find(c('primary-text'))).toHaveText(expectedText);
+  });
+
+  it('renders the primary text for non-system prompt', function () {
+    const wrapper = subject({ isSystemPrompt: false });
+
+    const expectedText = 'Access your encrypted messages between devices and logins with an account backup.';
+
+    expect(wrapper.find(c('primary-text'))).toHaveText(expectedText);
+  });
+
+  it('shows the "Backup later" button for system prompt', function () {
+    const wrapper = subject({ isSystemPrompt: true });
+
+    expect(buttonLabelled(wrapper, 'Backup later')).toExist();
+  });
+
+  it('does not show the "Backup later" button for non-system prompt', function () {
+    const wrapper = subject({ isSystemPrompt: false });
+
+    expect(buttonLabelled(wrapper, 'Backup later')).not.toExist();
+  });
+
+  it('publishes onGenerate event', function () {
+    const onGenerate = jest.fn();
+    const wrapper = subject({ onGenerate });
+
+    pressButton(wrapper, 'Backup my account');
+
+    expect(onGenerate).toHaveBeenCalledOnce();
+  });
+
+  it('publishes onClose event for system prompt', function () {
+    const onClose = jest.fn();
+    const wrapper = subject({ onClose, isSystemPrompt: true });
+
+    pressButton(wrapper, 'Backup later');
+
+    expect(onClose).toHaveBeenCalledOnce();
+  });
+
+  it('renders an error message', () => {
+    const wrapper = subject({ errorMessage: 'test-error-message' });
+
+    expect(wrapper.find(c('error-message'))).toHaveText('test-error-message');
+  });
+});

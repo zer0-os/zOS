8570be86a35c2ec571bce9f4f903c97639997b49
dominic <domw30@gmail.com>
2024-02-27 11:12:56 +0000

    feat: add verify key phrase component

---
+++ src/components/secure-backup/verify-key-phrase/index.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+
+import { bemClassName } from '../../../lib/bem';
+
+import { Alert, Button, Input } from '@zero-tech/zui/components';
+
+import '../styles.scss';
+
+const cn = bemClassName('secure-backup');
+
+export interface State {
+  userInputKeyPhrase: string;
+}
+
+export interface Properties {
+  errorMessage: string;
+  onBack: () => void;
+  onSubmit: (keyPhrase: string) => void;
+}
+
+export class VerifyKeyPhrase extends React.Component<Properties, State> {
+  state = { userInputKeyPhrase: '' };
+
+  trackKeyPhrase = (value) => {
+    this.setState({ userInputKeyPhrase: value });
+  };
+
+  back = () => {
+    this.props.onBack();
+  };
+
+  submitKeyPhrase = () => {
+    this.props.onSubmit(this.keyPhrase);
+  };
+
+  get keyPhrase() {
+    return this.state.userInputKeyPhrase;
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <p {...cn('primary-text')}>Verify Backup Phrase</p>
+
+          <p {...cn('secondary-text')}>Confirm that you have safely stored your backup phrase by entering it below</p>
+
+          <div {...cn('input-container')}>
+            <Input placeholder='Enter your backup phrase' onChange={this.trackKeyPhrase} value={this.keyPhrase} />
+
+            {this.props.errorMessage && (
+              <Alert {...cn('alert')} variant='error'>
+                {this.props.errorMessage}
+              </Alert>
+            )}
+          </div>
+        </div>
+
+        <div {...cn('footer', 'has-secondary-button')}>
+          <Button {...cn('button')} onPress={this.back} variant='text'>
+            Back to phrase
+          </Button>
+
+          <Button {...cn('button')} onPress={this.submitKeyPhrase} isDisabled={!this.keyPhrase}>
+            Verify and complete backup
+          </Button>
+        </div>
+      </>
+    );
+  }
+}

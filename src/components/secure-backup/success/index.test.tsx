1d6c9ab1f25b46a34b1abb1944d54d396cdeb1bd
dominic <domw30@gmail.com>
2024-02-27 02:25:48 +0000

    feat(secure-backup-success): add test coverage

---
+++ src/components/secure-backup/success/index.test.tsx
@@ -0,0 +1,26 @@
+import { shallow } from 'enzyme';
+
+import { Success, Properties } from '.';
+
+import { Button } from '@zero-tech/zui/components';
+
+describe(Success, () => {
+  const subject = (props: Partial<Properties> = {}) => {
+    const allProps: Properties = {
+      successMessage: '',
+      onClose: () => null,
+      ...props,
+    };
+
+    return shallow(<Success {...allProps} />);
+  };
+
+  it('publishes onClose event', function () {
+    const onClose = jest.fn();
+    const wrapper = subject({ onClose });
+
+    wrapper.find(Button).simulate('close');
+
+    expect(onClose).toHaveBeenCalledOnce();
+  });
+});

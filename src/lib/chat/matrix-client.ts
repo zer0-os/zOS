360b79ff19a4eff7e0ac39e5ecd7040cec0c4b3c
Dale Fukami <dalefukami@users.noreply.github.com>
2024-02-28 13:06:26 -0700

    Use the modern way to check for cross-signing (#1585)

--- src/lib/chat/matrix-client.ts
+++ src/lib/chat/matrix-client.ts
@@ -165,7 +165,7 @@ export class MatrixClient implements IChatClient {
   }
 
   async getSecureBackup() {
-    const crossSigning = await this.matrix.getStoredCrossSigningForUser(this.userId);
+    const crossSigning = await this.doesUserHaveCrossSigning();
     const backupInfo = await this.matrix.checkKeyBackup();
     (backupInfo as any).isLegacy = !crossSigning;
     return backupInfo;
@@ -200,7 +200,7 @@ export class MatrixClient implements IChatClient {
       throw new Error('Backup broken or not there');
     }
 
-    const crossSigning = await this.matrix.getStoredCrossSigningForUser(this.userId);
+    const crossSigning = await this.doesUserHaveCrossSigning();
     if (crossSigning) {
       await this.restoreSecretStorageBackup(recoveryKey, backup);
     } else {
@@ -1027,6 +1027,10 @@ export class MatrixClient implements IChatClient {
     const key = this.matrix.keyBackupKeyFromRecoveryKey(this.secretStorageKey);
     return [keyId, key];
   };
+
+  private async doesUserHaveCrossSigning() {
+    return await this.matrix.getCrypto()?.userHasCrossSigningKeys(this.userId, true);
+  }
 
   /*
    * DEBUGGING

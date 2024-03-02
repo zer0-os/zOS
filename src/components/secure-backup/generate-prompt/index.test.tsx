12663dca97724d19d76e770e56665ddc7cb2ce31
ratik21 <ratikjindal21@gmail.com>
2024-03-02 15:19:41 +0530

    add test

--- src/components/secure-backup/generate-prompt/index.test.tsx
+++ src/components/secure-backup/generate-prompt/index.test.tsx
@@ -13,6 +13,7 @@ describe(GeneratePrompt, () => {
       errorMessage: '',
       onGenerate: () => null,
       onClose: () => null,
+      onLearnMore: () => null,
       ...props,
     };
 
@@ -70,5 +71,15 @@ describe(GeneratePrompt, () => {
     const wrapper = subject({ errorMessage: '' });
 
     expect(wrapper.find(c('error-message'))).not.toExist();
+  });
+
+  it('publishes onLearnMore event', function () {
+    const onLearnMore = jest.fn();
+    const wrapper = subject({ onLearnMore });
+
+    const button = wrapper.find(c('learn-more'));
+    button.simulate('click');
+
+    expect(onLearnMore).toHaveBeenCalledOnce();
   });
 });

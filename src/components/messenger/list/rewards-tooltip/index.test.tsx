2d199df507749df3374d904fde56c907fcde0e6e
ratik21 <ratikjindal21@gmail.com>
2024-03-20 22:18:17 +0530

    close tooltip

--- src/components/messenger/list/rewards-tooltip/index.test.tsx
+++ src/components/messenger/list/rewards-tooltip/index.test.tsx
@@ -18,4 +18,19 @@ describe(RewardsTooltip, () => {
 
     expect(wrapper.find(TooltipPopup).prop('content')).toEqual('You earned $3.14');
   });
+
+  it('closes the tooltip after 3 seconds', function () {
+    jest.useFakeTimers();
+    const wrapper = subject({ meowPreviousDayInUSD: '$3.14' });
+
+    expect(wrapper.find(TooltipPopup).prop('open')).toEqual(true);
+
+    // still open after 1s
+    jest.advanceTimersByTime(1000);
+    expect(wrapper.find(TooltipPopup).prop('open')).toEqual(true);
+
+    // closed after 1s + 2s
+    jest.advanceTimersByTime(2000);
+    expect(wrapper.find(TooltipPopup).prop('open')).toEqual(false);
+  });
 });

b1c852c8b7ce6b8be20746e7e62d251c887e2bfb
Dale Fukami <dale@zero.tech>
2024-03-26 12:54:03 -0600

    Add Apps icon

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { WorldPanelItem } from './world-panel-item';
-import { IconMessageSquare2 } from '@zero-tech/zui/icons';
+import { IconDotsGrid, IconMessageSquare2 } from '@zero-tech/zui/icons';
 import { featureFlags } from '../../lib/feature-flags';
 import { bemClassName } from '../../lib/bem';
 
@@ -16,6 +16,7 @@ export class AppBar extends React.Component<Properties> {
     return (
       <div {...cn('', !featureFlags.enableAppBar && 'disabled')}>
         <WorldPanelItem Icon={IconMessageSquare2} isActive />
+        <WorldPanelItem Icon={IconDotsGrid} isActive={false} />
       </div>
     );
   }

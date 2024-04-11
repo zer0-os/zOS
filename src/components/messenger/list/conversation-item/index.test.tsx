e25317170bedecf8e8a80256c17fbbd46470f237
dominic <domw30@gmail.com>
2024-04-11 14:07:52 +0100

    refactor(conversation-item): improve tests to follow existing test conventions

--- src/components/messenger/list/conversation-item/index.test.tsx
+++ src/components/messenger/list/conversation-item/index.test.tsx
@@ -4,9 +4,13 @@ import { shallow } from 'enzyme';
 import { ConversationItem, Properties } from '.';
 import moment from 'moment';
 import { ContentHighlighter } from '../../../content-highlighter';
-import { MoreMenu } from './more-menu';
+import { Avatar } from '@zero-tech/zui/components';
+import { bem } from '../../../../lib/bem';
+import Tooltip from '../../../tooltip';
 
-describe('ConversationItem', () => {
+const c = bem('.conversation-item');
+
+describe(ConversationItem, () => {
   const subject = (props: Partial<Properties>) => {
     const allProps: Properties = {
       filter: '',
@@ -24,44 +28,30 @@ describe('ConversationItem', () => {
 
   it('renders other members avatar for one on one', function () {
     const wrapper = subject({
-      conversation: {
-        isOneOnOne: true,
-        ...convoWith({ firstName: 'Johnny', profileImage: 'image-url' }),
-      },
+      conversation: { isOneOnOne: true, ...convoWith({ profileImage: 'image-url' }) },
     });
 
-    expect(wrapper.find('Avatar').prop('imageURL')).toEqual('image-url');
+    expect(wrapper.find(Avatar)).toHaveProp('imageURL', 'image-url');
   });
 
   it('renders group icon for group conversation with an image', function () {
     const wrapper = subject({
-      conversation: {
-        icon: 'custom-image-url',
-        ...convoWith({ firstName: 'one' }, { firstName: 'two' }),
-      },
+      conversation: { icon: 'custom-image-url', ...convoWith({ firstName: 'one' }, { firstName: 'two' }) },
     });
 
-    expect(wrapper.find('Avatar').prop('imageURL')).toEqual('custom-image-url');
+    expect(wrapper.find(Avatar)).toHaveProp('imageURL', 'custom-image-url');
   });
 
   it('renders default group icon if group conversation has no image', function () {
-    const wrapper = subject({
-      conversation: {
-        icon: 'https://static.sendbird.com/sample/cover/cover_11.jpg',
-        ...convoWith({ firstName: 'one' }, { firstName: 'two' }),
-      },
-    });
+    const wrapper = subject({ conversation: { ...convoWith({ firstName: 'one' }, { firstName: 'two' }) } });
 
-    expect(wrapper).toHaveElement('.conversation-item__group-icon');
+    expect(wrapper).toHaveElement(c('group-icon'));
   });
 
   it('renders conversation title for one on one', function () {
-    const wrapper = subject({
-      conversation: convoWith({ firstName: 'Johnny', lastName: 'Cash' }),
-    });
+    const wrapper = subject({ conversation: convoWith({ firstName: 'Johnny', lastName: 'Cash' }) });
 
-    const displayChatNames = wrapper.find('.conversation-item__name').map((node) => node.text());
-    expect(displayChatNames).toStrictEqual(['Johnny Cash']);
+    expect(title(wrapper)).toStrictEqual('Johnny Cash');
   });
 
   it('renders conversation title for multi-members', function () {
@@ -74,111 +64,70 @@ describe('ConversationItem', () => {
 
   it('renders conversation title for named conversation', function () {
     const wrapper = subject({
-      conversation: {
-        ...convoWith({ firstName: 'Johnny', lastName: 'Cash' }),
-        name: 'My Named Conversation',
-      },
+      conversation: { ...convoWith({ firstName: 'Johnny', lastName: 'Cash' }), name: 'My Named Conversation' },
     });
 
     expect(title(wrapper)).toStrictEqual('My Named Conversation');
   });
 
   it('publishes click event', function () {
-    const handleMemberClick = jest.fn();
-    const wrapper = subject({
-      onClick: handleMemberClick,
-      conversation: {
-        ...convoWith(),
-        id: 'test-conversation-id',
-      },
-    });
+    const onClick = jest.fn();
+    const wrapper = subject({ onClick, conversation: { ...convoWith(), id: 'test-conversation-id' } });
 
-    wrapper.find('.conversation-item').first().simulate('click');
+    wrapper.find(c('')).first().simulate('click');
 
-    expect(handleMemberClick).toHaveBeenCalledWith('test-conversation-id');
+    expect(onClick).toHaveBeenCalledWith('test-conversation-id');
   });
 
   it('does not show unread count if there are no unread messages', function () {
     const wrapper = subject({
-      conversation: {
-        id: 'id',
-        unreadCount: 0,
-        otherMembers: [],
-      } as any,
+      conversation: { id: 'id', unreadCount: 0, otherMembers: [] } as any,
     });
 
-    expect(wrapper).not.toHaveElement('.conversation-item__unread-count');
+    expect(wrapper).not.toHaveElement(c('unread-count'));
   });
 
   it('shows unread message count', function () {
-    const wrapper = subject({
-      conversation: {
-        id: 'id',
-        unreadCount: 7,
-        otherMembers: [],
-      } as any,
-    });
+    const wrapper = subject({ conversation: { id: 'id', unreadCount: 7, otherMembers: [] } as any });
 
-    expect(wrapper.find('.conversation-item__unread-count').text()).toEqual('7');
+    expect(wrapper.find(c('unread-count'))).toHaveText('7');
   });
 
   it('renders the message preview', function () {
-    const messagePreview = 'I said something here';
+    const wrapper = subject({ conversation: { messagePreview: 'I said something here', otherMembers: [] } as any });
 
-    const wrapper = subject({ conversation: { messagePreview, otherMembers: [] } as any });
-
-    expect(wrapper.find(ContentHighlighter).prop('message')).toEqual(messagePreview);
+    expect(wrapper.find(ContentHighlighter)).toHaveProp('message', 'I said something here');
   });
 
   it('renders the previewDisplayDate', function () {
-    const previewDisplayDate = 'Aug 1, 2021';
+    const wrapper = subject({ conversation: { previewDisplayDate: 'Aug 1, 2021', otherMembers: [] } as any });
 
-    const wrapper = subject({ conversation: { previewDisplayDate, otherMembers: [] } as any });
-
-    expect(wrapper.find('.conversation-item__timestamp').text()).toEqual(previewDisplayDate);
-  });
-
-  it('prevents click event propagation from MoreMenu to ConversationItem', () => {
-    const onClick = jest.fn();
-    const wrapper = subject({ conversation: convoWith(), onClick });
-
-    wrapper.find('.conversation-item').simulate('mouseEnter');
-    wrapper.find(MoreMenu).simulate('click');
-
-    expect(onClick).not.toHaveBeenCalled();
+    expect(wrapper.find(c('timestamp'))).toHaveText('Aug 1, 2021');
   });
 
   describe('status', () => {
     it('renders inactive if no other members are online', function () {
       const wrapper = subject({
-        conversation: {
-          icon: 'icon-url',
-          ...convoWith({ isOnline: false }, { isOnline: false }),
-        },
+        conversation: { icon: 'icon-url', ...convoWith({ isOnline: false }, { isOnline: false }) },
       });
 
-      expect(wrapper.find('Avatar').prop('statusType')).toEqual('offline');
+      expect(wrapper.find(Avatar)).toHaveProp('statusType', 'offline');
     });
 
     it('renders active if any other members are online', function () {
       const wrapper = subject({
-        conversation: {
-          icon: 'icon-url',
-          ...convoWith({ isOnline: false }, { isOnline: true }),
-        },
+        conversation: { icon: 'icon-url', ...convoWith({ isOnline: false }, { isOnline: true }) },
       });
 
-      expect(wrapper.find('Avatar').prop('statusType')).toEqual('active');
+      expect(wrapper.find(Avatar)).toHaveProp('statusType', 'active');
     });
   });
 
   describe('tooltip', () => {
     it('renders Online if single member is currently online', function () {
-      const wrapper = subject({
-        conversation: convoWith({ isOnline: true }),
-      });
+      const wrapper = subject({ conversation: convoWith({ isOnline: true }) });
 
-      expect(wrapper.find('Tooltip').prop('overlay')).toEqual('Online');
+      expect(wrapper.find(Tooltip)).toHaveProp('overlay', 'Online');
     });
 
     it('renders last seen time if single member has previously been online', function () {
@@ -186,7 +135,7 @@ describe('ConversationItem', () => {
         conversation: convoWith({ lastSeenAt: moment().subtract(2, 'days').toISOString() }),
       });
 
-      expect(wrapper.find('Tooltip').prop('overlay')).toEqual('Last Seen: 2 days ago');
+      expect(wrapper.find(Tooltip)).toHaveProp('overlay', 'Last Seen: 2 days ago');
     });
 
     it('renders member names when multiple other members', function () {
@@ -194,13 +143,13 @@ describe('ConversationItem', () => {
         conversation: convoWith({ firstName: 'Johnny', lastName: 'Cash' }, { firstName: 'Jackie', lastName: 'Chan' }),
       });
 
-      expect(wrapper.find('Tooltip').prop('overlay')).toEqual('Johnny Cash, Jackie Chan');
+      expect(wrapper.find(Tooltip)).toHaveProp('overlay', 'Johnny Cash, Jackie Chan');
     });
   });
 });
 
 function title(wrapper) {
-  return wrapper.find('.conversation-item__name').text();
+  return wrapper.find(c('name')).text();
 }
 
 function convoWith(...otherMembers): any {

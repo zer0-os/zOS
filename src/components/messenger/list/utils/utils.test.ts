15c9ca5406ab275323a066c0e069a0f2db77883e
DomW <domw30@gmail.com>
2024-04-19 00:34:14 +0100

    refactor: update last seen helper function output (#1729)
    * refactor: update last seen helper logic
    
    * refactor: remove test

--- src/components/messenger/list/utils/utils.test.ts
+++ src/components/messenger/list/utils/utils.test.ts
@@ -1,4 +1,4 @@
-import { isUserAdmin, sortMembers } from './utils';
+import { isUserAdmin, lastSeenText, sortMembers } from './utils';
 import { User } from '../../../../store/channels';
 
 describe('sortMembers', () => {
@@ -42,5 +42,22 @@ describe('isUserAdmin', () => {
     const adminIds: string[] = [];
 
     expect(isUserAdmin(user, adminIds)).toBe(false);
+  });
+});
+
+describe('lastSeenText', () => {
+  it('returns "Online" if the user is currently online', () => {
+    const user = { isOnline: true };
+    expect(lastSeenText(user)).toEqual('Online');
+  });
+
+  it('returns an empty string if lastSeenAt is more than 6 months ago', () => {
+    const user = { isOnline: false, lastSeenAt: new Date('2020-01-01') };
+    expect(lastSeenText(user)).toEqual('');
+  });
+
+  it('returns an empty string if lastSeenAt is null', () => {
+    const user = { isOnline: false, lastSeenAt: null };
+    expect(lastSeenText(user)).toEqual('');
   });
 });

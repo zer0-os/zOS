275a569d1dfeb10226cfe5134af8a847c21227ef
DomW <domw30@gmail.com>
2024-05-08 13:57:20 +0100

    feat(user-profile): add logout and invite actions (#1838)
    * feat(authentication): close user profile on terminate
    
    * feat(user-profile): add logout and invite actions
    
    * refactor: remove empty tags

--- src/components/messenger/user-profile/index.tsx
+++ src/components/messenger/user-profile/index.tsx
@@ -3,8 +3,11 @@ import * as React from 'react';
 import { bemClassName } from '../../../lib/bem';
 
 import { PanelHeader } from '../list/panel-header';
-import { Image } from '@zero-tech/zui/components';
-import { IconCurrencyEthereum } from '@zero-tech/zui/icons';
+import { Image, Modal } from '@zero-tech/zui/components';
+import { Button, Variant as ButtonVariant, Color as ButtonColor } from '@zero-tech/zui/components/Button';
+import { IconCurrencyEthereum, IconLogOut3, IconPlus } from '@zero-tech/zui/icons';
+import { LogoutConfirmationModalContainer } from '../../logout-confirmation-modal/container';
+import { InviteDialogContainer } from '../../invite-dialog/container';
 
 import './styles.scss';
 
@@ -14,13 +17,43 @@ export interface Properties {
   name: string;
   image: string;
   subHandle?: string;
+  isLogoutOpen: boolean;
 
   onBack: () => void;
+  onOpenLogoutDialog: () => void;
 }
 
-export class UserProfile extends React.Component<Properties> {
+interface State {
+  isInviteDialogOpen: boolean;
+}
+
+export class UserProfile extends React.Component<Properties, State> {
+  state = {
+    isInviteDialogOpen: false,
+  };
+
   navigateBack = () => {
     this.props.onBack();
+  };
+
+  openInviteDialog = () => {
+    this.setState({ isInviteDialogOpen: true });
+  };
+
+  closeInviteDialog = () => {
+    this.setState({ isInviteDialogOpen: false });
+  };
+
+  renderInviteDialog = (): JSX.Element => {
+    return (
+      <Modal open={this.state.isInviteDialogOpen} onOpenChange={this.closeInviteDialog}>
+        <InviteDialogContainer onClose={this.closeInviteDialog} />
+      </Modal>
+    );
+  };
+
+  openLogoutDialog = () => {
+    this.props.onOpenLogoutDialog();
   };
 
   renderDetails = () => {
@@ -44,14 +77,50 @@ export class UserProfile extends React.Component<Properties> {
     );
   };
 
+  renderActions() {
+    return (
+      <Button
+        {...cn('action-button')}
+        variant={ButtonVariant.Secondary}
+        onPress={this.openInviteDialog}
+        startEnhancer={<IconPlus size={20} isFilled />}
+      >
+        Invite Friends
+      </Button>
+    );
+  }
+
+  renderFooter() {
+    return (
+      <div {...cn('footer')}>
+        <Button
+          {...cn('footer-button')}
+          variant={ButtonVariant.Secondary}
+          color={ButtonColor.Greyscale}
+          onPress={this.openLogoutDialog}
+          startEnhancer={<IconLogOut3 size={20} />}
+        >
+          Log Out
+        </Button>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div {...cn()}>
         <div {...cn('header-container')}>
-          <PanelHeader title={'Profile'} onBack={this.props.onBack} />
+          <PanelHeader title={'Profile'} onBack={this.navigateBack} />
         </div>
 
-        <div {...cn('body')}>{this.renderDetails()}</div>
+        <div {...cn('body')}>
+          {this.renderDetails()}
+          {this.renderActions()}
+        </div>
+
+        {this.renderFooter()}
+        {this.renderInviteDialog()}
+        {this.props.isLogoutOpen && <LogoutConfirmationModalContainer />}
       </div>
     );
   }

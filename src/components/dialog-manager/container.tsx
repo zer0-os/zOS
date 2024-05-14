1b0547c793d99ffb53b2a27c1de6422aaf0f3992
dominic <domw30@gmail.com>
2024-05-15 00:44:27 +0100

    feat(dialog-manager): add rewards dialog

--- src/components/dialog-manager/container.tsx
+++ src/components/dialog-manager/container.tsx
@@ -4,14 +4,18 @@ import { connectContainer } from '../../store/redux-container';
 import { SecureBackupContainer } from '../secure-backup/container';
 import { closeBackupDialog } from '../../store/matrix';
 import { LogoutConfirmationModalContainer } from '../logout-confirmation-modal/container';
+import { RewardsModalContainer } from '../rewards-modal/container';
+import { closeRewardsDialog } from '../../store/rewards';
 
 export interface PublicProperties {}
 
 export interface Properties extends PublicProperties {
   displayLogoutModal: boolean;
   isBackupDialogOpen: boolean;
+  isRewardsDialogOpen: boolean;
 
   closeBackupDialog: () => void;
+  closeRewardsDialog: () => void;
 }
 
 export class Container extends React.Component<Properties> {
@@ -19,20 +23,26 @@ export class Container extends React.Component<Properties> {
     const {
       authentication: { displayLogoutModal },
       matrix: { isBackupDialogOpen },
+      rewards,
     } = state;
 
     return {
       displayLogoutModal,
       isBackupDialogOpen,
+      isRewardsDialogOpen: rewards.showRewardsInPopup,
     };
   }
 
   static mapActions(_props: Properties): Partial<Properties> {
-    return { closeBackupDialog };
+    return { closeBackupDialog, closeRewardsDialog };
   }
 
   closeBackup = () => {
     this.props.closeBackupDialog();
+  };
+
+  closeRewards = () => {
+    this.props.closeRewardsDialog();
   };
 
   renderSecureBackupDialog = (): JSX.Element => {
@@ -43,11 +53,16 @@ export class Container extends React.Component<Properties> {
     return <LogoutConfirmationModalContainer />;
   };
 
+  renderRewardsDialog = (): JSX.Element => {
+    return <RewardsModalContainer onClose={this.closeRewards} />;
+  };
+
   render() {
     return (
       <>
         {this.props.isBackupDialogOpen && this.renderSecureBackupDialog()}
         {this.props.displayLogoutModal && this.renderLogoutDialog()}
+        {this.props.isRewardsDialogOpen && this.renderRewardsDialog()}
       </>
     );
   }

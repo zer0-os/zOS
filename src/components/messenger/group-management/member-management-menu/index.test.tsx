d8c3646d4b25f4610b9e894b3d0244ace92f48e0
ratik21 <ratikjindal21@gmail.com>
2024-05-22 05:50:44 +0530

    update member-management-menu to reflect Remove mod

--- src/components/messenger/group-management/member-management-menu/index.test.tsx
+++ src/components/messenger/group-management/member-management-menu/index.test.tsx
@@ -4,6 +4,12 @@ import { Properties, MemberManagementMenu } from '.';
 import { DropdownMenu } from '@zero-tech/zui/components';
 import { MemberManagementAction } from '../../../../store/group-management';
 
+const featureFlags = { allowModeratorActions: true };
+
+jest.mock('../../../../lib/feature-flags', () => ({
+  featureFlags: featureFlags,
+}));
+
 describe(MemberManagementMenu, () => {
   const subject = (props: Partial<Properties> = {}) => {
     const allProps: Properties = {
@@ -16,15 +22,19 @@ describe(MemberManagementMenu, () => {
     return shallow(<MemberManagementMenu {...allProps} />);
   };
 
-  it('does not render the menu if user cannot do anything', function () {
-    const wrapper = subject({
-      canRemove: false,
-    });
-
-    expect(wrapper).not.toHaveElement(DropdownMenu);
+  it('renders Make Mod menu item when user is NOT a moderator', () => {
+    const wrapper = subject({ isUserModerator: false });
+    expect(menuItem(wrapper, 'make-mod')).toBeDefined();
+    expect(menuItem(wrapper, 'remove-mod')).toBeUndefined();
   });
 
-  describe('Remove', () => {
+  it('renders Remove Mod menu item when user is a moderator', () => {
+    const wrapper = subject({ isUserModerator: true });
+    expect(menuItem(wrapper, 'remove-mod')).toBeDefined();
+    expect(menuItem(wrapper, 'make-mod')).toBeUndefined();
+  });
+
+  describe('Member Management Actions', () => {
     it('publishes onRemoveMember event when Remove member is clicked', () => {
       const onOpenMemberManagement = jest.fn();
       const wrapper = subject({ onOpenMemberManagement, canRemove: true });
@@ -32,6 +42,24 @@ describe(MemberManagementMenu, () => {
       selectItem(wrapper, 'remove-member');
 
       expect(onOpenMemberManagement).toBeCalledWith(MemberManagementAction.RemoveMember);
+    });
+
+    it('publishes onMakeMod event when Make Mod is clicked', () => {
+      const onOpenMemberManagement = jest.fn();
+      const wrapper = subject({ onOpenMemberManagement });
+
+      selectItem(wrapper, 'make-mod');
+
+      expect(onOpenMemberManagement).toBeCalledWith(MemberManagementAction.MakeModerator);
+    });
+
+    it('publishes onRemoveMod event when Remove Mod is clicked', () => {
+      const onOpenMemberManagement = jest.fn();
+      const wrapper = subject({ onOpenMemberManagement, isUserModerator: true });
+
+      selectItem(wrapper, 'remove-mod');
+
+      expect(onOpenMemberManagement).toBeCalledWith(MemberManagementAction.RemoveModertor);
     });
   });
 });

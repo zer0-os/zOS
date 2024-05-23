5eeefa53ce4b01e8b6bc3f951023f32bd0012d04
Ratik Jindal <ratikjindal21@gmail.com>
2024-05-23 09:35:37 +0530

    update member management menu texts for: `remove member`, `remove mod` (#1917)
    * update UI
    
    * fix

--- src/components/messenger/group-management/member-management-menu/index.tsx
+++ src/components/messenger/group-management/member-management-menu/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { IconDotsHorizontal, IconUserCheck1, IconUserX1 } from '@zero-tech/zui/icons';
+import { IconDotsHorizontal, IconUserCheck1, IconUserRight1, IconUserX1 } from '@zero-tech/zui/icons';
 import { DropdownMenu } from '@zero-tech/zui/components';
 
 import './styles.scss';
@@ -50,7 +50,7 @@ export class MemberManagementMenu extends React.Component<Properties> {
         menuItems.push({
           id: 'remove-mod',
           className: 'remove-mod',
-          label: this.renderMenuItem(<IconUserX1 size={20} />, 'Remove Mod'),
+          label: this.renderMenuItem(<IconUserRight1 size={20} />, 'Remove As Mod'),
           onSelect: this.onRemoveMod,
         });
       } else {
@@ -67,7 +67,7 @@ export class MemberManagementMenu extends React.Component<Properties> {
       menuItems.push({
         id: 'remove-member',
         className: 'remove-member',
-        label: this.renderMenuItem(<IconUserX1 size={20} />, 'Remove'),
+        label: this.renderMenuItem(<IconUserX1 size={20} />, 'Remove Member'),
         onSelect: this.onRemoveMember,
       });
     }

01e1d183af7a972294fb0827f4403a192e6978eb
ratik21 <ratikjindal21@gmail.com>
2024-05-30 23:33:36 +0530

    fix

--- src/lib/chat/matrix-client.ts
+++ src/lib/chat/matrix-client.ts
@@ -749,7 +749,10 @@ export class MatrixClient implements IChatClient {
   async removeUser(roomId, user): Promise<void> {
     await this.waitForConnection();
 
-    await this.matrix.setPowerLevel(roomId, user.matrixId, PowerLevels.Viewer); // reset user power_level first
+    const isRoomAdmin = await this.isCurrentUserRoomAdmin(roomId);
+    if (isRoomAdmin) {
+      await this.matrix.setPowerLevel(roomId, user.matrixId, PowerLevels.Viewer); // reset user power_level first
+    }
     await this.matrix.kick(roomId, user.matrixId);
   }
 
@@ -763,6 +766,12 @@ export class MatrixClient implements IChatClient {
   async removeUserAsModerator(roomId: string, user): Promise<void> {
     await this.waitForConnection();
     await this.matrix.setPowerLevel(roomId, user.matrixId, PowerLevels.Viewer);
+  }
+
+  private async isCurrentUserRoomAdmin(roomId: string): Promise<boolean> {
+    const room = this.matrix.getRoom(roomId);
+    const [admins, _mods] = this.getRoomAdminsAndMods(room);
+    return admins.includes(this.userId);
   }
 
   private async onMessageUpdated(event): Promise<void> {

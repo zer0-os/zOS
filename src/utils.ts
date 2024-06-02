1ac1a3de19f9640d63fbb9dd50158ad5e4ad5331
DomW <domw30@gmail.com>
2024-06-02 20:44:17 +0100

    feat(main): handle and render animated or static background to 'main' with newly added background state (#1956)
    * feat(user-settings): add settings panel
    
    * refactor: update test description
    
    * feat(user-profile): add user profile saga
    
    * feat: update reducers and sagas with user profile
    
    * refactor(edit-profile-saga): remove user profile actions and functions and uses at update container and component level
    
    * feat(user-profile): wire up user settings panel to profile flow
    
    * refactor(edit-group-panel): remove unused open profile prop and attribute
    
    * feat(background-saga): add saga to handle local background preference state storage
    
    * feat(main): handle and render animated or static background to 'main' with newly added state

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/react';
 import { isDesktopApp } from '@todesktop/client-core/platform/todesktop';
 import { config } from './config';
+import { MainBackground } from './store/background';
 
 export const isElectron = (): boolean => isDesktopApp();
 
@@ -13,3 +14,25 @@ export const initializeErrorBoundary = () => {
     ...config.sentry,
   });
 };
+
+export function getMainBackgroundClass(selectedMainBackground) {
+  switch (selectedMainBackground) {
+    case MainBackground.AnimatedGreenParticles:
+    case MainBackground.AnimatedBlackParticles:
+      return 'animated';
+    case MainBackground.StaticGreenParticles:
+    default:
+      return 'static-green-particles';
+  }
+}
+
+export function getMainBackgroundVideoSrc(selectedMainBackground) {
+  switch (selectedMainBackground) {
+    case MainBackground.AnimatedGreenParticles:
+      return `${config.videoAssetsPath}GreenParticlesBG.mp4`;
+    case MainBackground.AnimatedBlackParticles:
+      return `${config.videoAssetsPath}BlackParticlesBG.mp4`;
+    default:
+      return '';
+  }
+}

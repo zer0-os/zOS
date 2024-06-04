c24de2c6abdf50b6a31b540029ae358236653781
Brett <12437916+colbr@users.noreply.github.com>
2024-06-04 15:50:09 +1000

    chore: vite svgr
    Handles importing SVG as as React component

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // svgr options
+      },
+    }),
+  ],
   resolve: {
     alias: {
       react: resolve('./node_modules/react'),

5e0bb9ad2baddc3718fcfbfba8105ceae7ad8e36
DomW <domw30@gmail.com>
2024-06-04 05:31:00 +0100

    feat(settings-panel): add read receipt preferences to settings panel and separate settings into sections (#1970)
    * refactor(matrix-client): update set read receipt function argument type and remove flag
    
    * feat(user-profile-saga): add saga functions to handle setting private and public receipts
    
    * feat(message-info-saga): add stage state to handle panel navigation
    
    * feat(group-management-flow): close message info when resetting group management state
    
    * feat(channels-saga): remove replaced set read receipt preference saga function and action
    
    * feat(message-info-panel): add message content, timestamp and scrolling to info panel
    
    * feat(sidekick): add message info container to the sidekick and render
    
    * feat(settings-panel): add read receipt preferences to settings panel and separate settings into sections

--- src/components/messenger/user-profile/settings-panel/container.tsx
+++ src/components/messenger/user-profile/settings-panel/container.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 
 import { RootState } from '../../../../store/reducer';
 import { connectContainer } from '../../../../store/redux-container';
+import { MainBackground, setMainBackground } from '../../../../store/background';
+import { privateReadReceipts, publicReadReceipts } from '../../../../store/user-profile';
 
 import { SettingsPanel } from '.';
-import { MainBackground, setMainBackground } from '../../../../store/background';
 
 export interface PublicProperties {
   onClose?: () => void;
@@ -12,28 +13,35 @@ export interface PublicProperties {
 
 export interface Properties extends PublicProperties {
   selectedMainBackground: MainBackground;
+  isPublicReadReceipts: boolean;
 
   setMainBackground: (payload: { selectedBackground: MainBackground }) => void;
+  privateReadReceipts: () => void;
+  publicReadReceipts: () => void;
 }
 
 export class Container extends React.Component<Properties> {
   static mapState(state: RootState) {
-    const { background } = state;
+    const { background, userProfile } = state;
     return {
       selectedMainBackground: background.selectedMainBackground,
+      isPublicReadReceipts: userProfile.isPublicReadReceipts,
     };
   }
 
   static mapActions(_props: Properties): Partial<Properties> {
-    return { setMainBackground };
+    return { setMainBackground, privateReadReceipts, publicReadReceipts };
   }
 
   render() {
     return (
       <SettingsPanel
         selectedMainBackground={this.props.selectedMainBackground}
+        isPublicReadReceipts={this.props.isPublicReadReceipts}
         onBack={this.props.onClose}
         onSetMainBackground={this.props.setMainBackground}
+        onPrivateReadReceipts={this.props.privateReadReceipts}
+        onPublicReadReceipts={this.props.publicReadReceipts}
       />
     );
   }

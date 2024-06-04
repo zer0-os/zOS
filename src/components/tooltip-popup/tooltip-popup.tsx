cb294cd534e4240341d56f92a52f9e75b13bca52
Brett <12437916+colbr@users.noreply.github.com>
2024-06-04 15:13:09 +1000

    chore: update SVG imports
    Vite imports are a little different

--- src/components/tooltip-popup/tooltip-popup.tsx
+++ src/components/tooltip-popup/tooltip-popup.tsx
@@ -5,7 +5,7 @@ import type { ReactNode } from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { IconXClose } from '@zero-tech/zui/icons';
 import { IconButton } from '@zero-tech/zui/components';
-import { ReactComponent as TooltipHead } from '../../tooltip-head.svg';
+import TooltipHead from '../../tooltip-head.svg?react';
 
 const c = bem('tooltip-popup');
 

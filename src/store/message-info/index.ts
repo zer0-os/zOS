d500aa0b442cc523681240e60d4c595c0ce18f01
dominic <domw30@gmail.com>
2024-06-04 04:25:46 +0100

    feat(message-info-saga): add stage state to handle panel navigation

--- src/store/message-info/index.ts
+++ src/store/message-info/index.ts
@@ -1,5 +1,10 @@
 import { PayloadAction, createAction, createSlice } from '@reduxjs/toolkit';
 
+export enum Stage {
+  None = 'none',
+  Overview = 'overview',
+}
+
 export enum SagaActionTypes {
   OpenMessageInfo = 'message-info/open-message-info',
   CloseMessageInfo = 'message-info/close-message-info',
@@ -9,10 +14,12 @@ export const openMessageInfo = createAction<{ roomId: string; messageId: number 
 export const closeMessageInfo = createAction(SagaActionTypes.CloseMessageInfo);
 
 export type MessageInfoState = {
+  stage: Stage;
   selectedMessageId: string;
 };
 
 export const initialState: MessageInfoState = {
+  stage: Stage.None,
   selectedMessageId: '',
 };
 
@@ -20,11 +27,14 @@ const slice = createSlice({
   name: 'message-info',
   initialState,
   reducers: {
+    setStage: (state, action: PayloadAction<Stage>) => {
+      state.stage = action.payload;
+    },
     setSelectedMessageId: (state, action: PayloadAction<string>) => {
       state.selectedMessageId = action.payload;
     },
   },
 });
 
-export const { setSelectedMessageId } = slice.actions;
+export const { setSelectedMessageId, setStage } = slice.actions;
 export const { reducer } = slice;
--- src/store/message-info/saga.ts
+++ src/store/message-info/saga.ts
@@ -1,9 +1,10 @@
 import { call, fork, put, take, takeLatest } from 'redux-saga/effects';
 
 import { getMessageReadReceipts } from '../../lib/chat';
-import { SagaActionTypes, setSelectedMessageId } from './index';
+import { SagaActionTypes, Stage, setSelectedMessageId, setStage } from './index';
 import { Events, getAuthChannel } from '../authentication/channels';
 import { updateReadByUsers } from '../messages/saga';
+import { resetConversationManagement } from '../group-management/saga';
 
 function* authWatcher() {
   const channel = yield call(getAuthChannel);
@@ -16,6 +17,8 @@ function* authWatcher() {
 export function* openOverview(action) {
   const { roomId, messageId } = action.payload;
 
+  yield call(resetConversationManagement);
+  yield put(setStage(Stage.Overview));
   yield put(setSelectedMessageId(messageId));
 
   const receipts = yield call(getMessageReadReceipts, roomId, messageId);
@@ -24,6 +27,7 @@ export function* openOverview(action) {
 }
 
 export function* closeOverview() {
+  yield put(setStage(Stage.None));
   yield put(setSelectedMessageId(''));
 }
 

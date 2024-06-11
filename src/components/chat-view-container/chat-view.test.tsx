a118a889dd47dc5e448b7052ca2c03db8827866f
dominic <domw30@gmail.com>
2024-06-11 13:59:06 +0100

    refactor: amend test date

--- src/components/chat-view-container/chat-view.test.tsx
+++ src/components/chat-view-container/chat-view.test.tsx
@@ -288,13 +288,14 @@ describe('ChatView', () => {
     });
 
     it('returns the formatted date for the same year', () => {
-      const currentYearDate = moment('2024-06-11'); // Example date within the same year
+      // setting the date here is not ideal as at some point this will fail
+      const currentYearDate = moment('2024-12-11'); // Example date within the same year
       const messages = [
         { id: 111, message: 'what', createdAt: currentYearDate.valueOf() } as MessageModel,
       ];
 
       const wrapper = subject({ messages });
-      expect(wrapper.find('.message__header-date').text()).toEqual('Tue, Jun 11');
+      expect(wrapper.find('.message__header-date').text()).toEqual('Wed, Dec 11');
     });
 
     it('returns the formatted date for previous years', () => {

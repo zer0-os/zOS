629fc2b662e20f419606432ea6e24ae3a73d0fb4
Brett <12437916+colbr@users.noreply.github.com>
2024-06-14 18:06:12 +1200

    chore: switch node polyfill library Previous was outdated

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import svgr from 'vite-plugin-svgr';
-import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       svgr({
         include: '**/*.svg?react',
       }),
+      nodePolyfills(),
     ],
     envPrefix: 'REACT_APP_',
     css: {
@@ -48,20 +49,6 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: 'jsdom',
       setupFiles: ['./setupVitest.ts'],
-    },
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: 'globalThis',
-        },
-        // Enable esbuild polyfill plugins
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-          }),
-        ],
-      },
     },
   };
 });

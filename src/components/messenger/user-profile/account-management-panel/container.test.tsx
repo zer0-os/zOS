de67a15441fbb84b0d3d06c95a6b0aae835f9622
ratik21 <ratikjindal21@gmail.com>
2024-06-17 01:11:47 +0530

    update account management panel container

--- src/components/messenger/user-profile/account-management-panel/container.test.tsx
+++ src/components/messenger/user-profile/account-management-panel/container.test.tsx
@@ -41,5 +41,57 @@ describe('Container', () => {
         expect(props.isModalOpen).toEqual(true);
       });
     });
+
+    describe('currentUser', () => {
+      test('currentUser', () => {
+        const props = subject({
+          authentication: {
+            user: {
+              data: {
+                id: 'user-id',
+                profileSummary: {
+                  primaryEmail: 'email@zero.tech',
+                  firstName: 'John',
+                  lastName: 'Doe',
+                  profileImage: 'image',
+                },
+                wallets: [{ id: 'wallet-id-1', publicAddress: 'address456' }],
+              },
+            },
+          } as any,
+        });
+
+        expect(props.currentUser).toEqual({
+          userId: 'user-id',
+          firstName: 'John',
+          lastName: 'Doe',
+          profileImage: 'image',
+          primaryEmail: 'email@zero.tech',
+          wallets: [{ id: 'wallet-id-1', publicAddress: 'address456' }],
+        });
+      });
+    });
+
+    describe('canAddEmail', () => {
+      test('can add email', () => {
+        const props = subject({
+          authentication: {
+            user: { data: { profileSummary: { primaryEmail: null } } },
+          } as any,
+        });
+
+        expect(props.canAddEmail).toEqual(true);
+      });
+
+      test('cannot add email', () => {
+        const props = subject({
+          authentication: {
+            user: { data: { profileSummary: { primaryEmail: 'email@zero.tech' } } },
+          } as any,
+        });
+
+        expect(props.canAddEmail).toEqual(false);
+      });
+    });
   });
 });

0f2e99abfa2ebd0bd085d58814d71dc682224f6c
DomW <domw30@gmail.com>
2024-06-17 10:49:29 +0100

    refactor(reply-card-spec): improve test coverage and test quality for reply card tests (#2005)
    * feat(reply-card): render media if media is present in parent message
    
    * feat: add media prop to parent message type
    
    * feat: add media url and media name attributes to reply card in message input
    
    * refactor: render media when message is not present
    
    * refactor: optional message args
    
    * refactor: add test coverage for optional message
    
    * refactor: amend test date
    
    * feat: pass media into reply
    
    * refactor(reply-card-spec): improve test coverage and test quality for reply card tests

--- src/components/reply-card/reply-card.test.tsx
+++ src/components/reply-card/reply-card.test.tsx
@@ -4,6 +4,11 @@ import { shallow } from 'enzyme';
 
 import ReplyCard, { Properties } from './reply-card';
 import { ContentHighlighter } from '../content-highlighter';
+import { IconButton } from '@zero-tech/zui/components';
+
+import { bem } from '../../lib/bem';
+
+const c = bem('.reply-card');
 
 describe('ReplyCard', () => {
   const subject = (props: Partial<Properties>) => {
@@ -22,10 +27,9 @@ describe('ReplyCard', () => {
   };
 
   it('renders reply message', function () {
-    const message = 'hello';
-    const wrapper = subject({ message });
+    const wrapper = subject({ message: 'hello' });
 
-    expect(wrapper.find(ContentHighlighter).prop('message').trim()).toStrictEqual(message);
+    expect(wrapper.find(ContentHighlighter)).toHaveProp('message', 'hello');
   });
 
   it('does not render reply message if no message is present', function () {
@@ -34,72 +38,36 @@ describe('ReplyCard', () => {
     expect(wrapper).not.toHaveElement(ContentHighlighter);
   });
 
+  it('renders media when media url is present', function () {
+    const wrapper = subject({ mediaName: 'test-media-name', mediaUrl: 'test-media-url' });
+
+    expect(wrapper).toHaveElement(c('media-container'));
+  });
+
+  it('does not render media when media url is NOT present', function () {
+    const wrapper = subject({ message: 'hello' });
+
+    expect(wrapper).not.toHaveElement(c('media-container'));
+  });
+
+  it('renders the sender name when senderIsCurrentUser is false', function () {
+    const wrapper = subject({ senderIsCurrentUser: false, senderFirstName: 'Jackie', senderLastName: 'Chan' });
+
+    expect(wrapper.find(c('header'))).toHaveText('Jackie Chan');
+  });
+
+  it('renders "You" if the sender is the current user', function () {
+    const wrapper = subject({ senderIsCurrentUser: true, senderFirstName: 'Jackie', senderLastName: 'Chan' });
+
+    expect(wrapper.find(c('header'))).toHaveText('You');
+  });
+
   it('call onRemove when close icon is clicked', function () {
     const onRemove = jest.fn();
 
     const wrapper = subject({ onRemove });
-    wrapper.find('IconButton').simulate('click');
+    wrapper.find(IconButton).simulate('click');
 
     expect(onRemove).toHaveBeenCalledOnce();
-  });
-
-  it('renders the sender name', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: false,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-    });
-
-    expect(wrapper.find('.reply-card__header').text()).toEqual('Jackie Chan');
-  });
-
-  it('renders "You" if the sender is the current user', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: true,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-    });
-
-    expect(wrapper.find('.reply-card__header').text()).toEqual('You');
-  });
-
-  it('renders media when media url is present and NO message', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: true,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-      mediaName: 'test-media-name',
-      mediaUrl: 'test-media-url',
-    });
-
-    expect(wrapper).toHaveElement('.reply-card__media-container');
-    expect(wrapper).not.toHaveElement(ContentHighlighter);
-  });
-
-  it('renders media and message when media url and message are present', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: true,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-      mediaName: 'test-media-name',
-      mediaUrl: 'test-media-url',
-      message: 'hello',
-    });
-
-    expect(wrapper).toHaveElement('.reply-card__media-container');
-    expect(wrapper).toHaveElement(ContentHighlighter);
-  });
-
-  it('does not render media when media url is NOT present', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: true,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-      mediaName: '',
-      mediaUrl: '',
-      message: 'hello',
-    });
-
-    expect(wrapper).not.toHaveElement('.reply-card__media-container');
   });
 });

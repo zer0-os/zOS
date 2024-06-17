364db2ba451291ddd080f79edccafc738abedf1e
DomW <domw30@gmail.com>
2024-06-17 11:03:57 +0100

    feat(messges-parent-message): add and handle media replies and redesign component (#2007)
    * feat(reply-card): render media if media is present in parent message
    
    * feat: add media prop to parent message type
    
    * feat: add media url and media name attributes to reply card in message input
    
    * refactor: render media when message is not present
    
    * refactor: optional message args
    
    * refactor: add test coverage for optional message
    
    * refactor: amend test date
    
    * feat: pass media into reply
    
    * refactor(reply-card-spec): improve test coverage and test quality for reply card tests
    
    * feat(message-input-reply-card): redesign component
    
    * feat(messges-parent-message): add and handle media replies and redesign component
    
    * refactor: remove unused css
    
    * refactor: add correct class name to scss files

--- src/components/message/parent-message/index.test.tsx
+++ src/components/message/parent-message/index.test.tsx
@@ -1,37 +1,67 @@
 import { shallow } from 'enzyme';
 
 import { ParentMessage, Properties } from '.';
+import { ContentHighlighter } from '../../content-highlighter';
+
+import { bem } from '../../../lib/bem';
+
+const c = bem('.parent-message-container');
 
 describe(ParentMessage, () => {
   const subject = (props: Partial<Properties>) => {
     const allProps: Properties = {
-      message: 'stub message',
+      message: 'message',
       senderIsCurrentUser: false,
-      senderFirstName: 'StubFirst',
-      senderLastName: 'StubLast',
+      senderFirstName: '',
+      senderLastName: '',
+      mediaUrl: '',
+      mediaName: '',
+
       ...props,
     };
 
     return shallow(<ParentMessage {...allProps} />);
   };
 
-  it('renders the sender name', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: false,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-    });
+  it('renders reply message', function () {
+    const wrapper = subject({ message: 'hello' });
 
-    expect(wrapper.find('.parent-message__header').text()).toEqual('Jackie Chan');
+    expect(wrapper.find(ContentHighlighter)).toHaveProp('message', 'hello');
+  });
+
+  it('does not render reply message if no message is present', function () {
+    const wrapper = subject({ message: '' });
+
+    expect(wrapper).not.toHaveElement(ContentHighlighter);
+  });
+
+  it('renders media when media url is present', function () {
+    const wrapper = subject({ mediaName: 'test-media-name', mediaUrl: 'test-media-url' });
+
+    expect(wrapper).toHaveElement(c('media-container'));
+  });
+
+  it('does not render media when media url is NOT present', function () {
+    const wrapper = subject({ message: 'hello' });
+
+    expect(wrapper).not.toHaveElement(c('media-container'));
+  });
+
+  it('does not render when both message and mediaUrl are empty', function () {
+    const wrapper = subject({ message: '', mediaUrl: '' });
+
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('renders the sender name when senderIsCurrentUser is false', function () {
+    const wrapper = subject({ senderIsCurrentUser: false, senderFirstName: 'Jackie', senderLastName: 'Chan' });
+
+    expect(wrapper.find(c('header'))).toHaveText('Jackie Chan');
   });
 
   it('renders "You" if the sender is the current user', function () {
-    const wrapper = subject({
-      senderIsCurrentUser: true,
-      senderFirstName: 'Jackie',
-      senderLastName: 'Chan',
-    });
+    const wrapper = subject({ senderIsCurrentUser: true, senderFirstName: 'Jackie', senderLastName: 'Chan' });
 
-    expect(wrapper.find('.parent-message__header').text()).toEqual('You');
+    expect(wrapper.find(c('header'))).toHaveText('You');
   });
 });

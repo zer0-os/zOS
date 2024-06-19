4b48d3b0f7797a5bcbf3f6fe38c24cc8e0b2f298
DomW <domw30@gmail.com>
2024-06-19 12:58:56 +0100

    feat(restricted-route): add store link elements to restricted page (#2028)
    * feat(restricted-route): add store link elements to restricted page
    
    * refactor: remove comments

--- src/restricted.tsx
+++ src/restricted.tsx
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { connectContainer } from './store/redux-container';
 import { ThemeEngine } from './components/theme-engine';
 import ZeroLogo from './zero-logo.svg?react';
+import { config } from './config';
 
 import { bemClassName } from './lib/bem';
 
@@ -29,8 +30,15 @@ export class Container extends React.Component {
           <div {...cn('text-container')}>
             <p>Please accept your ZERO invite on a desktop browser, such as Chrome or Brave.</p>
             <p>
-              Once you have registered you can use ZERO on your phone, by downloading from the Apple App Store or Google
-              Play store.
+              Once you have registered you can use ZERO on your phone, by downloading from the
+              <a {...cn('link')} href={config.appleAppStorePath} target='_blank' rel='noopener noreferrer'>
+                {' Apple App Store '}
+              </a>
+              or
+              <a {...cn('link')} href={config.googlePlayStorePath} target='_blank' rel='noopener noreferrer'>
+                {' Google Play Store'}
+              </a>
+              .
             </p>
           </div>
         </div>

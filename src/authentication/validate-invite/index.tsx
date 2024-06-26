8ecf73c36e8d00d1410c040237c0d4a9ec067f70
Ratik Jindal <ratikjindal21@gmail.com>
2024-06-26 23:45:15 +0530

    update (#2043)

--- src/authentication/validate-invite/index.tsx
+++ src/authentication/validate-invite/index.tsx
@@ -14,8 +14,7 @@ export interface Properties {
   validateInvite: (data: { code: string }) => void;
 }
 
-// allow upto 14 chars (number/string)
-const MAX_INVITE_CODE_LENGTH = 14;
+const MAX_INVITE_CODE_LENGTH = 6;
 
 interface State {
   inviteCode: string;
@@ -84,6 +83,7 @@ export class Invite extends React.Component<Properties, State> {
               type='text'
               error={isError}
               autoFocus
+              maxLength={MAX_INVITE_CODE_LENGTH}
             />
 
             {isError && this.renderAlert(this.props.inviteCodeStatus)}

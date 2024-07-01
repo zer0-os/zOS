d39fcdea304a77980fa234ad28f24b74d6a0b196
colbr <colbr243@gmail.com>
2024-07-01 14:13:27 +1000

    feat: create wagmi config https://www.rainbowkit.com/docs/installation#configure Leaving WalletConnect `projectId` blank for now

---
+++ src/lib/web3/wagmi-config.ts
@@ -0,0 +1,8 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { mainnet } from 'wagmi/chains';
+
+export const wagmiConfig = getDefaultConfig({
+  appName: 'zOS',
+  projectId: 'TEMP_ID',
+  chains: [mainnet],
+});

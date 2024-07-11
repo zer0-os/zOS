acd17fcdf28579e8a0109bbc1262bd8499c82af4
colbr <colbr243@gmail.com>
2024-07-11 16:17:20 +1000

    feat: mock project ID

--- src/lib/web3/wagmi-config.ts
+++ src/lib/web3/wagmi-config.ts
@@ -3,7 +3,9 @@ import { Config, http } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
 import { config } from '../../config';
 
-let network;
+let projectId: string = process.env.NODE_ENV === 'test' ? 'mock-project-id' : config.WALLET_CONNECT_PROJECT_ID;
+
+let network: typeof mainnet | typeof sepolia;
 const chainIdAsNumber = Number(config.supportedChainId);
 
 if (chainIdAsNumber === 1) {
@@ -16,7 +18,7 @@ if (chainIdAsNumber === 1) {
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'zOS',
-  projectId: config.WALLET_CONNECT_PROJECT_ID ?? 'mock-project-id',
+  projectId,
   chains: [network],
   transports: {
     [network.id]: http(config.INFURA_URL),

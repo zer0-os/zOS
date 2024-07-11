6062aee375529b87a3c9be2349c40aeec3a6925e
colbr <colbr243@gmail.com>
2024-07-11 15:46:10 +1000

    feat: wallet connect project ID

--- src/lib/web3/wagmi-config.ts
+++ src/lib/web3/wagmi-config.ts
@@ -1,10 +1,11 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { Config } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
+import { config } from '../../config';
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'zOS',
-  projectId: 'TEMP_ID',
+  projectId: config.WALLET_CONNECT_PROJECT_ID,
   chains: [mainnet],
 });
 

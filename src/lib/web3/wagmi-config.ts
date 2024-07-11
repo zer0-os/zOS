376902d91843424edae5fa53eb4dcd3ef80dc450
colbr <colbr243@gmail.com>
2024-07-11 15:52:34 +1000

    feat(wagmi): transport setup

--- src/lib/web3/wagmi-config.ts
+++ src/lib/web3/wagmi-config.ts
@@ -1,12 +1,26 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { Config } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
+import { Config, http } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
 import { config } from '../../config';
+
+let network;
+const chainIdAsNumber = Number(config.supportedChainId);
+
+if (chainIdAsNumber === 1) {
+  network = mainnet;
+} else if (chainIdAsNumber === 11155111) {
+  network = sepolia;
+} else {
+  throw new Error('Unsupported chain ID');
+}
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'zOS',
   projectId: config.WALLET_CONNECT_PROJECT_ID,
-  chains: [mainnet],
+  chains: [network],
+  transports: {
+    [network.id]: http(config.INFURA_URL),
+  },
 });
 
 export const getWagmiConfig = (): Config => {

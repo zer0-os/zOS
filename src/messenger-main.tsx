f1b03889908ef01d218e1b8a8869ce7e5c4f61df
colbr <colbr243@gmail.com>
2024-07-26 16:26:31 +1000

    lint: unused import

--- src/messenger-main.tsx
+++ src/messenger-main.tsx
@@ -3,7 +3,6 @@ import { RootState } from './store/reducer';
 import { connectContainer } from './store/redux-container';
 
 import { Main } from './Main';
-import { ZUIProvider } from '@zero-tech/zui/ZUIProvider';
 import { Provider as AuthenticationContextProvider } from './components/authentication/context';
 import { setActiveConversationId } from './store/chat';
 

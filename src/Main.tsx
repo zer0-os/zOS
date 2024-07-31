44c0188a43a4d04825584119cb47353a4d515478
Dale Fukami <dalefukami@users.noreply.github.com>
2024-07-31 09:37:31 -0600

    Revert "feat: app switcher (#2128)"
    This reverts commit 9263ed453e5db904866ab8ec6a7adbbe29b04753.

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,33 +1,65 @@
 import React from 'react';
 import { RootState } from './store/reducer';
 import { connectContainer } from './store/redux-container';
+import { ThemeEngine } from './components/theme-engine';
 
+import './main.scss';
+import classNames from 'classnames';
 import { Sidekick } from './components/sidekick/index';
 import { withContext as withAuthenticationContext } from './components/authentication/context';
 import { MessengerChat } from './components/messenger/chat';
 import { DevPanelContainer } from './components/dev-panel/container';
 import { FeatureFlag } from './components/feature-flag';
+import { AppBar } from './components/app-bar';
+import { DialogManager } from './components/dialog-manager/container';
+import { getMainBackgroundClass, getMainBackgroundVideoSrc } from './utils';
 
 export interface Properties {
   context: {
     isAuthenticated: boolean;
   };
+  selectedMainBackground: string;
 }
 
 export class Container extends React.Component<Properties> {
-  static mapState(_state: RootState): Partial<Properties> {
-    return {};
+  static mapState(state: RootState): Partial<Properties> {
+    const {
+      background: { selectedMainBackground },
+    } = state;
+
+    return { selectedMainBackground };
   }
 
   static mapActions(_state: RootState): Partial<Properties> {
     return {};
   }
 
-  render() {
+  renderAnimatedBackground(src) {
     return (
-      <>
+      <video key={src} className='main-background-video' autoPlay loop muted>
+        <source src={src} type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+    );
+  }
+
+  render() {
+    const videoSrc = getMainBackgroundVideoSrc(this.props.selectedMainBackground);
+    const backgroundClass = getMainBackgroundClass(this.props.selectedMainBackground);
+
+    const mainClassName = classNames('main', 'messenger-full-screen', backgroundClass, {
+      'sidekick-panel-open': this.props.context.isAuthenticated,
+      background: this.props.context.isAuthenticated,
+    });
+
+    return (
+      <div className={mainClassName}>
         {this.props.context.isAuthenticated && (
           <>
+            {videoSrc && this.renderAnimatedBackground(videoSrc)}
+
+            <DialogManager />
+            <AppBar />
             <Sidekick />
             <MessengerChat />
             <Sidekick variant='secondary' />
@@ -36,7 +68,8 @@ export class Container extends React.Component<Properties> {
             </FeatureFlag>
           </>
         )}
-      </>
+        <ThemeEngine />
+      </div>
     );
   }
 }

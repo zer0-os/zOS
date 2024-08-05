bfb05d61181e721893b9cd97afec6d2c816f4781
dominic <domw30@gmail.com>
2024-08-05 10:25:25 +0100

    fix: move key prop out of props spread to resolve react error

--- src/components/content-highlighter/index.tsx
+++ src/components/content-highlighter/index.tsx
@@ -30,13 +30,12 @@ export class ContentHighlighter extends React.Component<Properties> {
 
       if (match[2] === 'user') {
         const mention = `${match[1]}`.trim();
-        const props: { className: string; key: string } = {
+        const props: { className: string } = {
           ...cn('user-mention'),
-          key: match[3] + index,
         };
 
         return (
-          <span data-variant={this.props.variant} {...props}>
+          <span key={match[3] + index} data-variant={this.props.variant} {...props}>
             {this.props.variant === 'negative' && '@'}
             {mention}
           </span>

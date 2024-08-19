0b6f933ff073fb0efd8a02b2aef6859286c25e62
colbr <colbr243@gmail.com>
2024-08-19 15:20:31 +0200

    feat: mock submit, avatar

--- src/components/messenger/feed/components/create-post/index.tsx
+++ src/components/messenger/feed/components/create-post/index.tsx
@@ -1,28 +1,41 @@
 import { useEffect, useRef, useState } from 'react';
 
-import { Button, IconButton } from '@zero-tech/zui/components';
+import { Avatar, Button, IconButton } from '@zero-tech/zui/components';
 import { IconCamera1, IconPlus, IconMicrophone2 } from '@zero-tech/zui/icons';
 
 import styles from './styles.module.scss';
 
 export const CreatePost = () => {
   const [value, setValue] = useState('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
   };
 
+  const handleOnSubmit = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
   return (
-    <div className={styles.Create}>
-      <PostInput value={value} onChange={handleOnChange} />
-      <hr />
-      <div className={styles.Actions}>
-        <div className={styles.Media}>
-          <IconButton Icon={IconPlus} onClick={() => {}} />
-          <IconButton Icon={IconCamera1} onClick={() => {}} />
-          <IconButton Icon={IconMicrophone2} onClick={() => {}} />
+    <div className={styles.Container}>
+      <Avatar size={'medium'} />
+      <div className={styles.Create}>
+        <PostInput value={value} onChange={handleOnChange} />
+        <hr />
+        <div className={styles.Actions}>
+          <div className={styles.Media}>
+            <IconButton Icon={IconPlus} onClick={() => {}} />
+            <IconButton Icon={IconCamera1} onClick={() => {}} />
+            <IconButton Icon={IconMicrophone2} onClick={() => {}} />
+          </div>
+          <Button isDisabled={!value} isLoading={isLoading} className={styles.Button} onPress={handleOnSubmit}>
+            Create
+          </Button>
         </div>
-        <Button className={styles.Button}>Create</Button>
       </div>
     </div>
   );

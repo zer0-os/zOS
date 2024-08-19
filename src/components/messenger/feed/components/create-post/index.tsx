24497b39395fe904d18392a627ee66f34211a2d6
colbr <colbr243@gmail.com>
2024-08-19 15:07:26 +0200

    feat: auto-resize `textarea`

--- src/components/messenger/feed/components/create-post/index.tsx
+++ src/components/messenger/feed/components/create-post/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Button, IconButton } from '@zero-tech/zui/components';
-import { IconCamera1, IconMicrophone2, IconPlus } from '@zero-tech/zui/icons';
+import { IconCamera1, IconPlus, IconMicrophone2 } from '@zero-tech/zui/icons';
 
 import styles from './styles.module.scss';
 
@@ -14,7 +14,8 @@ export const CreatePost = () => {
 
   return (
     <div className={styles.Create}>
-      <textarea className={styles.Input} placeholder='Write a post' value={value} onChange={handleOnChange} />
+      <PostInput value={value} onChange={handleOnChange} />
+      <hr />
       <div className={styles.Actions}>
         <div className={styles.Media}>
           <IconButton Icon={IconPlus} onClick={() => {}} />
@@ -24,5 +25,38 @@ export const CreatePost = () => {
         <Button className={styles.Button}>Create</Button>
       </div>
     </div>
+  );
+};
+
+interface PostInputProps {
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  value: string;
+}
+
+const PostInput = ({ onChange, value }: PostInputProps) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.style.height = 'auto';
+      ref.current.style.height = `${ref.current.scrollHeight}px`;
+
+      // Auto-resize on input
+      ref.current.addEventListener('input', () => {
+        ref.current.style.height = 'auto';
+        ref.current.style.height = `${ref.current.scrollHeight}px`;
+      });
+    }
+  }, []);
+
+  return (
+    <textarea
+      className={styles.Input}
+      onChange={onChange}
+      placeholder='Write a post'
+      ref={ref}
+      rows={2}
+      value={value}
+    />
   );
 };

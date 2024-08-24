2a883f0fdbf019d970738b09871e7da08be47dde
DomW <domw30@gmail.com>
2024-08-24 13:43:47 +0100

    feat(matrix-client): update map event to post message sender data to include display sub handle field (#2204)

--- src/lib/chat/matrix/chat-message.ts
+++ src/lib/chat/matrix/chat-message.ts
@@ -125,6 +125,7 @@ export function mapEventToPostMessage(matrixMessage, sdkMatrixClient: SDKMatrixC
       lastName: '',
       profileImage: '',
       profileId: '',
+      displaySubHandle: '',
     },
 
     isAdmin: false,

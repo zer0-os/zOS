b1944481f0c12c69f59ff88322612677a250a6c1
ratik21 <ratikjindal21@gmail.com>
2024-10-01 21:08:09 +0530

    update edit-profile flow

--- src/store/edit-profile/saga.test.ts
+++ src/store/edit-profile/saga.test.ts
@@ -2,7 +2,7 @@ import { expectSaga } from 'redux-saga-test-plan';
 import { call } from 'redux-saga/effects';
 import { editProfile as editProfileSaga, updateUserProfile, fetchOwnedZIDs, getLocalUrl } from './saga';
 import { editUserProfile as apiEditUserProfile, fetchOwnedZIDs as apiFetchOwnedZIDs } from './api';
-import { uploadFile } from '../../lib/chat';
+import { uploadFile, editProfile as matrixEditProfile } from '../../lib/chat';
 
 import { EditProfileState, State, initialState as initialEditProfileState, setLoadingZIDs } from '.';
 import { rootReducer } from '../reducer';
@@ -32,6 +32,7 @@ describe('editProfile', () => {
           call(apiEditUserProfile, { name, profileImage, primaryZID }),
           { success: true },
         ],
+        [call(matrixEditProfile, profileImage), {}],
         [call(getLocalUrl, image), 'local-image-url'],
       ])
       .call(updateUserProfile, { name, profileImage: 'local-image-url', primaryZID })
@@ -42,6 +43,7 @@ describe('editProfile', () => {
           { profileSummary: { firstName: 'old-name', profileImage: 'old-image' } as any, primaryZID: 'old-zid' }
         )
       )
+      .call(matrixEditProfile, profileImage)
       .run();
 
     expect(authentication.user.data.profileSummary.firstName).toEqual('John Doe');
@@ -98,6 +100,7 @@ describe('editProfile', () => {
           call(apiEditUserProfile, { name, primaryZID, profileImage: undefined }),
           { success: true },
         ],
+        [call(matrixEditProfile, undefined), {}],
       ])
       .withReducer(
         rootReducer,

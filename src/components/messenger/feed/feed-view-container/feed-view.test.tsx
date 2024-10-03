5bc976f3f3938f81e603b1edb22861464acb0683
DomW <domw30@gmail.com>
2024-10-03 11:29:41 +0100

    fix(feed-view-container): ensure fetch more posts is called by removing waypoint conditional height check restriction (#2313)
    * fix(feed-view-container): ensure fetch more posts is called by removing waypoint conditional height check restriction
    
    * fix: failing test - remove state check

--- src/components/messenger/feed/feed-view-container/feed-view.test.tsx
+++ src/components/messenger/feed/feed-view-container/feed-view.test.tsx
@@ -50,33 +50,13 @@ describe('FeedView', () => {
     expect(wrapper).toHaveElement(Spinner);
   });
 
-  it('renders a waypoint if posts are present and content exceeds viewport height', () => {
+  it('renders a waypoint if posts are present', () => {
     const onFetchMoreSpy = jest.fn();
     const wrapper = subject({ postMessages: POST_MESSAGES_TEST, onFetchMore: onFetchMoreSpy });
-
-    wrapper.setState({ shouldRenderWaypoint: true });
 
     const waypoint = wrapper.find(Waypoint);
     expect(waypoint.exists()).toBe(true);
     expect(waypoint.prop('onEnter')).toEqual(onFetchMoreSpy);
-  });
-
-  it('does not render a waypoint if content does not exceed viewport height', () => {
-    const wrapper = subject({ postMessages: POST_MESSAGES_TEST });
-
-    wrapper.setState({ shouldRenderWaypoint: false });
-
-    expect(wrapper.find(Waypoint).exists()).toBe(false);
-  });
-
-  it('renders a waypoint if posts are present and content height changes after update', () => {
-    const onFetchMoreSpy = jest.fn();
-    const wrapper = subject({ postMessages: POST_MESSAGES_TEST, onFetchMore: onFetchMoreSpy });
-
-    wrapper.setState({ shouldRenderWaypoint: true });
-
-    const waypoint = wrapper.find(Waypoint);
-    expect(waypoint.exists()).toBe(true);
   });
 
   it('does not render a waypoint if no posts are present', () => {
@@ -98,32 +78,5 @@ describe('FeedView', () => {
     const wrapper = shallow(<Message>Test Message</Message>);
 
     expect(wrapper.text()).toEqual('Test Message');
-  });
-
-  it('updates shouldRenderWaypoint state based on content height after mount', () => {
-    const wrapper = subject({ postMessages: POST_MESSAGES_TEST });
-
-    const instance = wrapper.instance() as FeedView;
-    jest.spyOn(instance, 'checkContentHeight');
-
-    instance.componentDidMount();
-
-    expect(instance.checkContentHeight).toHaveBeenCalled();
-  });
-
-  it('updates shouldRenderWaypoint state based on content height after update', () => {
-    const wrapper = subject({ postMessages: POST_MESSAGES_TEST });
-
-    const instance = wrapper.instance() as FeedView;
-    jest.spyOn(instance, 'checkContentHeight');
-
-    wrapper.setProps({
-      postMessages: [
-        ...POST_MESSAGES_TEST,
-        { id: 'post-three', message: 'Third post', createdAt: 1659018545429, isPost: true },
-      ],
-    });
-
-    expect(instance.checkContentHeight).toHaveBeenCalled();
   });
 });
--- src/components/messenger/feed/feed-view-container/feed-view.tsx
+++ src/components/messenger/feed/feed-view-container/feed-view.tsx
@@ -26,44 +26,9 @@ export interface Properties {
 }
 
 export class FeedView extends React.Component<Properties> {
-  contentRef: React.RefObject<HTMLDivElement>;
-
-  state = {
-    shouldRenderWaypoint: false,
-  };
-
-  constructor(props: Properties) {
-    super(props);
-    this.contentRef = React.createRef();
-  }
-
-  componentDidMount() {
-    this.checkContentHeight();
-  }
-
-  componentDidUpdate(prevProps: Properties) {
-    if (
-      prevProps.postMessages !== this.props.postMessages ||
-      prevProps.hasLoadedMessages !== this.props.hasLoadedMessages
-    ) {
-      this.checkContentHeight();
-    }
-  }
-
-  checkContentHeight() {
-    if (this.contentRef.current) {
-      const contentHeight = this.contentRef.current.clientHeight;
-      const viewportHeight = window.innerHeight;
-
-      this.setState({
-        shouldRenderWaypoint: contentHeight > viewportHeight,
-      });
-    }
-  }
-
   render() {
     return (
-      <div {...cn('')} ref={this.contentRef}>
+      <div {...cn('')}>
         {this.props.hasLoadedMessages && (
           <>
             {this.props.postMessages.length > 0 ? (
@@ -76,10 +41,8 @@ export class FeedView extends React.Component<Properties> {
                   userMeowBalance={this.props.userMeowBalance}
                 />
 
-                {this.props.messagesFetchStatus === MessagesFetchState.SUCCESS && this.state.shouldRenderWaypoint && (
-                  <div {...cn('waypoint')}>
-                    <Waypoint onEnter={this.props.onFetchMore} />
-                  </div>
+                {this.props.messagesFetchStatus === MessagesFetchState.SUCCESS && (
+                  <Waypoint onEnter={this.props.onFetchMore} />
                 )}
               </>
             ) : (

bd5ada3f8032f68915dea64f68e6dbf09e2917bb
DomW <domw30@gmail.com>
2024-11-13 11:29:38 +0000

    feat(notifications-app): add notifications app container (#2426)
    * feat(feature-flags): add enableNotificationsApp ff
    
    * feat(notification-feed): add notificiations feed item component
    
    * feat(notifications-feed): add notifications feed container
    
    * feat(notifications-app): add notifications app container

---
+++ src/apps/notifications/index.tsx
@@ -0,0 +1,2 @@
+export { NotificationsContainer as NotificationsApp } from './notifications-container';
+export type { Properties as NotificationsProperties } from './notifications-container';
---
+++ src/apps/notifications/notifications-container.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { RootState } from '../../store/reducer';
+import { connectContainer } from '../../store/redux-container';
+
+import { NotificationsFeed } from '../../components/notifications-feed';
+import { Provider as AuthenticationContextProvider } from '../../components/authentication/context';
+import { ScrollbarContainer } from '../../components/scrollbar-container';
+
+import styles from './notifications-container.module.scss';
+
+export interface Properties {
+  isAuthenticated: boolean;
+}
+
+export class Container extends React.Component<Properties> {
+  static mapState(state: RootState): Partial<Properties> {
+    return {
+      isAuthenticated: !!state.authentication.user?.data,
+    };
+  }
+
+  static mapActions() {
+    return {};
+  }
+
+  get authenticationContext() {
+    const { isAuthenticated } = this.props;
+    return {
+      isAuthenticated,
+    };
+  }
+
+  render() {
+    return (
+      <AuthenticationContextProvider value={this.authenticationContext}>
+        <div className={styles.NotificationsContainer}>
+          <ScrollbarContainer variant='on-hover'>
+            <NotificationsFeed />
+          </ScrollbarContainer>
+        </div>
+      </AuthenticationContextProvider>
+    );
+  }
+}
+
+export const NotificationsContainer = connectContainer<{}>(Container);

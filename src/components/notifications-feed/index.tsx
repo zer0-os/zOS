000bbd148a30b177fea8e19827fca4910d92cef2
DomW <domw30@gmail.com>
2024-11-15 14:28:53 +0000

    refactor(notifications-feed): add loading, error, and no notification states and replace mock data with actual date from state (#2431)
    * feat(notifications-saga/matrix-client): initial fetch, process and store notifications functionality
    
    * refactor(notifications-item): clean up and improve notifications item component
    
    * refactor(notifications-feed): add loading, error, and no notifications states and replace mock data with actual date from state

--- src/components/notifications-feed/index.tsx
+++ src/components/notifications-feed/index.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import { RootState } from '../../store/reducer';
 import { connectContainer } from '../../store/redux-container';
+import { Notification } from '../../store/notifications';
+import { fetchNotifications, openNotificationConversation } from '../../store/notifications';
 
 import { Header } from '../header';
 import { IconBell1 } from '@zero-tech/zui/icons';
 import { NotificationItem } from './notification-item';
+import { Spinner } from '@zero-tech/zui/components/LoadingIndicator';
 
 import styles from './styles.module.scss';
 
 export interface PublicProperties {}
 
-export interface Properties extends PublicProperties {}
+export interface Properties extends PublicProperties {
+  notifications: Notification[];
+  loading: boolean;
+  error: string | null;
+
+  fetchNotifications: () => void;
+  openNotificationConversation: (roomId: string) => void;
+}
 
 export class Container extends React.Component<Properties> {
-  static mapState(_state: RootState): Partial<Properties> {
-    return {};
+  static mapState(state: RootState): Partial<Properties> {
+    const {
+      notifications: { items, loading, error },
+    } = state;
+
+    return {
+      notifications: items,
+      loading,
+      error,
+    };
   }
 
   static mapActions(): Partial<Properties> {
-    return {};
+    return {
+      fetchNotifications,
+      openNotificationConversation,
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchNotifications();
+  }
+
+  onNotificationClick = (roomId: string) => {
+    this.props.openNotificationConversation(roomId);
+  };
+
+  getOldestTimestamp(notifications: Notification[] = []): number {
+    return notifications.reduce((previousTimestamp, notification: any) => {
+      return notification.createdAt < previousTimestamp ? notification.createdAt : previousTimestamp;
+    }, Date.now());
   }
 
   renderHeaderIcon() {
@@ -29,29 +64,34 @@ export class Container extends React.Component<Properties> {
     return <div className={styles.HeaderTitle}>Notifications</div>;
   }
 
+  renderNotifications() {
+    const { notifications } = this.props;
+
+    return notifications.map((notification) => (
+      <li key={notification.id}>
+        <NotificationItem notification={notification} onClick={this.onNotificationClick} />
+      </li>
+    ));
+  }
+
+  renderNoNotifications() {
+    return <div className={styles.NoNotifications}>No notifications</div>;
+  }
+
+  renderLoading() {
+    return (
+      <div className={styles.LoadingContainer}>
+        <Spinner />
+      </div>
+    );
+  }
+
+  renderError() {
+    return <div className={styles.Error}>{'this.props.error'}</div>;
+  }
+
   render() {
-    const notifications = [
-      { id: 1, content: 'Joe Bloggs mentioned you in OG Chat' },
-      { id: 2, content: 'Jeffy B mentioned you in Billionaires Hide Out' },
-      { id: 3, content: 'Elon M sent you a direct message' },
-      { id: 4, content: 'Sarah K liked your post in Travel Lovers' },
-      { id: 5, content: 'Mark Z commented on your photo in Tech Geeks' },
-      { id: 6, content: 'Emily R sent you a friend request' },
-      { id: 7, content: 'Alice W shared your post in Foodies United' },
-      { id: 8, content: 'Bob T invited you to join Startup Founders' },
-      { id: 9, content: 'John D mentioned you in Fitness Enthusiasts' },
-      { id: 10, content: 'Kevin H tagged you in a photo' },
-      { id: 11, content: 'Nina P sent you a direct message' },
-      { id: 12, content: 'Oscar L liked your comment in Movie Buffs' },
-      { id: 13, content: 'Lily G replied to your comment in Artists Hub' },
-      { id: 14, content: 'David S mentioned you in Coding Ninjas' },
-      { id: 15, content: 'Jessica M invited you to join Music Lovers' },
-      { id: 16, content: 'Tom R sent you a direct message' },
-      { id: 17, content: 'Rebecca C liked your post in Gardeners Club' },
-      { id: 18, content: 'George K mentioned you in Bookworms Group' },
-      { id: 19, content: 'Hannah F shared your post in Fashionistas' },
-      { id: 20, content: 'Ian V commented on your video in Gamers United' },
-    ];
+    const { notifications, loading, error } = this.props;
 
     return (
       <div className={styles.NotificationsFeed}>
@@ -61,13 +101,11 @@ export class Container extends React.Component<Properties> {
           </div>
 
           <div className={styles.Body}>
-            <ol className={styles.Notifications}>
-              {notifications.map((notification) => (
-                <li key={notification.id}>
-                  <NotificationItem content={notification.content} />
-                </li>
-              ))}
-            </ol>
+            <ol className={styles.Notifications}>{notifications.length > 0 && this.renderNotifications()}</ol>
+
+            {notifications.length === 0 && !loading && this.renderNoNotifications()}
+            {loading && this.renderLoading()}
+            {error && !loading && this.renderError()}
           </div>
         </div>
       </div>

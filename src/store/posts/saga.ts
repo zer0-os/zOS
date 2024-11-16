41a6b51993b4db7e4360bcd75800ffcb4e080d12
colbr <colbr243@gmail.com>
2024-11-17 00:14:32 +1100

    feat: upload posts to API Changed from Matrix! Also re-fetches on post. I'm not sure if this is the expected behaviour, but it will do for now. Still need to handle error states!

--- src/store/posts/saga.ts
+++ src/store/posts/saga.ts
@@ -2,17 +2,20 @@ import { takeLatest, call, select } from 'redux-saga/effects';
 
 import { SagaActionTypes } from '.';
 import { MediaType, Message } from '../messages';
-import { getPostMessageReactions, sendPostByChannelId } from '../../lib/chat';
+import { getPostMessageReactions } from '../../lib/chat';
 import { messageSelector, rawMessagesSelector, sendMessage } from '../messages/saga';
 import { currentUserSelector } from '../authentication/saga';
 import { createOptimisticPostObject } from './utils';
 import { rawChannelSelector, receiveChannel } from '../channels/saga';
 import { ConversationStatus, MessagesFetchState } from '../channels';
-import { Uploadable, createUploadableFile } from '../messages/uploadable';
+import { Uploadable } from '../messages/uploadable';
 import { Events as ChatEvents, getChatBus } from '../chat/bus';
 import { takeEveryFromBus } from '../../lib/saga';
 import { updateUserMeowBalance } from '../rewards/saga';
-import { get } from '../../lib/api/rest';
+import { get, post } from '../../lib/api/rest';
+import { getWagmiConfig } from '../../lib/web3/wagmi-config';
+import { WalletClient } from 'viem';
+import { getWalletClient } from '@wagmi/core';
 
 export interface Payload {
   channelId: string;
@@ -35,29 +38,157 @@ export interface PostPayload {
   optimisticId?: string;
 }
 
+interface SignedMessagePayload {
+  created_at: string;
+  text: string;
+  wallet_address: string;
+  zid: string;
+}
+
+async function signPostPayload(
+  payload: SignedMessagePayload,
+  walletClient: WalletClient
+): Promise<{ signedPost: string; unsignedPost: string }> {
+  const unsignedPost = JSON.stringify(payload);
+  const signedPost = await walletClient.signMessage({
+    account: payload.wallet_address as `0x${string}`,
+    message: unsignedPost,
+  });
+
+  return {
+    unsignedPost,
+    signedPost,
+  };
+}
+
+function mapPost(post) {
+  return {
+    createdAt: post.createdAt,
+    hidePreview: false,
+    id: post.id,
+    image: undefined,
+    isAdmin: false,
+    isPost: true,
+    media: null,
+    mentionedUsers: [],
+    message: post.text,
+    optimisticId: post.id,
+    preview: null,
+    reactions: {},
+    rootMessageId: '',
+    sendStatus: 0,
+    sender: {
+      userId: post.userId,
+      firstName: post.user.handle,
+      displaySubHandle: '0://' + post.zid,
+    },
+  };
+}
+
+async function uploadPost(formData: FormData, worldZid: string) {
+  const endpoint = `/api/v2/posts/channel/${worldZid}`;
+
+  try {
+    return await post(endpoint)
+      .field('text', formData.get('text'))
+      .field('unsignedMessage', formData.get('unsignedMessage'))
+      .field('signedMessage', formData.get('signedMessage'))
+      .field('zid', formData.get('zid'))
+      .field('walletAddress', formData.get('walletAddress'));
+  } catch (e) {
+    console.error('Failed to upload post', e);
+    throw new Error('Failed to upload post');
+  }
+}
+
+async function getWallet() {
+  const wagmiConfig = getWagmiConfig();
+  const walletClient: WalletClient = await getWalletClient(wagmiConfig);
+
+  return walletClient;
+}
+
 export function* sendPost(action) {
-  const { channelId, message, files = [] } = action.payload;
+  const { channelId, message } = action.payload;
 
-  const processedFiles: Uploadable[] = files.map(createUploadableFile);
+  const channel = yield select(rawChannelSelector(channelId));
 
-  // Create optimistic posts (for both the text and media files)
-  const { optimisticPost, uploadableFiles } = yield call(createOptimisticPosts, channelId, message, processedFiles);
-
-  let rootMessageId = '';
-  if (optimisticPost) {
-    // If the text post is created, send it first
-    const textPost = yield call(sendPostByChannelId, channelId, message, optimisticPost.optimisticId);
-
-    if (textPost) {
-      rootMessageId = textPost.id;
-    } else {
-      // If the text post fails, shift the first file to avoid sending it
-      uploadableFiles.shift();
-    }
+  if (channel.conversationStatus !== ConversationStatus.CREATED) {
+    return;
   }
 
-  // Upload the media files after sending the text post
-  yield call(uploadFileMessages, channelId, rootMessageId, uploadableFiles, true);
+  const user = yield select(currentUserSelector());
+  const userZid = user.primaryZID.split('0://')[1];
+
+  // If user does not have a primary ZID
+  if (!userZid || userZid.trim() === '') {
+    // throw an error
+  }
+
+  const channelZid = channel?.name?.split('0://')[1];
+
+  // If the message contect is empty, or the channel does not have a name
+  if (!message || message.trim() === '' || !channelZid) {
+    // throw an error
+  }
+
+  const walletClient = yield call(getWallet);
+  const connectedAddress = walletClient.account?.address;
+
+  // If the user does not have a connected address
+  if (!connectedAddress) {
+    // throw an error
+  }
+
+  // If the user is connected to a wallet which is not linked to their account
+  if (!user.wallets.find((w) => w.publicAddress.toLowerCase() === connectedAddress.toLowerCase())) {
+    // throw an error
+  }
+
+  const createdAt = new Date().getTime();
+
+  const formData = new FormData();
+
+  const payloadToSign: SignedMessagePayload = {
+    created_at: createdAt.toString(),
+    text: message,
+    wallet_address: connectedAddress,
+    zid: userZid,
+  };
+
+  const { unsignedPost, signedPost } = yield call(signPostPayload, payloadToSign, walletClient);
+
+  formData.append('text', message);
+  formData.append('unsignedMessage', unsignedPost);
+  formData.append('signedMessage', signedPost);
+  formData.append('zid', userZid);
+  formData.append('walletAddress', connectedAddress);
+
+  const res = yield call(uploadPost, formData, channelZid);
+
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`);
+  }
+
+  const existingPosts = yield select(rawMessagesSelector(channelId));
+  const filteredPosts = existingPosts.filter((m) => !m.startsWith('$'));
+
+  yield call(receiveChannel, {
+    id: channelId,
+    messages: [
+      ...filteredPosts,
+      mapPost({
+        createdAt,
+        id: res.body.id,
+        text: message,
+        user: {
+          handle: user.handle,
+          userId: user.id,
+        },
+        zid: userZid,
+      }),
+    ],
+  });
 }
 
 export function* createOptimisticPosts(channelId, postText, uploadableFiles?) {
@@ -142,27 +273,7 @@ export function* fetchPosts(action) {
 
     const irysPosts = res.body.posts;
 
-    const messagePosts: Message[] = irysPosts.map((post) => ({
-      createdAt: post.createdAt,
-      hidePreview: false,
-      id: post.id,
-      image: undefined,
-      isAdmin: false,
-      isPost: true,
-      media: null,
-      mentionedUsers: [],
-      message: post.text,
-      optimisticId: post.id,
-      preview: null,
-      reactions: {},
-      rootMessageId: '',
-      sendStatus: 0,
-      sender: {
-        userId: post.userId,
-        firstName: post.user.handle,
-        displaySubHandle: '0://' + post.zid,
-      },
-    }));
+    const messagePosts: Message[] = irysPosts.map(mapPost);
 
     posts = [...messagePosts, ...filteredPosts];
 

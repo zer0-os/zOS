05409d6bf1db02a54cade10b3fb0d4a163c6a175
dominic <domw30@gmail.com>
2024-11-25 11:57:39 +0000

    feat(notifications-app): add sidekick and conversation list to notifications app

--- src/apps/notifications/notifications-container.tsx
+++ src/apps/notifications/notifications-container.tsx
@@ -5,6 +5,7 @@ import { connectContainer } from '../../store/redux-container';
 import { NotificationsFeed } from '../../components/notifications-feed';
 import { Provider as AuthenticationContextProvider } from '../../components/authentication/context';
 import { ScrollbarContainer } from '../../components/scrollbar-container';
+import { Sidekick } from '../../components/sidekick';
 
 import styles from './notifications-container.module.scss';
 
@@ -34,9 +35,11 @@ export class Container extends React.Component<Properties> {
     return (
       <AuthenticationContextProvider value={this.authenticationContext}>
         <div className={styles.NotificationsContainer}>
+          <Sidekick />
           <ScrollbarContainer variant='on-hover'>
             <NotificationsFeed />
           </ScrollbarContainer>
+          <div />
         </div>
       </AuthenticationContextProvider>
     );

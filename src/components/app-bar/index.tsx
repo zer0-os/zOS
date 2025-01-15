5f012245b9b5fa6659c1cfa7280f8b018fd8077d
colbr <colbr243@gmail.com>
2025-01-15 12:38:28 +1100

    feat: add Feed app link to `app-bar` Lets users navigate to the Feed app Currently feature flagged

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { WorldPanelItem } from './world-panel-item';
-import { IconBell1, IconDotsGrid, IconGlobe3, IconMessageSquare2 } from '@zero-tech/zui/icons';
+import { IconBell1, IconDotsGrid, IconGlobe3, IconList, IconMessageSquare2 } from '@zero-tech/zui/icons';
 import { MoreAppsModal } from './more-apps-modal';
 import { Link } from 'react-router-dom';
 import { IconProps } from '@zero-tech/zui/components/Icons/Icons.types';
@@ -49,6 +49,9 @@ export class AppBar extends React.Component<Properties, State> {
       <>
         <div {...cn('')}>
           <AppLink Icon={IconMessageSquare2} isActive={isActive('conversation')} label='Messenger' to='/conversation' />
+          {featureFlags.enableFeedApp && (
+            <AppLink Icon={IconList} isActive={isActive('feed')} label='Feed' to='/feed' />
+          )}
           {featureFlags.enableNotificationsApp && (
             <AppLink
               Icon={this.renderNotificationIcon}

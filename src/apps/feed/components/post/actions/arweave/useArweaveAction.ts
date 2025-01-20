e257520ef9d8a097da33041cedb5dddb5b2d2d64
colbr <colbr243@gmail.com>
2025-01-20 11:57:57 +1100

    docs: add comment re: hardcoding

--- src/apps/feed/components/post/actions/arweave/useArweaveAction.ts
+++ src/apps/feed/components/post/actions/arweave/useArweaveAction.ts
@@ -1,5 +1,6 @@
 export const useArweaveAction = (arweaveId: string) => {
   const handleOnClick = () => {
+    // @note this is temporarily hardcoded, and will not work on devnet
     window.open(`https://irys.xyz/${arweaveId}`, '_blank');
   };
 

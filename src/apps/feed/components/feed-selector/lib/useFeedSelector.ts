c8765909d0b734118fd18f0ff68fbe4131896ad0
colbr <colbr243@gmail.com>
2025-01-20 17:07:57 +1100

    feat: highlight selected feed

--- src/apps/feed/components/feed-selector/lib/useFeedSelector.ts
+++ src/apps/feed/components/feed-selector/lib/useFeedSelector.ts
@@ -1,12 +1,18 @@
+import { useRouteMatch } from 'react-router-dom';
+
 import { useOwnedZids } from '../../../../../lib/hooks/useOwnedZids';
 
 interface UseFeedSelectorReturn {
   zids?: string[];
   isLoadingZids: boolean;
   isErrorZids: boolean;
+  selectedZId?: string;
 }
 
 export const useFeedSelector = (): UseFeedSelectorReturn => {
+  const route = useRouteMatch('/feed/:zid');
+  const selectedZId = route?.params?.zid;
+
   const { zids, isLoading, isError } = useOwnedZids();
 
   const worldZids = zids?.map((zid) => zid.split('.')[0]);
@@ -16,5 +22,6 @@ export const useFeedSelector = (): UseFeedSelectorReturn => {
     zids: uniqueWorldZids,
     isLoadingZids: isLoading,
     isErrorZids: isError,
+    selectedZId,
   };
 };

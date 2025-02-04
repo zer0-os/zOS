911f365d82d0825b5f6d8a482c34141cbdb3af3a
Brett <12437916+colbr@users.noreply.github.com>
2025-02-04 17:16:15 +1100

    feat: increase bottom offset of feed waypoint Triggers next page of infinite scroll earlier, which should create a smoother scrolling experience

--- src/apps/feed/components/feed/index.tsx
+++ src/apps/feed/components/feed/index.tsx
@@ -65,7 +65,15 @@ export const Feed = ({ zid }: FeedProps) => {
       )}
       {isError && <Message>Failed to load posts</Message>}
       {isFetchingNextPage && <Message>Loading more posts</Message>}
-      {hasNextPage && !isFetchingNextPage && !isError && <Waypoint onEnter={() => fetchNextPage()} />}
+      {hasNextPage && !isFetchingNextPage && !isError && (
+        <Waypoint
+          onEnter={() => {
+            console.log('onEnter');
+            fetchNextPage();
+          }}
+          bottomOffset={'-90%'}
+        />
+      )}
     </div>
   );
 };

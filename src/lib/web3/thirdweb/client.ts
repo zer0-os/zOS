9d6f49c4cbfd00b09d0ecf9a9d6a7759563b0944
Brett <12437916+colbr@users.noreply.github.com>
2025-02-07 16:33:21 +1100

    refactor: improve types

--- src/lib/web3/thirdweb/client.ts
+++ src/lib/web3/thirdweb/client.ts
@@ -1,14 +1,14 @@
-import { createThirdwebClient } from 'thirdweb';
+import { createThirdwebClient, ThirdwebClient } from 'thirdweb';
 import { config } from '../../../config';
 import { mainnet, sepolia } from 'thirdweb/chains';
 
 let client;
-export function getThirdWebClient() {
+export function getThirdWebClient(): ThirdwebClient {
   client = client ?? createClient();
   return client;
 }
 
-function createClient() {
+function createClient(): ThirdwebClient {
   return createThirdwebClient({
     clientId: config.thirwebClientId,
   });

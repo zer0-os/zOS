1fb895155d0695bf72e85af130bdf91c5f4fed7f
Brett <12437916+colbr@users.noreply.github.com>
2025-02-10 01:38:12 +1100

    feat: match feed sidekick styles

--- src/apps/feed/components/feed-selector/index.tsx
+++ src/apps/feed/components/feed-selector/index.tsx
@@ -2,30 +2,44 @@ import { ReactNode } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { useFeedSelector } from './lib/useFeedSelector';
-
+import { Container as SidekickContainer } from '../../../../components/sidekick/components/container';
+import { UserProfileContainer } from '../../../../components/messenger/user-profile/container';
+import { Header } from '../../../../components/sidekick/components/header';
+import { CurrentUserDetails } from '../../../../components/sidekick/components/current-user-details';
 import { ScrollbarContainer } from '../../../../components/scrollbar-container';
 
 import styles from './styles.module.scss';
 
 export const FeedSelector = () => {
-  const { zids, isLoadingZids, isErrorZids, selectedZId } = useFeedSelector();
+  const { isErrorZids, isLoadingZids, isProfileOpen, selectedZId, zids } = useFeedSelector();
 
   return (
-    <ul className={styles.List}>
-      <ScrollbarContainer variant='on-hover' className={styles.Scroll}>
-        <FeedItem key={'explore'} route={'/feed'} isSelected={selectedZId === undefined}>
-          Explore
-        </FeedItem>
-        {isLoadingZids && <li>Loading Feeds...</li>}
-        {isErrorZids && <li>Error loading Feeds</li>}
-        {zids?.map((zid) => (
-          <FeedItem key={zid} route={`/feed/${zid}`} isSelected={selectedZId === zid}>
-            <span>0://</span>
-            {zid}
-          </FeedItem>
-        ))}
-      </ScrollbarContainer>
-    </ul>
+    <SidekickContainer className={styles.Container}>
+      {isProfileOpen ? (
+        <UserProfileContainer />
+      ) : (
+        <>
+          <Header>
+            <CurrentUserDetails />
+          </Header>
+          <ScrollbarContainer variant='on-hover' className={styles.Scroll}>
+            <ul className={styles.List}>
+              <FeedItem key={'explore'} route={'/feed'} isSelected={selectedZId === undefined}>
+                Explore
+              </FeedItem>
+              {isLoadingZids && <li>Loading Feeds...</li>}
+              {isErrorZids && <li>Error loading Feeds</li>}
+              {zids?.map((zid) => (
+                <FeedItem key={zid} route={`/feed/${zid}`} isSelected={selectedZId === zid}>
+                  <span>0://</span>
+                  {zid}
+                </FeedItem>
+              ))}
+            </ul>
+          </ScrollbarContainer>
+        </>
+      )}
+    </SidekickContainer>
   );
 };
 
--- src/apps/feed/components/feed-selector/lib/useFeedSelector.ts
+++ src/apps/feed/components/feed-selector/lib/useFeedSelector.ts
@@ -1,17 +1,22 @@
 import { useRouteMatch } from 'react-router-dom';
 
 import { useOwnedZids } from '../../../../../lib/hooks/useOwnedZids';
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../../store/reducer';
+import { Stage as ProfileStage } from '../../../../../store/user-profile';
 interface UseFeedSelectorReturn {
-  zids?: string[];
-  isLoadingZids: boolean;
   isErrorZids: boolean;
+  isLoadingZids: boolean;
+  isProfileOpen: boolean;
   selectedZId?: string;
+  zids?: string[];
 }
 
 export const useFeedSelector = (): UseFeedSelectorReturn => {
   const route = useRouteMatch('/feed/:zid');
   const selectedZId = route?.params?.zid;
+
+  const isProfileOpen = useSelector((state: RootState) => state.userProfile.stage !== ProfileStage.None);
 
   const { zids, isLoading, isError } = useOwnedZids();
 
@@ -19,9 +24,10 @@ export const useFeedSelector = (): UseFeedSelectorReturn => {
   const uniqueWorldZids = worldZids ? ([...new Set(worldZids)] as string[]) : undefined;
 
   return {
-    zids: uniqueWorldZids,
-    isLoadingZids: isLoading,
     isErrorZids: isError,
+    isLoadingZids: isLoading,
+    isProfileOpen,
     selectedZId,
+    zids: uniqueWorldZids,
   };
 };

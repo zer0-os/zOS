70d660fc788d2ed75e4549e0904cbb537318c31e
Brett <12437916+colbr@users.noreply.github.com>
2025-02-15 23:48:14 +1100

    feat: sidekick panel

--- src/components/sidekick/components/container/index.tsx
+++ src/components/sidekick/components/container/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import { IfAuthenticated } from '../../../authentication/if-authenticated';
 import styles from './styles.module.scss';
+import { Panel } from '../../../layout/panel';
 
 export interface ContainerProps {
   className?: string;
@@ -11,11 +12,11 @@ export const Container = ({ className, children }: ContainerProps) => {
   return (
     <IfAuthenticated showChildren>
       <div className={classNames(styles.Container, className)}>
-        <div className={styles.Wrapper}>
+        <Panel className={styles.Wrapper}>
           <div className={styles.Content}>
             <div className={styles.Messages}>{children}</div>
           </div>
-        </div>
+        </Panel>
       </div>
     </IfAuthenticated>
   );

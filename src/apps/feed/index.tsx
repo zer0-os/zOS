90a425f6a33debfd5db2aa40357093bb43b656f9
Brett <12437916+colbr@users.noreply.github.com>
2025-02-19 17:12:40 +1100

    feat: add loading state

--- src/apps/feed/index.tsx
+++ src/apps/feed/index.tsx
@@ -1,8 +1,12 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { useOwnedZids } from '../../lib/hooks/useOwnedZids';
 
 import { Feed } from './components/feed';
 import { Sidekick } from './components/sidekick';
 import { PostView } from './components/post-view-container';
+import { PanelBody } from '../../components/layout/panel';
+import { IconSlashes } from '@zero-tech/zui/icons';
 
 import styles from './styles.module.scss';
 
@@ -16,8 +20,30 @@ export const FeedApp = () => {
           component={({ match }: any) => <PostView postId={match.params.postId} isFeed={true} />}
         />
         <Route path='/feed/:zid' component={({ match }: any) => <Feed zid={match.params.zid} />} />
-        <Route path='/feed' component={Feed} />
+        <Route path='/feed' component={Loading} />
       </Switch>
     </div>
   );
+};
+
+const Loading = () => {
+  const { isLoading, zids } = useOwnedZids();
+
+  if (isLoading) {
+    return (
+      <PanelBody className={styles.Loading}>
+        <IconSlashes /> Loading channels...
+      </PanelBody>
+    );
+  }
+
+  if (!zids?.length) {
+    return (
+      <PanelBody className={styles.Loading}>
+        <IconSlashes /> You are not a member of any channels.
+      </PanelBody>
+    );
+  }
+
+  return <Redirect to={`/feed/${zids?.[0]}`} />;
 };

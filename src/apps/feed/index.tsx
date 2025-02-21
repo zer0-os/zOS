77eb307154d13ff298bf83ccc43462579d56b0a0
Brett <12437916+colbr@users.noreply.github.com>
2025-02-21 19:16:42 +1100

    feat: show chat on post page

--- src/apps/feed/index.tsx
+++ src/apps/feed/index.tsx
@@ -8,6 +8,7 @@ import { Sidekick } from './components/sidekick';
 import { PostView } from './components/post-view-container';
 import { PanelBody } from '../../components/layout/panel';
 import { IconSlashes } from '@zero-tech/zui/icons';
+import { FeedChat } from './components/feed-chat/container';
 
 import styles from './styles.module.scss';
 
@@ -15,14 +16,17 @@ export const FeedApp = () => {
   return (
     <div className={styles.Feed}>
       <Sidekick />
-      <Switch>
-        <Route
-          path='/feed/:zid/:postId'
-          component={({ match }: any) => <PostView postId={match.params.postId} isFeed={true} />}
-        />
-        <Route path='/feed/:zid' component={({ match }: any) => <Feed zid={match.params.zid} />} />
-        <Route path='/feed' component={Loading} />
-      </Switch>
+      <div className={styles.Wrapper}>
+        <Switch>
+          <Route
+            path='/feed/:zid/:postId'
+            component={({ match }: any) => <PostView postId={match.params.postId} isFeed={true} />}
+          />
+          <Route path='/feed/:zid' component={({ match }: any) => <Feed zid={match.params.zid} />} />
+          <Route path='/feed' component={Loading} />
+        </Switch>
+        <FeedChat />
+      </div>
     </div>
   );
 };

a9c954e0873095bc0fcb3e54aa5920505c6d4664
Brett <colbr243@gmail.com>
2025-02-21 19:22:21 +1100

    feat: feed chat on post pages (#2683)
    * feat: show chat on post page
    
    * fix: home position

--- src/apps/feed/components/feed/index.tsx
+++ src/apps/feed/components/feed/index.tsx
@@ -5,7 +5,6 @@ import { Post } from '../post';
 import { PostInput } from '../post-input-hook';
 import { Waypoint } from 'react-waypoint';
 import { Panel, PanelBody, PanelHeader, PanelTitle } from '../../../../components/layout/panel';
-import { FeedChat } from '../feed-chat/container';
 
 import styles from './styles.module.scss';
 
@@ -32,54 +31,50 @@ export const Feed = ({ zid, hideZidAction }: FeedProps) => {
   } = useFeed(zid);
 
   return (
-    <div className={styles.FeedContainer}>
-      <Panel className={styles.Feed}>
-        <PanelHeader>
-          <PanelTitle>{headerText}</PanelTitle>
-        </PanelHeader>
-        <PanelBody className={styles.Panel}>
-          {channelZid && <PostInput className={styles.Input} channelZid={channelZid} />}
-          {isLoading && <Message>Loading posts...</Message>}
-          {isEmpty && <Message>This feed is empty</Message>}
-          {hasLoadedMessages && (
-            <ol>
-              {posts?.pages.map((page) =>
-                page.map((reply) => (
-                  <li key={reply.id}>
-                    <Post
-                      className={styles.Post}
-                      arweaveId={reply.arweaveId}
-                      avatarUrl={reply.sender?.avatarUrl}
-                      author={reply.sender?.displaySubHandle}
-                      channelZid={reply.channelZid}
-                      currentUserId={userId}
-                      loadAttachmentDetails={() => {}}
-                      meowPost={meowPostFeed}
-                      messageId={reply.id.toString()}
-                      nickname={reply.sender?.firstName}
-                      numberOfReplies={reply.numberOfReplies}
-                      ownerUserId={reply.sender?.userId}
-                      reactions={reply.reactions}
-                      text={reply.message}
-                      timestamp={reply.createdAt}
-                      userMeowBalance={userMeowBalance}
-                      variant='default'
-                      hideZidAction={hideZidAction}
-                    />
-                  </li>
-                ))
-              )}
-            </ol>
-          )}
-          {isError && <Message>Failed to load posts</Message>}
-          {isFetchingNextPage && <Message>Loading more posts</Message>}
-          {hasNextPage && !isFetchingNextPage && !isError && (
-            <Waypoint onEnter={() => fetchNextPage()} bottomOffset={'-90%'} />
-          )}
-        </PanelBody>
-      </Panel>
-
-      <FeedChat />
-    </div>
+    <Panel className={styles.Feed}>
+      <PanelHeader>
+        <PanelTitle>{headerText}</PanelTitle>
+      </PanelHeader>
+      <PanelBody className={styles.Panel}>
+        {channelZid && <PostInput className={styles.Input} channelZid={channelZid} />}
+        {isLoading && <Message>Loading posts...</Message>}
+        {isEmpty && <Message>This feed is empty</Message>}
+        {hasLoadedMessages && (
+          <ol>
+            {posts?.pages.map((page) =>
+              page.map((reply) => (
+                <li key={reply.id}>
+                  <Post
+                    className={styles.Post}
+                    arweaveId={reply.arweaveId}
+                    avatarUrl={reply.sender?.avatarUrl}
+                    author={reply.sender?.displaySubHandle}
+                    channelZid={reply.channelZid}
+                    currentUserId={userId}
+                    loadAttachmentDetails={() => {}}
+                    meowPost={meowPostFeed}
+                    messageId={reply.id.toString()}
+                    nickname={reply.sender?.firstName}
+                    numberOfReplies={reply.numberOfReplies}
+                    ownerUserId={reply.sender?.userId}
+                    reactions={reply.reactions}
+                    text={reply.message}
+                    timestamp={reply.createdAt}
+                    userMeowBalance={userMeowBalance}
+                    variant='default'
+                    hideZidAction={hideZidAction}
+                  />
+                </li>
+              ))
+            )}
+          </ol>
+        )}
+        {isError && <Message>Failed to load posts</Message>}
+        {isFetchingNextPage && <Message>Loading more posts</Message>}
+        {hasNextPage && !isFetchingNextPage && !isError && (
+          <Waypoint onEnter={() => fetchNextPage()} bottomOffset={'-90%'} />
+        )}
+      </PanelBody>
+    </Panel>
   );
 };

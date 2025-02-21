22c390a80f534cc00b3f1e7c19b7fe9800f670c1
Brett <12437916+colbr@users.noreply.github.com>
2025-02-21 19:19:10 +1100

    fix: home position

--- src/apps/home/index.tsx
+++ src/apps/home/index.tsx
@@ -2,13 +2,23 @@ import { useRouteMatch } from 'react-router-dom';
 import { PostView } from '../feed/components/post-view-container';
 import { Feed } from '../feed/components/feed';
 
+import styles from './styles.module.scss';
+
 export const HomeApp = () => {
   const route = useRouteMatch<{ postId: string }>('/home/:postId');
   const postId = route?.params?.postId;
 
   if (postId) {
-    return <PostView postId={postId} hideZidAction={true} isFeed={true} />;
+    return (
+      <div className={styles.Home}>
+        <PostView postId={postId} hideZidAction={true} isFeed={true} />
+      </div>
+    );
   }
 
-  return <Feed hideZidAction={true} />;
+  return (
+    <div className={styles.Home}>
+      <Feed hideZidAction={true} />
+    </div>
+  );
 };

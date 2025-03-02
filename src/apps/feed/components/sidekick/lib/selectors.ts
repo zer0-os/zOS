fdada1543cc9e5eee6dfc781b1962d018f496fea
dominic <domw30@gmail.com>
2025-03-02 12:17:16 +0000

    feat: wire up existing muting system to channels app

--- src/apps/feed/components/sidekick/lib/selectors.ts
+++ src/apps/feed/components/sidekick/lib/selectors.ts
@@ -2,6 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../../../../store/reducer';
 import { denormalizeConversations } from '../../../../../store/channels-list';
 import { UnreadCount } from './useSidekick';
+import { DefaultRoomLabels } from '../../../../../store/channels';
 
 export const selectSocialChannelsUnreadCounts = createSelector(
   [(state: RootState) => denormalizeConversations(state)],
@@ -12,5 +13,17 @@ export const selectSocialChannelsUnreadCounts = createSelector(
         acc[channel.zid!] = { total: channel.unreadCount?.total || 0, highlight: channel.unreadCount?.highlight || 0 };
         return acc;
       }, {} as { [zid: string]: UnreadCount });
+  }
+);
+
+export const selectMutedChannels = createSelector(
+  [(state: RootState) => denormalizeConversations(state)],
+  (conversations) => {
+    return conversations
+      .filter((c) => c.isSocialChannel && c.zid)
+      .reduce((acc, channel) => {
+        acc[channel.zid!] = channel.labels?.includes(DefaultRoomLabels.MUTE);
+        return acc;
+      }, {} as { [zid: string]: boolean });
   }
 );

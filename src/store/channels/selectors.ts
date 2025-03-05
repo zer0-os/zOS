271d309240d7755b7cbaf5eecf1b8be34e1e589c
dominic <domw30@gmail.com>
2025-03-05 23:13:14 +0000

    refactor(components): implement memoized selectors

--- src/store/channels/selectors.ts
+++ src/store/channels/selectors.ts
@@ -1,6 +1,17 @@
 import { RootState } from '../reducer';
 import getDeepProperty from 'lodash.get';
+import { createSelector } from 'reselect';
+import { denormalize as denormalizeChannel } from '../channels';
 
 export const rawChannel = (state: RootState, channelId: string) => {
   return getDeepProperty(state, `normalized.channels['${channelId}']`, null);
 };
+
+// Memoized selector for denormalized channel
+export const denormalizedChannelSelector = createSelector(
+  [(state: RootState) => state, (_state: RootState, channelId: string) => channelId],
+  (state, channelId) => {
+    console.log('Recalculating denormalizedChannelSelector for', channelId);
+    return denormalizeChannel(channelId, state);
+  }
+);

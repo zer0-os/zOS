ac3ccaa3273006da9f8459a04ef7a4f8e53c30af
dominic <domw30@gmail.com>
2025-03-28 14:15:50 +0400

    refactor: revert changes to use load attachement effect

--- src/components/message/hooks/useLoadAttachmentEffect.ts
+++ src/components/message/hooks/useLoadAttachmentEffect.ts
@@ -10,28 +10,19 @@ export const useLoadAttachmentEffect = (
 ) => {
   useEffect(() => {
     const isImage = media?.mimetype?.startsWith('image/');
-    const isEncrypted = !!media?.file;
-    const isMatrixUrl = media?.url?.startsWith('mxc://') || media?.file?.url?.startsWith('mxc://');
-    const isBlobUrl = media?.url?.startsWith('blob:');
 
     const isLoading = media?.downloadStatus === MediaDownloadStatus.Loading;
     const hasFailed = media?.downloadStatus === MediaDownloadStatus.Failed;
-
-    // We need to load attachment details for:
-    // 1. Non-image files with Matrix URLs
-    // 2. Encrypted image files that don't have a blob URL yet
-    // 3. Encrypted non-image files
     if (
       media &&
-      ((!isImage && isMatrixUrl) || (isEncrypted && isImage && !isBlobUrl)) &&
+      !isImage &&
+      (!media.url || media.url.startsWith('mxc://')) &&
       !isLoading &&
       !hasFailed &&
       messagesFetchStatus === MessagesFetchState.SUCCESS
     ) {
-      console.log('Loading attachment details for:', { messageId, isImage, isEncrypted, isMatrixUrl, isBlobUrl });
       loadAttachmentDetails({ media, messageId });
     }
-
     // `media` is re-rendering constantly - excluding until we figure out why
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
@@ -39,6 +30,5 @@ export const useLoadAttachmentEffect = (
     messageId,
     loadAttachmentDetails,
     media?.downloadStatus,
-    media?.url,
   ]);
 };

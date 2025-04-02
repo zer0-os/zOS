9390964e1916ff5fefcdcf55105a7d038ce73534
RickyRoller <travelingroller@gmail.com>
2025-04-02 09:05:20 -0700

    Add vercel preview header handling

--- src/lib/api/rest.ts
+++ src/lib/api/rest.ts
@@ -18,11 +18,22 @@ function apiUrl(path: string): string {
   ].join('');
 }
 
+const headers = new Headers();
 /**
  * The zOS code now passes an 'x-app-platform' header to ensure that the
  * access_token cookie is unique for the scope of zos user.
  */
-const xPlatFormHeader = { 'X-APP-PLATFORM': 'zos' };
+headers.set('X-APP-PLATFORM', 'zos');
+
+/**
+ * Vercel preview urls are on a different domain so we can't use cookie authentication.
+ * Adding a workaround to use header based authentication for these requests when in preview mode.
+ */
+export const addVercelPreviewAuthHeader = (token: string) => {
+  if (process.env.VERCEL_ENV === 'preview') {
+    headers.set('Authorization', `Bearer ${token}`);
+  }
+};
 
 export function get<T>(path: string, filter?: RequestFilter | string, query?: any) {
   let queryData;
@@ -40,21 +51,21 @@ export function get<T>(path: string, filter?: RequestFilter | string, query?: an
     queryData = { ...queryData, ...query };
   }
 
-  return Request.get<T>(apiUrl(path)).set(xPlatFormHeader).withCredentials().query(queryData);
+  return Request.get<T>(apiUrl(path)).set(headers).withCredentials().query(queryData);
 }
 
 export function post<T>(path: string) {
-  return Request.post<T>(apiUrl(path)).set(xPlatFormHeader).withCredentials();
+  return Request.post<T>(apiUrl(path)).set(headers).withCredentials();
 }
 
 export function put<T>(path: string) {
-  return Request.put<T>(apiUrl(path)).set(xPlatFormHeader).withCredentials();
+  return Request.put<T>(apiUrl(path)).set(headers).withCredentials();
 }
 
 export function patch<T>(path: string) {
-  return Request.patch<T>(apiUrl(path)).set(xPlatFormHeader).withCredentials();
+  return Request.patch<T>(apiUrl(path)).set(headers).withCredentials();
 }
 
 export function del<T>(path: string) {
-  return Request.delete<T>(apiUrl(path)).set(xPlatFormHeader).withCredentials();
+  return Request.delete<T>(apiUrl(path)).set(headers).withCredentials();
 }

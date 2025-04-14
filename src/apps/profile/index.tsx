f23e014cfcb0b8a74d9459e1170d7b75262cbfd9
colbr <colbr243@gmail.com>
2025-04-14 15:14:02 +0900

    refactor: move feed to switcher

--- src/apps/profile/index.tsx
+++ src/apps/profile/index.tsx
@@ -1,16 +1,12 @@
-import { useProfileApp } from './lib/useProfileApp';
-
-import { Feed } from '../feed/components/feed';
 import { UserPanel } from './panels/UserPanel';
+import { Switcher } from './panels/Switcher';
 
 import styles from './styles.module.scss';
 
 export const ProfileApp = () => {
-  const { data } = useProfileApp();
-
   return (
     <div className={styles.Wrapper}>
-      {data?.primaryZid && <Feed zid={data.primaryZid} />}
+      <Switcher />
       <div>
         <UserPanel />
       </div>
---
+++ src/apps/profile/panels/Switcher/index.tsx
@@ -0,0 +1,15 @@
+import { useProfileApp } from '../../lib/useProfileApp';
+
+import { Feed } from '../../../feed/components/feed';
+
+import styles from './styles.module.scss';
+
+export const Switcher = () => {
+  const { data } = useProfileApp();
+
+  return (
+    <div className={styles.Container}>
+      <div className={styles.Content}>{data?.primaryZid && <Feed zid={data.primaryZid} />}</div>
+    </div>
+  );
+};

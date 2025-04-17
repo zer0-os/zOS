96fc39e8d6fc3220f21e79cebf048ee242f04c3a
dominic <domw30@gmail.com>
2025-04-17 09:52:28 +0100

    feat(profile): enable post viewing within profile context

--- src/apps/profile/index.tsx
+++ src/apps/profile/index.tsx
@@ -1,15 +1,28 @@
+import { useRouteMatch } from 'react-router-dom';
 import { UserPanel } from './panels/UserPanel';
 import { Switcher } from './panels/Switcher';
+import { PostView } from '../feed/components/post-view-container';
+import { useScrollPosition } from '../../lib/hooks/useScrollPosition';
 
 import styles from './styles.module.scss';
 
 export const ProfileApp = () => {
+  const route = useRouteMatch<{ zid: string; postId?: string }>('/profile/:zid/:postId?');
+  const postId = route?.params?.postId;
+  useScrollPosition(postId);
+
   return (
     <div className={styles.Wrapper}>
-      <Switcher />
-      <div>
-        <UserPanel />
-      </div>
+      {postId ? (
+        <PostView postId={postId} isFeed={true} />
+      ) : (
+        <>
+          <Switcher />
+          <div>
+            <UserPanel />
+          </div>
+        </>
+      )}
     </div>
   );
 };

c0c73d9b2721d3ebfba4c01a65c69bc75f1a9526
dominic <domw30@gmail.com>
2025-04-22 12:49:37 +0100

    refactor: add more logs

--- src/apps/profile/lib/useProfileApp.ts
+++ src/apps/profile/lib/useProfileApp.ts
@@ -5,8 +5,10 @@ import { featureFlags } from '../../../lib/feature-flags';
 export const useProfileApp = () => {
   const match = useRouteMatch('/profile/:id');
   const id = match?.params.id;
+  console.log('XXXX Profile Match:', { match, id });
 
   const isEnabled = featureFlags.enableUserProfiles;
+  console.log('XXXX Profile Feature Flag:', { isEnabled, id });
 
   return useProfile({ id: isEnabled ? id : undefined });
 };

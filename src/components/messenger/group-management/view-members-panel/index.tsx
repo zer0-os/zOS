b8a63c42bce63995f21b8d913b0f7a5f5928b05c
colbr <colbr243@gmail.com>
2025-05-30 00:29:56 +0200

    fix: add missing key

--- src/components/messenger/group-management/view-members-panel/index.tsx
+++ src/components/messenger/group-management/view-members-panel/index.tsx
@@ -89,7 +89,7 @@ export class ViewMembersPanel extends React.Component<Properties, State> {
           <ScrollbarContainer>
             <CitizenListItem user={this.props.currentUser} tag={this.getTag(this.props.currentUser)}></CitizenListItem>
             {visibleMembers.map((u) => (
-              <CitizenListItem user={u} tag={this.getTag(u)}></CitizenListItem>
+              <CitizenListItem key={u.userId} user={u} tag={this.getTag(u)}></CitizenListItem>
             ))}
           </ScrollbarContainer>
 

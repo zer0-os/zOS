543b9edaf535c1ff14880615ddf1b37b2dc6a8c8
Ricky Roller <travelingroller@gmail.com>
2025-06-19 16:39:38 -0600

    Fix avatar flicker (#3111)

--- src/components/matrix-avatar/index.tsx
+++ src/components/matrix-avatar/index.tsx
@@ -8,16 +8,25 @@
 import { Avatar, AvatarProps } from '@zero-tech/zui/components/Avatar';
 import { useMatrixImage } from '../../lib/hooks/useMatrixImage';
 import { memo } from 'react';
+import cx from 'classnames';
+import styles from './matrix-avatar.module.css';
 
 interface MatrixAvatarProps extends AvatarProps {
   imageURL?: string;
   className?: string;
 }
 
-export const MatrixAvatar = memo(({ imageURL, size = 'regular', ...rest }: MatrixAvatarProps) => {
-  const { data: authenticatedUrl } = useMatrixImage(imageURL, { isThumbnail: true });
+export const MatrixAvatar = memo(({ imageURL, size = 'regular', className, ...rest }: MatrixAvatarProps) => {
+  const { data: authenticatedUrl, isFetching } = useMatrixImage(imageURL, { isThumbnail: true });
 
-  return <Avatar size={size} imageURL={authenticatedUrl} {...rest} />;
+  return (
+    <Avatar
+      size={size}
+      imageURL={authenticatedUrl}
+      className={cx({ [styles.hidden]: isFetching }, className)}
+      {...rest}
+    />
+  );
 });
 
 MatrixAvatar.displayName = 'MatrixAvatar';

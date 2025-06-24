6d0998b938e358ca6f7d1745437b72b7a43eb7f3
dominic <domw30@gmail.com>
2025-06-24 14:38:26 +0100

    fix: refresh current user data

--- src/store/authentication/index.ts
+++ src/store/authentication/index.ts
@@ -5,11 +5,13 @@ export enum SagaActionTypes {
   Logout = 'authentication/saga/logout',
   ForceLogout = 'authentication/saga/force-logout',
   CloseLogoutModal = 'authentication/saga/close-logout-modal',
+  RefreshCurrentUser = 'authentication/saga/refresh-current-user',
 }
 
 export const logout = createAction(SagaActionTypes.Logout);
 export const forceLogout = createAction(SagaActionTypes.ForceLogout);
 export const closeLogoutModal = createAction(SagaActionTypes.CloseLogoutModal);
+export const refreshCurrentUser = createAction(SagaActionTypes.RefreshCurrentUser);
 
 export const initialState: AuthenticationState = {
   user: { data: null },

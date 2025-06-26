c85d3b8534b63c7d3f55fab0b15d6204bbef976f
colbr <colbr243@gmail.com>
2025-06-26 21:15:23 +0200

    chore: clean up types

--- src/apps/feed/components/post-input-hook/index.tsx
+++ src/apps/feed/components/post-input-hook/index.tsx
@@ -10,14 +10,12 @@ export interface PostInputProps {
 }
 
 export const PostInput = ({ className, channelZid, replyToId }: PostInputProps) => {
-  const { error, handleOnSubmit, isLoading, userProfileImageUrl } = usePostInput(channelZid, replyToId);
+  const { handleOnSubmit, userProfileImageUrl } = usePostInput(channelZid, replyToId);
 
   return (
     <PostInputComponent
       avatarUrl={userProfileImageUrl}
       className={className}
-      error={error?.message}
-      isSubmitting={isLoading}
       onSubmit={handleOnSubmit}
       viewMode={ViewModes.Dark}
     />
--- src/apps/feed/components/post-input-hook/usePostInput.ts
+++ src/apps/feed/components/post-input-hook/usePostInput.ts
@@ -5,16 +5,14 @@ import { userProfileImageSelector } from '../../../../store/authentication/selec
 
 export const usePostInput = (channelZid: string, replyToId?: string) => {
   const userProfileImageUrl = useSelector(userProfileImageSelector);
-  const { error, handleOnSubmit: handleOnSubmitPost, isLoading } = useSubmitPost();
+  const { handleOnSubmit: handleOnSubmitPost } = useSubmitPost();
 
   const handleOnSubmit = (value: string, media: Media[]) => {
     handleOnSubmitPost({ channelZid, media, message: value, replyToId });
   };
 
   return {
-    error,
     handleOnSubmit,
-    isLoading,
     userProfileImageUrl,
   };
 };

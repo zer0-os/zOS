8b3dae00b5fc9743fad1f810f3aff3e99973ed73
Brett <colbr243@gmail.com>
2025-07-09 16:53:36 +0200

    feat: staking app setup (#3161)
    * chore: add staking feature flag
    
    * feat: add template staking app
    
    * feat: staking app positioning
    
    * feat: hide sidekick on staking app
    
    * test: fix tests

--- src/lib/feature-flags/flags.ts
+++ src/lib/feature-flags/flags.ts
@@ -30,7 +30,8 @@ export type FeatureFlagKey =
   | 'enableMatrixDebug'
   | 'enableProfileDirectMessage'
   | 'enableZeroPro'
-  | 'enableQuotes';
+  | 'enableQuotes'
+  | 'enableStaking';
 
 export interface FeatureFlagConfig {
   defaultValue: boolean;

6fb39343b2f01e502c5474c3110f84de14b2b829
RickyRoller <travelingroller@gmail.com>
2025-07-10 15:13:23 -0600

    Styling clean up and zero states

--- src/apps/wallet/tokens/tokens-list.tsx
+++ src/apps/wallet/tokens/tokens-list.tsx
@@ -2,23 +2,31 @@ import { useBalancesQuery } from '../queries/useBalancesQuery';
 import { Token } from './token';
 import { useSelector } from 'react-redux';
 import { selectedWalletSelector } from '../../../store/wallet/selectors';
+import { Skeleton } from '@zero-tech/zui/components';
+import { WalletEmptyState } from '../components/empty-state/wallet-empty-state';
 
 import styles from './tokens-list.module.scss';
+
+const skeletons = Array.from({ length: 10 });
 
 export const TokensList = () => {
   const selectedWallet = useSelector(selectedWalletSelector);
   const { data, isPending } = useBalancesQuery(selectedWallet.address);
-  const tokens = data?.tokens;
-
-  if (isPending) {
-    return <div>Loading...</div>;
-  }
+  const tokens = data?.tokens ?? [];
 
   return (
-    <div className={styles.tokensList}>
-      {tokens?.map((token) => (
-        <Token key={token.tokenAddress} token={token} />
-      ))}
+    <div className={styles.tokensView}>
+      {(data || isPending) && (
+        <div className={styles.tokensList}>
+          {isPending && skeletons.map((_, index) => <Skeleton key={index} className={styles.tokenSkeleton} />)}
+
+          {tokens.map((token) => (
+            <Token key={token.tokenAddress} token={token} />
+          ))}
+        </div>
+      )}
+
+      {tokens.length === 0 && <WalletEmptyState className={styles.emptyState} title='No Tokens' />}
     </div>
   );
 };

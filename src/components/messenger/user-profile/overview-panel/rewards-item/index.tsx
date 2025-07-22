885ea30efb29f61c596b519670c7768511ca4665
DomW <domw30@gmail.com>
2025-07-22 23:14:04 +0100

    feat(hover-card/claim): add join button to hover card content (#3207)

--- src/components/messenger/user-profile/overview-panel/rewards-item/index.tsx
+++ src/components/messenger/user-profile/overview-panel/rewards-item/index.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 
 import { IconChevronRight, IconInfoCircle } from '@zero-tech/zui/icons';
 import { calculateTotalPriceInUSDCents, formatUSD, formatWeiAmount } from '../../../../../lib/number';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../../../store/reducer';
 import { ClaimRewardsButton } from '../../../../claim-rewards-button';
 import { HoverCard } from '../../../../hover-card';
+import { Button, Variant as ButtonVariant, Size } from '@zero-tech/zui/components';
+import { openZeroPro } from '../../../../../store/user-profile';
 
 import styles from './styles.module.scss';
 
 export interface Properties {}
 
 const selectRewards = (state: RootState) => state.rewards;
+const selectIsZeroProSubscriber = (state: RootState) => state.authentication.user.data.subscriptions.zeroPro;
 
 export const RewardsItem: React.FC<Properties> = () => {
   const rewards = useSelector(selectRewards);
+  const isZeroProSubscriber = useSelector(selectIsZeroProSubscriber);
+  const dispatch = useDispatch();
 
   const totalRewards =
     BigInt(rewards.totalDailyRewards) + BigInt(rewards.totalReferralFees) + BigInt(rewards.legacyRewards);
@@ -23,10 +28,38 @@ export const RewardsItem: React.FC<Properties> = () => {
     calculateTotalPriceInUSDCents(rewards.unclaimedRewards.toString(), rewards.meowInUSD ?? 0)
   );
   const claimableMeow = `${formatWeiAmount(rewards.unclaimedRewards.toString())} MEOW`;
-  const hoverCardContent =
-    BigInt(rewards.unclaimedRewards) > 0n
-      ? `You can now claim ${claimableRewardsUSD} or ${claimableMeow}`
-      : 'No earnings available to claim';
+
+  const handleJoinZeroPro = () => {
+    dispatch(openZeroPro());
+  };
+
+  const renderHoverCardContent = () => {
+    if (BigInt(rewards.unclaimedRewards) > 0n) {
+      return (
+        <div>
+          You can now claim {claimableRewardsUSD} or {claimableMeow}
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.HoverCardText}>
+        <div>No earnings available to claim</div>
+        {!isZeroProSubscriber && (
+          <>
+            <Button
+              variant={ButtonVariant.Secondary}
+              onPress={handleJoinZeroPro}
+              size={Size.Small}
+              className={styles.HoverCardTextButton}
+            >
+              Join ZERO Pro
+            </Button>
+          </>
+        )}
+      </div>
+    );
+  };
 
   return (
     <div className={styles.RewardsItem}>
@@ -49,7 +82,7 @@ export const RewardsItem: React.FC<Properties> = () => {
           <div className={styles.ClaimableRewards}>
             <HoverCard
               iconTrigger={<IconInfoCircle className={styles.InfoIcon} size={18} />}
-              content={<div>{hoverCardContent}</div>}
+              content={renderHoverCardContent()}
             />
           </div>
         </div>

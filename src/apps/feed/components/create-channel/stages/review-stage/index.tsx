b14b12d0556b50c82128b832786f7fe3dd8d78f4
dominic <domw30@gmail.com>
2025-08-01 11:25:29 +0100

    fix: revert wallet integration

--- src/apps/feed/components/create-channel/stages/review-stage/index.tsx
+++ src/apps/feed/components/create-channel/stages/review-stage/index.tsx
@@ -2,17 +2,15 @@ import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { IconChevronRightDouble } from '@zero-tech/zui/icons';
 import { ethers } from 'ethers';
+import { useAccount } from 'wagmi';
 import { calculateTotalPriceInUSDCents, formatUSD } from '../../../../../../lib/number';
 import { parsePrice } from '../../lib/utils';
 import { meowInUSDSelector } from '../../../../../../store/rewards/selectors';
 import { usePurchaseZid } from './hooks/usePurchaseZid';
 import { parsePurchaseError } from './utils';
 import { TokenData } from '../../lib/hooks/useTokenFinder';
-import { RootState } from '../../../../../../store/reducer';
 
 import styles from './styles.module.scss';
-
-const selectWeb3State = (state: RootState) => state.web3;
 
 interface ReviewStageProps {
   onNext: () => void;
@@ -33,9 +31,8 @@ export const ReviewStage: React.FC<ReviewStageProps> = ({
 }) => {
   const meowPriceUSD = useSelector(meowInUSDSelector);
 
-  // Use app's wallet connection status
-  const web3State = useSelector(selectWeb3State);
-  const account = web3State?.value?.address;
+  // RainbowKit/Wagmi wallet integration
+  const { address: account } = useAccount();
 
   const signer = useCallback(() => {
     if (typeof window !== 'undefined' && window.ethereum) {

7aa78f6d620a3aa65a05d6ec1bd59e534457b875
Ricky Roller <travelingroller@gmail.com>
2025-08-06 10:21:10 -0700

    Add vMEOW to total balance (#3277)
    * Add vMEOW to total balance
    
    * Fix conditional

--- src/apps/wallet/components/zero-card/zero-card.tsx
+++ src/apps/wallet/components/zero-card/zero-card.tsx
@@ -6,7 +6,7 @@ import zeroWordmark from './zero-wordmark.png';
 import { useSelector } from 'react-redux';
 import { selectedWalletSelector } from '../../../../store/wallet/selectors';
 import { useBalancesQuery } from '../../queries/useBalancesQuery';
-import { MEOW_TOKEN_ADDRESS } from '../../constants';
+import { MEOW_TOKEN_ADDRESS, VMEOW_TOKEN_ADDRESS } from '../../constants';
 
 interface ZeroCardProps {
   displayName: string;
@@ -16,6 +16,9 @@ export const ZeroCard = ({ displayName }: ZeroCardProps) => {
   const selectedWallet = useSelector(selectedWalletSelector);
   const { data } = useBalancesQuery(selectedWallet.address);
   const meowToken = data?.tokens?.find((token) => token.tokenAddress === MEOW_TOKEN_ADDRESS);
+  const vmeowToken = data?.tokens?.find((token) => token.tokenAddress === VMEOW_TOKEN_ADDRESS);
+  const price = meowToken?.price ?? vmeowToken?.price;
+  const totalAmount = Number(meowToken?.amount ?? 0) + Number(vmeowToken?.amount ?? 0);
 
   return (
     <div className={styles.zeroCard}>
@@ -23,10 +26,10 @@ export const ZeroCard = ({ displayName }: ZeroCardProps) => {
       <div className={styles.zeroCardHeader}>
         <img src={zeroWordmark} alt='Zero Logo' className={styles.zeroCardWordmark} />
       </div>
-      {meowToken && !!meowToken.price && (
+      {price && totalAmount > 0 && (
         <div className={styles.zeroCardBody}>
           <div className={styles.balanceLabel}>Balance</div>
-          <div className={styles.balanceValue}>{formatDollars(meowToken?.price * Number(meowToken?.amount))}</div>
+          <div className={styles.balanceValue}>{formatDollars(price * totalAmount)}</div>
           {/* <PercentChange percent={null} /> */}
         </div>
       )}

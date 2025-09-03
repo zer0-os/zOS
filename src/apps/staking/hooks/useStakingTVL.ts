1c85f4e098e5dc5f4eabd81debc96cfe411af3e4
Ricky Roller <travelingroller@gmail.com>
2025-09-03 11:44:21 -0700

    Fix tvl for z chain staking (#3339)

--- src/apps/staking/hooks/useStakingTVL.ts
+++ src/apps/staking/hooks/useStakingTVL.ts
@@ -10,15 +10,17 @@ export const useStakingTVL = (poolAddress: string, chainId: number) => {
   const { data: stakingTokenPrice, isPending } = useTokenPrice(stakingTokenInfo?.address, chainId);
   const meowPrice = useSelector(meowInUSDSelector);
 
+  let loading = isPending;
   let price = stakingTokenPrice ?? 0;
   if (stakingTokenInfo?.address === MEOW_TOKEN_ADDRESS || stakingTokenInfo?.address === MEOW_ZEPHYR_TOKEN_ADDRESS) {
     price = meowPrice;
+    loading = false;
   }
 
   const totalStakedFormatted = totalStaked ? parseFloat(ethers.utils.formatUnits(totalStaked, 18)) : 0;
   return {
     tvl: totalStakedFormatted * price,
     stakingTokenPrice: price,
-    loading: isPending,
+    loading,
   };
 };

d0a459c0885c71ee7541e4644fdadeebe1f7007d
Ricky Roller <travelingroller@gmail.com>
2025-09-02 18:11:53 -0700

    Fix: Disable Aura zApp (#3335)
    * Disable aura zapp
    
    * Remove aura auth handler

--- src/apps/external-app/types/messageTypeGuard.ts
+++ src/apps/external-app/types/messageTypeGuard.ts
@@ -1,17 +1,7 @@
-import {
-  ZAppMessageType,
-  IncomingMessage,
-  RouteChangeMessage,
-  AuthenticateMessage,
-  ChannelHandshakeMessage,
-} from './types';
+import { ZAppMessageType, IncomingMessage, RouteChangeMessage, ChannelHandshakeMessage } from './types';
 
 export function isRouteChangeEvent(event: MessageEvent<IncomingMessage>): event is MessageEvent<RouteChangeMessage> {
   return event.data.type === ZAppMessageType.RouteChange;
-}
-
-export function isAuthenticateEvent(event: MessageEvent<IncomingMessage>): event is MessageEvent<AuthenticateMessage> {
-  return event.data.type === ZAppMessageType.Authenticate;
 }
 
 export function isChannelHandshakeEvent(

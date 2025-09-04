758114fb09947b62da66255554b38e157f2bd3d0
Ricky Roller <travelingroller@gmail.com>
2025-09-04 11:27:52 -0700

    Update wallet balance to take into account multi-chain assets (#3347)
    * Update wallet balance to take into account multi-chain assets
    
    * Update naming

--- src/apps/wallet/components/zero-card/zero-card.tsx
+++ src/apps/wallet/components/zero-card/zero-card.tsx
@@ -3,22 +3,14 @@ import { Barcode } from './barcode';
 import styles from './zero-card.module.scss';
 import zeroLogo from './zero-logo.png';
 import zeroWordmark from './zero-wordmark.png';
-import { useSelector } from 'react-redux';
-import { selectedWalletSelector } from '../../../../store/wallet/selectors';
-import { useBalancesQuery } from '../../queries/useBalancesQuery';
-import { MEOW_TOKEN_ADDRESS, VMEOW_TOKEN_ADDRESS } from '../../constants';
+import { useWalletBalance } from '../../hooks/useWalletBalance';
 
 interface ZeroCardProps {
   displayName: string;
 }
 
 export const ZeroCard = ({ displayName }: ZeroCardProps) => {
-  const selectedWallet = useSelector(selectedWalletSelector);
-  const { data } = useBalancesQuery(selectedWallet.address);
-  const meowToken = data?.tokens?.find((token) => token.tokenAddress === MEOW_TOKEN_ADDRESS);
-  const vmeowToken = data?.tokens?.find((token) => token.tokenAddress === VMEOW_TOKEN_ADDRESS);
-  const price = meowToken?.price ?? vmeowToken?.price;
-  const totalAmount = Number(meowToken?.amount ?? 0) + Number(vmeowToken?.amount ?? 0);
+  const { balance } = useWalletBalance();
 
   return (
     <div className={styles.zeroCard}>
@@ -26,10 +18,10 @@ export const ZeroCard = ({ displayName }: ZeroCardProps) => {
       <div className={styles.zeroCardHeader}>
         <img src={zeroWordmark} alt='Zero Logo' className={styles.zeroCardWordmark} />
       </div>
-      {price && totalAmount > 0 && (
+      {balance > 0 && (
         <div className={styles.zeroCardBody}>
           <div className={styles.balanceLabel}>Balance</div>
-          <div className={styles.balanceValue}>{formatDollars(price * totalAmount)}</div>
+          <div className={styles.balanceValue}>{formatDollars(balance)}</div>
           {/* <PercentChange percent={null} /> */}
         </div>
       )}

d669ad4c97fb658016913f49fad0ddd65772bde0
Ratik Jindal <ratikjindal21@gmail.com>
2025-09-10 08:19:55 +0530

    add hyperlink (#3354)

--- src/apps/leaderboard/components/table-row/user-display/index.tsx
+++ src/apps/leaderboard/components/table-row/user-display/index.tsx
@@ -1,4 +1,5 @@
 import { IconZeroProVerified } from '@zero-tech/zui/icons';
+import { Link } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
@@ -8,22 +9,36 @@ interface UserDisplayProps {
   isProUser: boolean;
 }
 
-export const UserDisplay = ({ name, primaryZid, isProUser }: UserDisplayProps) => {
-  return (
-    <div className={styles.userContainer}>
-      <div className={styles.nameRow}>
-        <span className={styles.name}>{name}</span>
-        {isProUser && (
-          <div className={styles.proBadge}>
-            <IconZeroProVerified size={16} />
-          </div>
-        )}
-      </div>
-      {primaryZid && (
-        <div className={styles.zidRow}>
-          <span className={styles.zid}>{primaryZid}</span>
+const UserContent = ({ name, primaryZid, isProUser }: UserDisplayProps) => (
+  <div className={styles.userContainer}>
+    <div className={styles.nameRow}>
+      <span className={styles.name}>{name}</span>
+      {isProUser && (
+        <div className={styles.proBadge}>
+          <IconZeroProVerified size={16} />
         </div>
       )}
     </div>
+    {primaryZid && (
+      <div className={styles.zidRow}>
+        <span className={styles.zid}>{primaryZid}</span>
+      </div>
+    )}
+  </div>
+);
+
+export const UserDisplay = ({ name, primaryZid, isProUser }: UserDisplayProps) => {
+  // Remove '0://' prefix from ZID if present for the URL
+  const profileIdentifier = primaryZid?.replace('0://', '');
+
+  // If we don't have a primaryZid, render without a link
+  if (!profileIdentifier) {
+    return <UserContent name={name} primaryZid={primaryZid} isProUser={isProUser} />;
+  }
+
+  return (
+    <Link to={`/profile/${profileIdentifier}`} className={styles.userLink}>
+      <UserContent name={name} primaryZid={primaryZid} isProUser={isProUser} />
+    </Link>
   );
 };

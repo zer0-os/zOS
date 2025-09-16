45a1fd9c97bf17624577abce63095e777bc8ed15
Ricky Roller <travelingroller@gmail.com>
2025-09-16 15:27:32 -0700

    Add error message for gas balance (#3363)

--- src/apps/staking/lib/useClaimRewards.ts
+++ src/apps/staking/lib/useClaimRewards.ts
@@ -3,18 +3,27 @@ import { post } from '../../../lib/api/rest';
 import { useSelector } from 'react-redux';
 import { selectedWalletSelector } from '../../../store/wallet/selectors';
 import { queryClient } from '../../../lib/web3/rainbowkit/provider';
+import { isWalletAPIError } from '../../../store/wallet/utils';
 
 export const useClaimRewards = (poolAddress: string, chainId: number) => {
   const { address: userAddress } = useSelector(selectedWalletSelector);
 
   return useMutation({
     mutationFn: async () => {
-      const response = await post(`/api/wallet/${userAddress}/transactions/claim-staking-rewards`).send({
-        poolAddress,
-        chainId,
-      });
+      try {
+        const response = await post(`/api/wallet/${userAddress}/transactions/claim-staking-rewards`).send({
+          poolAddress,
+          chainId,
+        });
 
-      return response.body;
+        return response.body;
+      } catch (e) {
+        if (isWalletAPIError(e) && e.response.body.code === 'INSUFFICIENT_BALANCE') {
+          throw new Error('Gas balance is not enough for this transaction');
+        }
+        console.error(e);
+        throw new Error('Failed to claim rewards.');
+      }
     },
     onSuccess: () => {
       queryClient.setQueryData(

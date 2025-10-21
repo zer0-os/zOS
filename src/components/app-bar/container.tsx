c75d12a41349823dd2c2567a440a42b91b06d4f2
ratik21 <ratikjindal21@gmail.com>
2025-10-21 23:32:14 +0530

    update app logic in app-bar

--- src/components/app-bar/container.tsx
+++ src/components/app-bar/container.tsx
@@ -3,10 +3,16 @@ import { AppBar as AppBarComponent } from './';
 import { useSelector } from 'react-redux';
 import { getLastActiveConversation } from '../../lib/last-conversation';
 import { useMemo } from 'react';
-import { activeZAppFeatureSelector } from '../../store/active-zapp/selectors';
-import { activeConversationIdSelector, rawActiveConversationSelector } from '../../store/chat/selectors';
-import { hasUnreadHighlightsSelector, hasUnreadNotificationsSelector } from '../../store/channels/selectors';
+import {
+  channelMapSelector,
+  isEncryptedNonSocial,
+  allChannelsSelector,
+  hasUnreadHighlightsSelector,
+  hasUnreadNotificationsSelector,
+} from '../../store/channels/selectors';
+import { activeConversationIdSelector } from '../../store/chat/selectors';
 import { hasActiveWalletSelector } from '../../store/wallet/selectors';
+import { activeZAppFeatureSelector } from '../../store/active-zapp/selectors';
 
 export const AppBar = () => {
   const {
@@ -32,26 +38,63 @@ export const AppBar = () => {
 
 const useAppBar = () => {
   const match = useRouteMatch('/:app');
+  const conversationRouteMatch = useRouteMatch<{ conversationId: string }>('/conversation/:conversationId');
   const activeConversationId = useSelector(activeConversationIdSelector);
-  const rawActiveConversation = useSelector(rawActiveConversationSelector);
-  const isActiveConversationSocialChannel = rawActiveConversation?.isSocialChannel;
   const hasActiveWallet = useSelector(hasActiveWalletSelector);
 
   const hasUnreadNotifications = useSelector(hasUnreadNotificationsSelector);
   const hasUnreadHighlights = useSelector(hasUnreadHighlightsSelector);
+  const conversations = useSelector(allChannelsSelector);
+  const conversationMap = useSelector(channelMapSelector);
+
+  const pickEncryptedId = (id?: string | null) =>
+    id && isEncryptedNonSocial(conversationMap.get(id)) ? id : undefined;
+
+  const firstEncryptedConversationId = useMemo(() => {
+    for (const conversation of conversations ?? []) {
+      if (isEncryptedNonSocial(conversation)) {
+        return conversation.id;
+      }
+    }
+    return undefined;
+  }, [conversations]);
 
   const lastActiveMessengerConversationId = useMemo(() => {
-    if (!isActiveConversationSocialChannel) {
-      return getLastActiveConversation();
+    const storedId = pickEncryptedId(getLastActiveConversation());
+    if (storedId) {
+      return storedId;
     }
-    return null;
+
+    const activeId = pickEncryptedId(activeConversationId);
+    if (activeId) {
+      return activeId;
+    }
+
+    return firstEncryptedConversationId;
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [!isActiveConversationSocialChannel ? activeConversationId : null]);
+  }, [activeConversationId, conversationMap, firstEncryptedConversationId]);
 
   const zAppIsFullscreen = useSelector(activeZAppFeatureSelector('fullscreen'));
 
+  const routeApp = match?.params?.app ?? '';
+  const routeConversationId = conversationRouteMatch?.params?.conversationId;
+  const routedConversation = routeConversationId ? conversationMap.get(routeConversationId) : undefined;
+
+  const activeApp = useMemo(() => {
+    if (routeApp === 'conversation') {
+      if (routedConversation && !isEncryptedNonSocial(routedConversation)) {
+        // routed conversation is unencrypted → feed
+      }
+      const unencryptedByRoute = routedConversation && !isEncryptedNonSocial(routedConversation);
+      if (unencryptedByRoute) {
+        return 'feed';
+      }
+    }
+    return routeApp;
+  }, [routeApp, routedConversation]);
+
   return {
-    activeApp: match?.params?.app ?? '',
+    activeApp,
     hasUnreadNotifications,
     hasUnreadHighlights,
     lastActiveMessengerConversationId,
